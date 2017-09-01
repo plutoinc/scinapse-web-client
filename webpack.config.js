901043c53c17adaaa2fcca29b2ffd3dbd548c058
Tylor Shin <shincode@icloud.com>
2017-09-01 20:31:27 +0900

    Set Basic webpack development configuration

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: ["babel-polyfill", "./app/index.tsx"],
+  output: {
+    filename: "./dist/bundle.js",
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-sprite-loader",
+        options: {
+          classPrefix: false,
+          idPrefix: true,
+        },
+      },
+      {
+        test: /\.html$/,
+        use: ["raw-loader"],
+      },
+      {
+        test: /\.md?$/,
+        use: [{ loader: "html-loader" }, { loader: "markdown-loader" }],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "isomorphic-style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: "[name]__[local]",
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [require("precss"), require("autoprefixer"), require("postcss-flexbugs-fixes")];
+              },
+            },
+          },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  node: {
+    fs: "empty",
+  },
+  externals: {
+    "react/lib/ExecutionEnvironment": true,
+    "react/lib/ReactContext": true,
+    "react/addons": true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "app/index.ejs",
+      inject: false,
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "app"),
+    compress: true,
+    host: "0.0.0.0",
+    hot: true,
+    open: true,
+    allowedHosts: ["localhost", "lvh.me"],
+  },
+};
