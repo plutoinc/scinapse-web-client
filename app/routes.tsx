f256c67d571c4aabcc427692a5592c61f298d309
academey <academey@gmail.com>
2017-09-04 14:30:22 +0900

    Add react-router to default routing

--- app/routes.tsx
+++ app/routes.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
-import Home from "./components/home";
+// containers
+import RootComponent from "./components/root";
+import HomeComponent from "./components/home";
 
 const routesMap = (
   <div>
-    <Route path="/" component={Home} exact />
+    <Route path="/" component={RootComponent} />
+    <Route exact path="/" component={HomeComponent} />
   </div>
 );
 
