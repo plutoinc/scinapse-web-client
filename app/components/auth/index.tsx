12e25d36ba971804e70e9e5a3f3d60558aa20e9d
academey <academey@gmail.com>
2017-09-04 20:38:23 +0900

    Add layout to auth page

--- app/components/auth/index.tsx
+++ app/components/auth/index.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 // components
+import { Header, Footer } from '../layouts';
 import Login from './login';
 import SignIn from './signIn';
 
 export default class AuthComponent extends React.PureComponent<null, null> {
   render() {
     return (
-      <Switch>
-        <Route exact path="/auth/login" component={Login} />
-        <Route exact path="/auth/signIn" component={SignIn} />
-      </Switch>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/auth/login" component={Login} />
+          <Route exact path="/auth/signIn" component={SignIn} />
+        </Switch>
+        <Footer />
+      </div>
     )
   }
 }
