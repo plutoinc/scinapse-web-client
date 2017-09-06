a7ca19c2a4610ea80ab8d93295c6ff9c6f153260
academey <academey@gmail.com>
2017-09-06 17:10:05 +0900

    Add footer styling

--- app/components/layouts/footer.tsx
+++ app/components/layouts/footer.tsx
@@ -10,17 +10,13 @@ export default class Footer extends React.PureComponent<null, null> {
     return (
       <footer className={styles.footer}>
         <div className={styles.footerContainer}>
-          <ul className={styles.copyRightWrapper}>
-            <li>
-              <Icon className={styles.footerLogo} icon="FOOTER_LOGO" />
-            </li>
-            <li>
-              <div className={styles.copyRightContent}>
-                Copyright © 2017 Pluto Network.<br />
-                All rights reserved.
-              </div>
-            </li>
-          </ul>
+          <div className={styles.copyRightWrapper}>
+            <Icon className={styles.footerLogo} icon="FOOTER_LOGO" />
+            <div className={styles.copyRightContent}>
+              Copyright © 2017 Pluto Network.<br />
+              All rights reserved.
+            </div>
+          </div>
           <ul className={styles.navbarWrapper}>
             <li className={styles.boldContent}>
               Pluto network
@@ -36,18 +32,18 @@ export default class Footer extends React.PureComponent<null, null> {
             <li className={styles.boldContent}>
               About
             </li>
-            <a href="https://github.com/pluto-net" target="_blank" className={styles.footerIconWrapper}>
+            <a className={styles.normalContent} href="https://github.com/pluto-net" target="_blank">
               Blog
             </a>
             <Link className={styles.normalContent} to="/faq">
               FAQ
             </Link>
           </ul>
-          <ul className={styles.iconsWrapper}>
-            <li className={styles.boldContent}>
+          <div className={styles.iconsWrapper}>
+            <div className={styles.boldContent}>
               Follow
-            </li>
-            <li>
+            </div>
+            <div>
               <a href="https://github.com/pluto-net" target="_blank" className={styles.footerIconWrapper}>
                 <Icon icon="FACEBOOK" />
               </a>
@@ -63,8 +59,8 @@ export default class Footer extends React.PureComponent<null, null> {
               <a href="https://github.com/pluto-net" target="_blank" className={styles.footerIconWrapper}>
                 <Icon icon="GITHUB" />
               </a>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </footer>
     );
