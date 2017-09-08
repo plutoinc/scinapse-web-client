ac9463bd5d1d0e3371493b1b7f83443d9d08da13
academey <academey@gmail.com>
2017-09-08 15:37:06 +0900

    Connect signUp with redux

---
+++ app/components/auth/signUp/actions.tsx
@@ -0,0 +1,71 @@
+// import { toJS } from "immutable";
+import { Dispatch } from "redux";
+
+export const ACTION_TYPES = {
+  CHANGE_EMAIL_INPUT            : 'SIGN_UP.CHANGE_EMAIL_INPUT',
+  CHANGE_PASSWORD_INPUT         : 'SIGN_UP.CHANGE_PASSWORD_INPUT',
+  CHANGE_REPEAT_PASSWORD_INPUT  : 'SIGN_UP.CHANGE_REPEAT_PASSWORD_INPUT',
+  CHANGE_FULL_NAME_INPUT        : 'SIGN_UP.CHANGE_FULL_NAME_INPUT',
+
+  START_TO_CREATE_ACCOUNT       : 'SIGN_UP.START_TO_CREATE_ACCOUNT',
+  FAILED_TO_CREATE_ACCOUNT      : 'SIGN_UP.FAILED_TO_CREATE_ACCOUNT',
+  SUCCEEDED_TO_CREATE_ACCOUNT   : 'SIGN_UP.SUCCEEDED_TO_CREATE_ACCOUNT',
+};
+
+export function changeEmailInput(email:any) {
+  return {
+    type: ACTION_TYPES.CHANGE_EMAIL_INPUT,
+    payload: {
+      email,
+    },
+  };
+}
+
+export function changePasswordInput(password:any) {
+  return {
+    type: ACTION_TYPES.CHANGE_PASSWORD_INPUT,
+    payload: {
+      password,
+    },
+  };
+}
+
+export function changeRepeatPasswordInput(password:any) {
+  return {
+    type: ACTION_TYPES.CHANGE_REPEAT_PASSWORD_INPUT,
+    payload: {
+      password,
+    },
+  };
+}
+
+export function changeFullNameInput(fullName:string) {
+  return {
+    type: ACTION_TYPES.CHANGE_FULL_NAME_INPUT,
+    payload: {
+      fullName,
+    },
+  };
+}
+export function createNewAccount(userInfo:Map<string,any>) {
+  return async (dispatch: Dispatch<any>) => { // TODO: Change any to specific type
+    dispatch({
+      type: ACTION_TYPES.START_TO_CREATE_ACCOUNT,
+    });
+    try {
+      console.log('userInfo is ', userInfo);
+      // const user = toJS(userInfo);
+      const user = userInfo;
+      dispatch({
+      type: ACTION_TYPES.SUCCEEDED_TO_CREATE_ACCOUNT,
+        payload: {
+          user,
+        },
+      });
+    } catch (err) {
+      dispatch({
+        type: ACTION_TYPES.FAILED_TO_CREATE_ACCOUNT,
+      });
+    }
+  };
+}
--- app/reducers/index.tsx
+++ app/reducers/index.tsx
@@ -1,12 +1,25 @@
 import * as Redux from "redux";
 import { routerReducer } from "react-router-redux";
+import * as signUpReducer from "../components/auth/signUp/reducer";
 
 export interface IAppState {
   routing?: any;
+  signUp: signUpReducer.ISignUpStateManager;
 }
 
-export const initialState: IAppState = {};
+export const initialState: IAppState = {
+  signUp: signUpReducer.SIGN_UP_INITIAL_STATE,
+};
+
+export interface IStateManager<T, S> {
+  get(key: "meta"): T;
+  get(key: "data"): S;
+
+  set(key: "meta", value: T): IStateManager<T, S>;
+  set(key: "data", value: S): IStateManager<T, S>;
+}
 
 export const rootReducer = Redux.combineReducers({
   routing: routerReducer,
+  signUp: signUpReducer.reducer,
 });
