d23242eaf1f4c8791d9af01a43b3cf191259d2d2
Tylor Shin <shincode@icloud.com>
2017-09-09 16:26:08 +0900

    Change Sign in action logics more Redux & React way

--- app/components/auth/signIn/actions.tsx
+++ app/components/auth/signIn/actions.tsx
@@ -1,54 +1,49 @@
 // import { toJS } from "immutable";
 import { Dispatch } from "redux";
-import apiHelper from '../../../helpers/apiHelper';
-export const ACTION_TYPES = {
-  CHANGE_EMAIL_INPUT            : 'SIGN_IN.CHANGE_EMAIL_INPUT',
-  CHANGE_PASSWORD_INPUT         : 'SIGN_IN.CHANGE_PASSWORD_INPUT',
+import apiHelper from "../../../helpers/apiHelper";
+import { ACTION_TYPES } from "../../../actions/actionTypes";
 
-  START_TO_SIGN_IN       : 'SIGN_IN.START_TO_SIGN_IN',
-  FAILED_TO_SIGN_IN      : 'SIGN_IN.FAILED_TO_SIGN_IN',
-  SUCCEEDED_TO_SIGN_IN   : 'SIGN_IN.SUCCEEDED_TO_SIGN_IN',
-};
-
-export function changeEmailInput(email:any) {
+export function changeEmailInput(email: string) {
   return {
-    type: ACTION_TYPES.CHANGE_EMAIL_INPUT,
+    type: ACTION_TYPES.SIGN_IN_CHANGE_EMAIL_INPUT,
     payload: {
       email,
     },
   };
 }
 
-export function changePasswordInput(password:any) {
+export function changePasswordInput(password: string) {
   return {
-    type: ACTION_TYPES.CHANGE_PASSWORD_INPUT,
+    type: ACTION_TYPES.SIGN_IN_CHANGE_PASSWORD_INPUT,
     payload: {
       password,
     },
   };
 }
 
-export function signIn(userInfo:Map<string,any>) {
-  return async (dispatch: Dispatch<any>) => { // TODO: Change any to specific type
+interface ISignInParams {
+  email: string;
+  password: string;
+}
+
+export function signIn(params: ISignInParams) {
+  return async (dispatch: Dispatch<Function>) => {
     dispatch({
-      type: ACTION_TYPES.START_TO_SIGN_IN,
+      type: ACTION_TYPES.SIGN_IN_START_TO_SIGN_IN,
     });
+
     try {
-      console.log('userInfo is ', userInfo);
-      // const user = toJS(userInfo);
-      const user = userInfo;
-      const result = await apiHelper.signIn({
-        password: user.get('password'),
-        email: user.get('email'),
+      await apiHelper.signIn({
+        email: params.email,
+        password: params.password,
       });
-      console.log('result is ', result);
+
       dispatch({
-        type: ACTION_TYPES.SUCCEEDED_TO_SIGN_IN,
+        type: ACTION_TYPES.SIGN_IN_SUCCEEDED_TO_SIGN_IN,
       });
     } catch (err) {
-      console.log('err is ', err);
       dispatch({
-        type: ACTION_TYPES.FAILED_TO_SIGN_IN,
+        type: ACTION_TYPES.SIGN_IN_FAILED_TO_SIGN_IN,
       });
     }
   };
