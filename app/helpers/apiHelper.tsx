0ef41ab3bcc52cadf8470508017773a0afc79a62
Tylor Shin <shincode@icloud.com>
2017-09-10 15:15:04 +0900

    Add sign in actions specs

--- app/helpers/apiHelper.tsx
+++ app/helpers/apiHelper.tsx
@@ -1,41 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
 class APIHelper {
-  private targetUrl:string = 'http://localhost:8080';
+  private targetUrl: string = "http://localhost:8080";
 
-  signUp (userInfo:any) {
+  signUp(userInfo: any) {
     return new Promise((resolve, reject) => {
-      const path = 'members';
+      const path = "members";
       const finalUrl = `${this.targetUrl}/${path}`;
       const paramObj = {
         member_id: userInfo.fullName,
         password: userInfo.password,
         email: userInfo.email,
-      }
-      console.log('paramObj is ', paramObj);
-      axios.post(finalUrl, paramObj).then(result => {
-        resolve(result);
-      }).catch(err => {
-        reject(err);
-      });
+      };
+      console.log("paramObj is ", paramObj);
+      axios
+        .post(finalUrl, paramObj)
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
     });
   }
 
-  signIn (userInfo:any) {
+  signIn(userInfo: any) {
     return new Promise((resolve, reject) => {
-      const path = 'members';
+      const path = "members";
       const finalUrl = `${this.targetUrl}/${path}`;
       const paramObj = {
         member_id: userInfo.fullName,
         password: userInfo.password,
         email: userInfo.email,
-      }
-      console.log('paramObj is ', paramObj);
-      axios.post(finalUrl, paramObj).then(result => {
-        resolve(result);
-      }).catch(err => {
-        reject(err);
-      });
+      };
+      console.log("paramObj is ", paramObj);
+      axios
+        .post(finalUrl, paramObj)
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          reject(err);
+        });
     });
   }
 }
