64d48bb9a5d258160a30d29aebc6c70598483855
Tylor Shin <shincode@icloud.com>
2017-09-10 19:40:34 +0900

    Add sign up records spec

--- app/components/auth/signUp/records.tsx
+++ app/components/auth/signUp/records.tsx
@@ -2,7 +2,6 @@ import { TypedRecord, makeTypedFactory } from "typed-immutable-record";
 
 export interface ISignUpState {
   isLoading: boolean;
-  isFailed: boolean;
   hasError: boolean;
   email: string;
   password: string;
@@ -14,7 +13,6 @@ export interface ISignUpStateRecord extends TypedRecord<ISignUpStateRecord>, ISi
 
 const initialSignInState = {
   isLoading: false,
-  isFailed: false,
   hasError: false,
   email: "",
   password: "",
