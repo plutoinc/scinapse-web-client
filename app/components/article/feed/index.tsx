d826c2fab160a51ae74365663eaca52dac5ca111
academey <academey@gmail.com>
2017-09-16 16:55:00 +0900

    Add Default Dialog to Feed

--- app/components/article/feed/index.tsx
+++ app/components/article/feed/index.tsx
@@ -1,41 +1,49 @@
 import * as React from "react";
-// import Modal from "react-modal";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { IArticleFeedStateRecord } from "./records";
 import * as Actions from "./actions";
 import { IAppState } from "../../../reducers";
+import Dialog from "material-ui/Dialog";
+import SignIn from "../../auth/signIn";
+const styles = require("./feed.scss");
 
 export interface IArticleFeedContainerProps {
   dispatch: Dispatch<any>;
-  ArticleFeedState: IArticleFeedStateRecord;
+  articleFeedState: IArticleFeedStateRecord;
 }
 
 function mapStateToProps(state: IAppState) {
   return {
-    ArticleFeedState: state.articleFeed
+    articleFeedState: state.articleFeed,
   };
 }
 
-class ArticleFeed extends React.PureComponent<
-  IArticleFeedContainerProps,
-  null
-> {
+class ArticleFeed extends React.PureComponent<IArticleFeedContainerProps, null> {
   private toggleModal = () => {
     const { dispatch } = this.props;
     dispatch(Actions.toggleModal());
   };
   render() {
+    const { articleFeedState } = this.props;
     return (
       <div>
         <h1>Article Feed</h1>
         <h1>Article Feed</h1>
         <h1>Article Feed</h1>
         <h1>Article Feed</h1>
+        <h1>Article Fe,ed</h1>
         <h1>Article Feed</h1>
         <h1>Article Feed</h1>
-        <h1>Article Feed</h1>
-        <button onClick={this.toggleModal} />
+        <Dialog
+          open={articleFeedState.isModalOpen}
+          modal={false}
+          onRequestClose={this.toggleModal}
+          bodyStyle={styles.dialogContent}
+        >
+          <SignIn />
+        </Dialog>
+        <button onClick={this.toggleModal}>Open Dialog</button>
       </div>
     );
   }
