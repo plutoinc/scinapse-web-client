00a5bfdb474793548207a8b3a7ed26a54b69268f
academey <academey@gmail.com>
2017-09-16 18:14:20 +0900

    Change structure for using common dialog component

--- app/components/article/feed/actions.tsx
+++
@@ -1,7 +0,0 @@
-import { ACTION_TYPES } from "../../../actions/actionTypes";
-
-export function toggleModal() {
-  return {
-    type: ACTION_TYPES.ARTICLE_FEED_TOGGLE_MODAL
-  };
-}
--- app/components/article/feed/records.tsx
+++ app/components/article/feed/records.tsx
@@ -3,22 +3,17 @@ import { TypedRecord, makeTypedFactory } from "typed-immutable-record";
 export interface IArticleFeedState {
   isLoading: boolean;
   hasError: boolean;
-  isModalOpen: boolean;
 }
 
-export interface IArticleFeedStateRecord
-  extends TypedRecord<IArticleFeedStateRecord>,
-    IArticleFeedState {}
+export interface IArticleFeedStateRecord extends TypedRecord<IArticleFeedStateRecord>, IArticleFeedState {}
 
 const initialArticleFeedState = {
   isLoading: false,
   hasError: false,
-  isModalOpen: false
 };
 
-export const ArticleFeedStateFactory = makeTypedFactory<
-  IArticleFeedState,
-  IArticleFeedStateRecord
->(initialArticleFeedState);
+export const ArticleFeedStateFactory = makeTypedFactory<IArticleFeedState, IArticleFeedStateRecord>(
+  initialArticleFeedState,
+);
 
 export const ARTICLE_FEED_INITIAL_STATE = ArticleFeedStateFactory();
