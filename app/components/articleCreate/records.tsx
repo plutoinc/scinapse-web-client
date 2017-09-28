db2a6982e821390dda6898a320711314392f0448
academey <academey@gmail.com>
2017-09-28 14:30:28 +0900

    Add Article Category Btn

--- app/components/articleCreate/records.tsx
+++ app/components/articleCreate/records.tsx
@@ -4,6 +4,8 @@ export interface IArticleCreateState {
   isLoading: boolean;
   hasError: boolean;
   currentStep: ARTICLE_CREATE_STEP;
+  isArticleCategoryDropDownOpen: boolean;
+  articleCategory: ARTICLE_CATEGORY | null;
 }
 
 export interface IArticleCreateStateRecord extends TypedRecord<IArticleCreateStateRecord>, IArticleCreateState {}
@@ -13,11 +15,14 @@ export enum ARTICLE_CREATE_STEP {
   SECOND,
   FINAL,
 }
+export type ARTICLE_CATEGORY = "Post Paper" | "Pre Paper" | "White Paper" | "Tech Blog";
 
 const initialArticleCreateState: IArticleCreateState = {
   isLoading: false,
   hasError: false,
   currentStep: ARTICLE_CREATE_STEP.FIRST,
+  isArticleCategoryDropDownOpen: false,
+  articleCategory: null,
 };
 
 export const ArticleCreateFactory = makeTypedFactory<IArticleCreateState, IArticleCreateStateRecord>(
