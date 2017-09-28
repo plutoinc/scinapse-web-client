a2df1741ed1f00a434ea61e525c1a8cea9293b53
Tylor Shin <shincode@icloud.com>
2017-09-28 11:19:01 +0900

    Upgrade & fix broken jest setting caused by React v16 upgrade (#44)

--- app/__tests__/preload.tsx
+++ app/__tests__/preload.tsx
@@ -1,3 +1,8 @@
+const Enzyme = require("enzyme");
+const Adapter = require("enzyme-adapter-react-16");
+
+Enzyme.configure({ adapter: new Adapter() });
+
 Object.defineProperty(window.location, "host", {
   writable: true,
 });
