865ce365b54a39735d2a178f1cfe8cb75ef962b1
Tylor Shin <shincode@icloud.com>
2017-10-06 12:02:09 +0900

    Change error tracking logic following by React 16's way (#48)

--- app/api/pluto.tsx
+++ app/api/pluto.tsx
@@ -1,27 +1,18 @@
 import axios, { AxiosRequestConfig } from "axios";
 import getAPIHost from "./getHost";
-import { logException } from "../helpers/errorHandler";
 
 export default class PlutoAxios {
   protected instance = axios.create({
     baseURL: getAPIHost(),
     withCredentials: true,
-    timeout: 1000
+    timeout: 1000,
   });
 
   protected get(path: string, config?: AxiosRequestConfig) {
-    try {
-      return this.instance.get(path, config);
-    } catch (err) {
-      logException(err);
-    }
+    return this.instance.get(path, config);
   }
 
   protected post(path: string, data?: any, config?: AxiosRequestConfig) {
-    try {
-      return this.instance.post(path, data, config);
-    } catch (err) {
-      logException(err);
-    }
+    return this.instance.post(path, data, config);
   }
 }
