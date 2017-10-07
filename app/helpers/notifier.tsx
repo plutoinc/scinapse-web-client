c8ae8ffac1f9fffe1a2d768bda9368a08bfccdd2
Tylor Shin <shincode@icloud.com>
2017-10-07 13:06:47 +0900

    Clean up notification code & apply it to article show error handling logic

--- app/helpers/notifier.tsx
+++ app/helpers/notifier.tsx
@@ -1,10 +1,29 @@
-import { Middleware } from "redux";
 import * as toastr from "toastr";
+import { Middleware } from "redux";
 import { ACTION_TYPES } from "../actions/actionTypes";
 
+export interface INotificationAction {
+  type: Symbol;
+  payload: {
+    type: ToastrType;
+    message: string;
+    title?: string;
+    options?: ToastrOptions;
+  };
+}
+
 const ReduxNotifier: Middleware = _store => next => (action: any) => {
-  if (action.type === ACTION_TYPES.GLOBAL_LOCATION_CHANGE) {
-    toastr.success("We do have the Kapua suite available.", "Turtle Bay Resort", { timeOut: 5000 });
+  if (action.type === ACTION_TYPES.GLOBAL_ALERT_NOTIFICATION) {
+    const notificationAction: INotificationAction = action;
+    const notificationOptions = { ...{ timeOut: 4000 }, ...notificationAction.payload.options };
+
+    toastr[notificationAction.payload.type](
+      notificationAction.payload.message,
+      notificationAction.payload.title,
+      notificationOptions,
+    );
+  } else if (action.type === ACTION_TYPES.GLOBAL_CLEAR_NOTIFICATION) {
+    toastr.clear();
   } else {
     return next(action);
   }
