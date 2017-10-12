353f4d75801730bc06085c0efbd7b7f48c6cf313
Tylor Shin <shincode@icloud.com>
2017-10-12 18:43:06 +0900

    Add ArticleNote component (#64)

---
+++ app/components/articleShow/components/note.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Icon from "../../../icons";
+const styles = require("./note.scss");
+
+interface IArticleNote {
+  note?: string;
+}
+
+const ArticleNote = ({ note }: IArticleNote) => {
+  if (!note) {
+    return null;
+  }
+
+  return (
+    <div className={styles.evaluateNoteWrapper}>
+      <div className={styles.noteIconWrapper}>
+        <div className={styles.noteIconContent}>Note to evaluators</div>
+        <Icon className={styles.noteIcon} icon="NOTE_TO_EVALUATE" />
+      </div>
+      <div className={styles.noteContent}>{note}</div>
+    </div>
+  );
+};
+
+export default ArticleNote;
