8a7adda6e0040285442c98ef9c979e02609f51b1
academey <academey@gmail.com>
2017-10-30 14:42:55 +0900

    Fix some article show styling

--- app/components/articleShow/components/articleInfo.tsx
+++ app/components/articleShow/components/articleInfo.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { IMemberRecord } from "../../../model/member";
+import { Link } from "react-router-dom";
 const styles = require("./articleInfo.scss");
 
 export interface IArticleInfoProps {
@@ -12,7 +13,9 @@ const ArticleInfo = (props: IArticleInfoProps) => {
   return (
     <div className={styles.articleInfoWrapper}>
       <span className={styles.articleInfoItem}>from {props.from}</span>
-      <span className={styles.articleInfoItem}>posted by {props.createdBy.name}</span>
+      <span className={styles.articleInfoItem}>
+        posted by <Link to={`/users/${props.createdBy.id}`}>{props.createdBy.name}</Link>
+      </span>
       <span className={styles.articleInfoItem}>posted at {props.createdAt}</span>
     </div>
   );
