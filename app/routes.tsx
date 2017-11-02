c5ab160881c7f677a3662b64ba417bd9ae407988
academey <academey@gmail.com>
2017-11-02 18:53:50 +0900

    Add footer's upper div for space (#121)

--- app/routes.tsx
+++ app/routes.tsx
@@ -26,6 +26,12 @@ const routesMap = (
       <Route path="/:errorNum" component={ErrorPage} />
     </Switch>
     <DialogComponent />
+    <div
+      style={{
+        // for footer upper space
+        height: 20,
+      }}
+    />
     <Footer />
   </div>
 );
