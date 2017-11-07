80bd8d977e030722536f6fd99b1ac407b74bf88f
academey <academey@gmail.com>
2017-11-07 16:07:22 +0900

    Change peer Evaluation

--- app/components/layouts/header.tsx
+++ app/components/layouts/header.tsx
@@ -11,6 +11,7 @@ import { ILayoutStateRecord } from "./records";
 import * as Actions from "./actions";
 import { openSignIn, openSignUp } from "../dialog/actions";
 import UserProfileIcon from "../common/userProfileIcon";
+import Popover from "material-ui/Popover/Popover";
 
 const styles = require("./header.scss");
 const HEADER_BACKGROUND_START_HEIGHT = 10;
@@ -233,6 +234,51 @@ class Header extends React.PureComponent<IHeaderProps, IHeaderStates> {
             </li>
           </ul>
           {this.getHeaderButton()}
+          <Popover
+            open={isCategoryPopOverOpen && !!props.categoryPopoverAnchorElement}
+            anchorEl={props.categoryPopoverAnchorElement}
+            anchorOrigin={{ horizontal: "middle", vertical: "bottom" }}
+            targetOrigin={{ horizontal: "middle", vertical: "top" }}
+            onRequestClose={props.handleCloseCategoryPopover}
+            style={{
+              borderRadius: "10px",
+              boxShadow: "none",
+              border: "none",
+            }}
+          >
+            <div className={styles.popoverMenuWrapper}>
+              <CategoryItem
+                currentCategory={props.currentCategory}
+                handleChangeCategory={props.handleChangeCategory}
+                type={FEED_CATEGORIES.ALL}
+                text="All"
+              />
+              <CategoryItem
+                currentCategory={props.currentCategory}
+                handleChangeCategory={props.handleChangeCategory}
+                type={FEED_CATEGORIES.POST_PAPER}
+                text="Post Paper"
+              />
+              <CategoryItem
+                currentCategory={props.currentCategory}
+                handleChangeCategory={props.handleChangeCategory}
+                type={FEED_CATEGORIES.PRE_PAPER}
+                text="Pre Paper"
+              />
+              <CategoryItem
+                currentCategory={props.currentCategory}
+                handleChangeCategory={props.handleChangeCategory}
+                type={FEED_CATEGORIES.TECH_WHITE_PAPER}
+                text="Tech Whitepaper"
+              />
+              <CategoryItem
+                currentCategory={props.currentCategory}
+                handleChangeCategory={props.handleChangeCategory}
+                type={FEED_CATEGORIES.TECH_BLOG}
+                text="Tech Blog"
+              />
+            </div>
+          </Popover>
         </div>
       </nav>
     );
