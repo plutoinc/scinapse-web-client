c25797f300d60c5a4f85f4ef22a495c223d2b7ab
Tylor Shin <shincode@icloud.com>
2017-11-27 14:03:06 +0900

    Add waiting 10s code after upload source files to s3 in deploy process

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -1,13 +1,21 @@
 import * as fs from "fs";
 import pushToS3 from "./helpers/pushToS3";
 import copyJsToRoot from "./helpers/copyJsToRoot";
+import { setTimeout } from "timers";
 
 async function deploy() {
   const NEW_TAG: string = new Date().toISOString().replace(/:/g, "-");
 
   fs.writeFileSync("./version", NEW_TAG);
 
+  console.log(`THE CURRENT VERSION IS === ${NEW_TAG}`);
+
   await pushToS3(NEW_TAG);
+  await new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
   await copyJsToRoot(NEW_TAG);
 }
 
