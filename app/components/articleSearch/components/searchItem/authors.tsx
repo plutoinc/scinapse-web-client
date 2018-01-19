b882d6613e39d8bc8561a6e493c16066c775351c
academey <academey@gmail.com>
2018-01-19 13:38:48 +0900

    Fix wrong Logic of authors, searchQueryHighlightedContent

--- app/components/articleSearch/components/searchItem/authors.tsx
+++ app/components/articleSearch/components/searchItem/authors.tsx
@@ -17,11 +17,11 @@ const Authors = (props: IAuthorsProps) => {
   const isAuthorsSameLessThanMinimumShowingAuthorNumber = authors.size <= MINIMUM_SHOWING_AUTHOR_NUMBER;
 
   if (isAuthorsSameLessThanMinimumShowingAuthorNumber) {
-    const authorItems = mapAuthorNodeToLastIndex(authors, authors.size - 1);
+    const authorItems = mapAuthorNodeToEndIndex(authors, authors.size - 1);
 
     return <span className={styles.authors}>{authorItems}</span>;
   } else if (!isAuthorsOpen) {
-    const authorItems = mapAuthorNodeToLastIndex(authors, MINIMUM_SHOWING_AUTHOR_NUMBER);
+    const authorItems = mapAuthorNodeToEndIndex(authors, MINIMUM_SHOWING_AUTHOR_NUMBER - 1);
 
     return (
       <span className={styles.authors}>
@@ -32,7 +32,7 @@ const Authors = (props: IAuthorsProps) => {
       </span>
     );
   } else {
-    const authorItems = mapAuthorNodeToLastIndex(authors, authors.size - 1);
+    const authorItems = mapAuthorNodeToEndIndex(authors, authors.size - 1);
 
     return (
       <span className={styles.authors}>
@@ -74,16 +74,16 @@ function getAuthorOrganization(organization: string) {
   }
 }
 
-function mapAuthorNodeToLastIndex(authors: List<IAuthorRecord>, lastIndex: number) {
-  return authors.slice(0, lastIndex).map((author, index) => {
-    const isLastAuthor = index !== authors.size - 1;
+function mapAuthorNodeToEndIndex(authors: List<IAuthorRecord>, endIndex: number) {
+  return authors.slice(0, endIndex + 1).map((author, index) => {
+    const isLastAuthor = index === endIndex;
 
     return (
       <span className={styles.author} key={`author_${index}`}>
         <span className={styles.authorName}>{`${author.name} `}</span>
         {getHIndexTooltip(author.hIndex)}
         {getAuthorOrganization(author.organization)}
-        {isLastAuthor ? <span className={styles.authorName}>{`, `}</span> : null}
+        {!isLastAuthor ? <span className={styles.authorName}>{`, `}</span> : null}
       </span>
     );
   });
