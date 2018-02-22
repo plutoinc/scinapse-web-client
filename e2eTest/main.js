468b9d4328e859f295fb0aa40628afb79a48a203
Tylor Shin <shincode@icloud.com>
2018-02-22 18:40:22 +0900

    Fix broken e2e test (#231)

--- e2eTest/main.js
+++ e2eTest/main.js
@@ -2,7 +2,7 @@ describe("Pluto Main Page", function() {
   it("should render proper page", function(browser) {
     browser
       .url("https://search.pluto.network")
-      .expect.element("[placeholder='Search papers']")
+      .expect.element("[placeholder='Search papers by title, author, doi or keyword']")
       .to.be.present.before(3000);
 
     browser.assert.title("Pluto Beta | Academic discovery");
