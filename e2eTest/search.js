985f447ed6a69cbfbcd64c8321fd0e0c29cee789
Tylor Shin <shincode@icloud.com>
2018-02-22 18:34:26 +0900

    Fix broken e2e test

--- e2eTest/search.js
+++ e2eTest/search.js
@@ -2,7 +2,7 @@ describe("Pluto Search Feature works", function() {
   it("should render search result", function(browser) {
     browser
       .url("https://search.pluto.network")
-      .expect.element("[placeholder='Search papers']")
+      .expect.element("[placeholder='Search papers by title, author, doi or keyword']")
       .to.be.present.before(3000);
 
     browser.setValue("[placeholder='Search papers']", ["paper", browser.Keys.ENTER]).pause(3000);
