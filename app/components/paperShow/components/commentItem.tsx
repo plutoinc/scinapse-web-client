c0430238b7bff4dbf9358c72c1c1e177659a7dfd
Tylor Shin <shincode@icloud.com>
2018-03-14 17:08:34 +0900

    Make ellipsis button be shown only proper user existing

--- app/components/paperShow/components/commentItem.tsx
+++ app/components/paperShow/components/commentItem.tsx
@@ -12,34 +12,44 @@ interface PaperShowCommentItemProps {
   handleDeleteComment: (comment: ICommentRecord) => void;
 }
 
-const PaperShowCommentItem = ({ comment, handleDeleteComment }: PaperShowCommentItemProps) => {
+function getMoreButton(props: PaperShowCommentItemProps) {
+  if (!props.currentUser.isLoggedIn || props.comment.createdBy.id !== props.currentUser.id) {
+    return null;
+  } else {
+    return (
+      <IconMenu
+        style={{ position: "absolute", right: "30px", top: 0, bottom: 0 }}
+        iconButtonElement={
+          <IconButton style={{ width: "inherit", height: "inherit", padding: "0", margin: "0" }}>
+            <Icon className={styles.commentMoreItemButton} icon="COMMENT_MORE_ITEM" />
+          </IconButton>
+        }
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        targetOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem
+          onClick={() => {
+            props.handleDeleteComment(props.comment);
+          }}
+          style={{
+            color: "#f54b5e",
+          }}
+          primaryText="Delete"
+        />
+      </IconMenu>
+    );
+  }
+}
+
+const PaperShowCommentItem = (props: PaperShowCommentItemProps) => {
   return (
     <div className={styles.commentItemWrapper}>
       <div className={styles.authorInformationBox}>
-        <span className={styles.authorName}>{comment.createdBy.name}</span>
-        <span className={styles.authorAffiliation}>{comment.createdBy.affiliation}</span>
-        <IconMenu
-          style={{ position: "absolute", right: "30px", top: 0, bottom: 0 }}
-          iconButtonElement={
-            <IconButton style={{ width: "inherit", height: "inherit", padding: "0", margin: "0" }}>
-              <Icon className={styles.commentMoreItemButton} icon="COMMENT_MORE_ITEM" />
-            </IconButton>
-          }
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          targetOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-          <MenuItem
-            onClick={() => {
-              handleDeleteComment(comment);
-            }}
-            style={{
-              color: "#f54b5e",
-            }}
-            primaryText="Delete"
-          />
-        </IconMenu>
+        <span className={styles.authorName}>{props.comment.createdBy.name}</span>
+        <span className={styles.authorAffiliation}>{props.comment.createdBy.affiliation}</span>
+        {getMoreButton(props)}
       </div>
-      <div className={styles.contentBox}>{comment.comment}</div>
+      <div className={styles.contentBox}>{props.comment.comment}</div>
     </div>
   );
 };
