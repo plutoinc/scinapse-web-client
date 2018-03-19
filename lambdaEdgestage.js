5312a87f0730a47efcce83b0406f3b1ae942bace
Tylor Shin <shincode@icloud.com>
2018-03-19 14:08:54 +0900

    Add CustomErrorResponse logic for 0 cache TTL && Add DNS_ALIAS for scinapse domains && fix wrong edge lambda function logics

---
+++ lambdaEdgestage.js
@@ -0,0 +1,29 @@
+"use strict";
+
+exports.handler = (event, context, callback) => {
+  const HOST = "https://stage.scinapse.io";
+  const request = event.Records[0].cf.request;
+  const requestHost = request.headers["host"][0].value;
+
+  let response;
+  if (requestHost === "stage.scinapse.io") {
+    response = request;
+  } else {
+    const targetLocation = request.querystring ? `${HOST}${request.uri}?${request.querystring}` : HOST + request.uri;
+
+    response = {
+      status: "302",
+      statusDescription: "Found",
+      headers: {
+        location: [
+          {
+            key: "Location",
+            value: targetLocation,
+          },
+        ],
+      },
+    };
+  }
+
+  callback(null, response);
+};
