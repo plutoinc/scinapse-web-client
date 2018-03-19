021e1883acbe315a011827c90f7b6a46f148f72c
Tylor Shin <shincode@icloud.com>
2018-03-19 14:47:46 +0900

    Add LambdaFunctionAssociations field after making Lambda function

--- lambdaEdgeprod.js
+++ lambdaEdgeprod.js
@@ -1,12 +1,12 @@
 "use strict";
 
 exports.handler = (event, context, callback) => {
-  const HOST = "https://scinapse.io";
+  const HOST = "https://search.pluto.network";
   const request = event.Records[0].cf.request;
   const requestHost = request.headers["host"][0].value;
 
   let response;
-  if (requestHost === "scinapse.io") {
+  if (requestHost === "search.pluto.network") {
     response = request;
   } else {
     const targetLocation = request.querystring ? `${HOST}${request.uri}?${request.querystring}` : HOST + request.uri;
