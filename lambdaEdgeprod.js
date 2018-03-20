f7ce4c5eca2f42c4d19ff8245178866569e2bf53
Tylor Shin <shincode@icloud.com>
2018-03-20 11:09:00 +0900

    Prevent production scinapse domain redirect

--- lambdaEdgeprod.js
+++ lambdaEdgeprod.js
@@ -1,29 +1,29 @@
 "use strict";
 
 exports.handler = (event, context, callback) => {
-  const HOST = "https://scinapse.io";
+  const HOST = "https://search.pluto.network";
   const request = event.Records[0].cf.request;
   const requestHost = request.headers["host"][0].value;
 
   let response;
-  if (requestHost === "scinapse.io") {
-    response = request;
-  } else {
-    const targetLocation = request.querystring ? `${HOST}${request.uri}?${request.querystring}` : HOST + request.uri;
+  // if (requestHost === "search.pluto.network") {
+  response = request;
+  // } else {
+  //   const targetLocation = request.querystring ? `${HOST}${request.uri}?${request.querystring}` : HOST + request.uri;
 
-    response = {
-      status: "302",
-      statusDescription: "Found",
-      headers: {
-        location: [
-          {
-            key: "Location",
-            value: targetLocation,
-          },
-        ],
-      },
-    };
-  }
+  //   response = {
+  //     status: "302",
+  //     statusDescription: "Found",
+  //     headers: {
+  //       location: [
+  //         {
+  //           key: "Location",
+  //           value: targetLocation,
+  //         },
+  //       ],
+  //     },
+  //   };
+  // }
 
   callback(null, response);
 };
