0138931f1a74d87cb78ca8609daceba2bab86f93
Tylor Shin <shincode@icloud.com>
2018-04-04 15:26:49 +0900

    Try isbot at Lambda@edge

--- lambdaEdgestage.js
+++ lambdaEdgestage.js
@@ -1,9 +1,14 @@
 "use strict";
 
+const isBot = require("isbot");
+
 exports.handler = (event, context, callback) => {
   const HOST = "https://stage.scinapse.io";
   const request = event.Records[0].cf.request;
   const requestHost = request.headers["host"][0].value;
+  const userAgent = request.headers["user-agent"][0].value;
+  console.log(userAgent);
+  console.log(isBot(userAgent));
 
   let response;
   if (requestHost === "stage.scinapse.io") {
