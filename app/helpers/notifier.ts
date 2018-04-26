008214246116b99a2fd89f84404612d8a8866d16
Tylor Shin <shincode@icloud.com>
2018-04-26 14:43:25 +0900

    Change alert library from Toastr to Notie for removing JQuery dependency (#314)

--- app/helpers/notifier.ts
+++ app/helpers/notifier.ts
@@ -1,37 +1,48 @@
-import * as toastr from "toastr";
 import { Middleware } from "redux";
 import { ACTION_TYPES } from "../actions/actionTypes";
+const notie = require("notie");
 
 export interface NotificationAction {
   type: symbol;
   payload: NotificationActionPayload;
 }
 
-export interface NotificationActionPayload {
-  type: ToastrType;
-  message: string;
-  title?: string;
-  options?: ToastrOptions;
+type NotieAlertTypes = "success" | "warning" | "error" | "info" | "neutral";
+
+export interface NotieAlertOptions {
+  text: string;
+  stay: boolean; // default = false
+  time: number; // default = 3, minimum = 1,
+  position: "top" | "bottom"; // default = 'top', enum: ['top', 'bottom']
 }
 
-const defaultToastrOptions = {
-  positionClass: "toast-bottom-center",
-  closeHtml: `<button class="pluto-toastr-close-button">Confirm</button>`,
-  timeOut: 4000,
+export interface NotificationActionPayload {
+  type: NotieAlertTypes;
+  message: string;
+  title?: string;
+  options?: NotieAlertOptions;
+}
+
+const defaultNotieOptions = {
+  time: 4,
+  position: "bottom",
+  stay: false,
 };
 
 const ReduxNotifier: Middleware = _store => next => (action: any) => {
   if (action.type === ACTION_TYPES.GLOBAL_ALERT_NOTIFICATION) {
     const notificationAction: NotificationAction = action;
-    const notificationOptions = { ...defaultToastrOptions, ...notificationAction.payload.options };
+    const notificationOptions = { ...defaultNotieOptions, ...notificationAction.payload.options };
 
-    toastr[notificationAction.payload.type](
-      notificationAction.payload.message,
-      notificationAction.payload.title,
-      notificationOptions,
-    );
+    notie.alert({
+      type: notificationAction.payload.type,
+      text: notificationAction.payload.message,
+      stay: notificationOptions.stay,
+      time: notificationOptions.time,
+      position: notificationOptions.position,
+    });
   } else if (action.type === ACTION_TYPES.GLOBAL_CLEAR_NOTIFICATION) {
-    toastr.clear();
+    notie.hideAlerts();
   } else {
     return next(action);
   }
