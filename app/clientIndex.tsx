df802e99ed3d2a481ecfe3cf501e8d2c008c000b
Tylor Shin <shincode@icloud.com>
2018-05-07 15:20:20 +0900

    Add preventing IE/Edge's clicking SVG problem logic

--- app/clientIndex.tsx
+++ app/clientIndex.tsx
@@ -2,6 +2,12 @@ import EnvChecker from "./helpers/envChecker";
 import PlutoRenderer from "./client";
 
 if (!EnvChecker.isServer()) {
+  // Prevent IE/Edge's Clicking SVG problem
+  // https://stackoverflow.com/questions/38648307/add-blur-method-to-svg-elements-in-ie-edge
+  if (typeof (SVGElement.prototype as any).blur === "undefined") {
+    // tslint:disable-next-line:no-empty
+    (SVGElement.prototype as any).blur = () => {};
+  }
   const plutoRenderer = new PlutoRenderer();
   plutoRenderer.renderPlutoApp();
 }
