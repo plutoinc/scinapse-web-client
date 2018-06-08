bec0cc8ddd2df4b8c89cd8edc1395041d10bdd0b
Tylor Shin <shincode@icloud.com>
2018-06-08 12:45:14 +0900

    Chagne the way of making instance from variable to function to make new instance per each request (#354)

--- app/api/pluto.ts
+++ app/api/pluto.ts
@@ -5,37 +5,39 @@ import EnvChecker from "../helpers/envChecker";
 export const TIMEOUT_FOR_SAFE_RENDERING = 5000;
 
 export default class PlutoAxios {
-  protected instance = axios.create({
-    baseURL: getAPIHost(),
-    withCredentials: true,
-    timeout: EnvChecker.isServer() ? TIMEOUT_FOR_SAFE_RENDERING : 60000,
-  });
+  protected getInstance = () => {
+    return axios.create({
+      baseURL: getAPIHost(),
+      withCredentials: true,
+      timeout: EnvChecker.isServer() ? TIMEOUT_FOR_SAFE_RENDERING : 60000,
+    });
+  };
 
   protected get(path: string, config?: AxiosRequestConfig) {
     if (EnvChecker.isServer()) {
       console.log(`HTTP REQUEST - GET : ${getAPIHost() + path}`);
     }
-    return this.instance.get(path, config);
+    return this.getInstance().get(path, config);
   }
 
   protected post(path: string, data?: any, config?: AxiosRequestConfig) {
     if (EnvChecker.isServer()) {
       console.log(`HTTP REQUEST - POST : ${getAPIHost() + path}`);
     }
-    return this.instance.post(path, data, config);
+    return this.getInstance().post(path, data, config);
   }
 
   protected put(path: string, data?: any, config?: AxiosRequestConfig) {
     if (EnvChecker.isServer()) {
       console.log(`HTTP REQUEST - PUT : ${getAPIHost() + path}`);
     }
-    return this.instance.put(path, data, config);
+    return this.getInstance().put(path, data, config);
   }
 
   protected delete(path: string, config?: AxiosRequestConfig) {
     if (EnvChecker.isServer()) {
       console.log(`HTTP REQUEST - DELETE : ${getAPIHost() + path}`);
     }
-    return this.instance.delete(path, config);
+    return this.getInstance().delete(path, config);
   }
 }
