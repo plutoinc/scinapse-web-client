5a4ddb514b40526380688cc587d7aaae6ccd5e59
Tylor Shin <shincode@icloud.com>
2018-06-14 12:56:31 +0900

    Add CloudWatch log at server-side render & Standardized rendering log (#361)
    * Add CloudWatch Put-Metric logic to track rendering && Standardized rendering methods
    
    * Logging error for test cloudwatch
    
    * Add CluodWatchMetricRole to serverless.yml

--- localServer/index.tsx
+++ localServer/index.tsx
@@ -8,51 +8,71 @@ const server = express();
 
 console.log("START SERVER");
 
-server.disable("x-powered-by").get("/*", async (req: express.Request, res: express.Response) => {
-  console.log(`Get request for ${req.url}`);
+server
+  .disable("x-powered-by")
+  .get("/*", async (req: express.Request, res: express.Response) => {
+    let succeededToServerRendering = false;
+    console.log(`Get request for ${req.url}`);
 
-  const mockUserAgent =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36";
+    const normalRender = async () => {
+      const resultHTML = await serverSideRender({
+        requestUrl: req.url,
+        scriptPath: "http://localhost:8080/bundle.js"
+      });
+      succeededToServerRendering = true;
 
-  const normalRender = async () => {
-    const resultHTML = await serverSideRender({
-      requestUrl: req.url,
-      scriptPath: "http://localhost:8080/bundle.js",
-      userAgent: mockUserAgent,
+      const buf = new Buffer(resultHTML);
+      if (buf.byteLength > 6291456 /* 6MB */) {
+        throw new Error("HTML SIZE IS OVER LAMBDA LIMITATION");
+      }
+
+      if (!succeededToServerRendering) {
+        console.log("============== NORMAL RENDERING FIRED! ============== ");
+      }
+
+      return resultHTML;
+    };
+
+    const safeTimeout = new Promise((resolve, _reject) => {
+      const jsOnlyHTML = renderJavaScriptOnly(
+        "http://localhost:8080/bundle.js"
+      );
+      setTimeout(
+        () => {
+          if (!succeededToServerRendering) {
+            console.log(
+              "============== FALLBACK RENDERING FIRED! =============="
+            );
+          }
+          succeededToServerRendering = false;
+          resolve(jsOnlyHTML);
+        },
+        TIMEOUT_FOR_SAFE_RENDERING,
+        jsOnlyHTML
+      );
     });
-    console.log("============== NORMAL SERVER SIDE RENDERING FIRED! ============== ");
-    const buf = new Buffer(resultHTML);
-    if (buf.byteLength > 6291456 /* 6MB */) {
-      throw new Error("HTML SIZE IS OVER LAMBDA LIMITATION");
-    }
-    return resultHTML;
-  };
 
-  const safeTimeout = new Promise((resolve, _reject) => {
-    const jsOnlyHTML = renderJavaScriptOnly("http://localhost:8080/bundle.js");
-    setTimeout(
-      () => {
-        console.log("============== SAFE RENDERING FIRED! ==============");
-        resolve(jsOnlyHTML);
-      },
-      TIMEOUT_FOR_SAFE_RENDERING,
-      jsOnlyHTML,
-    );
+    Promise.race([normalRender(), safeTimeout])
+      .then(html => {
+        res.send(html);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(
+          "============== ERROR HADNLING RENDERING FIRED! =============="
+        );
+        res.send(renderJavaScriptOnly("http://localhost:8080/bundle.js"));
+      });
   });
-
-  Promise.race([normalRender(), safeTimeout])
-    .then(html => {
-      console.log("========== Just before to send HTML string");
-      res.send(html);
-    })
-    .catch(err => {
-      console.error(err);
-      res.send(renderJavaScriptOnly("http://localhost:8080/bundle.js"));
-    });
-});
 
 const port: number = Number(process.env.PORT) || 3000;
 
 server
-  .listen(port, () => console.log(`Express server listening at ${port}! Visit https://localhost:${port}`))
-  .on("error", err => console.error("LOCAL_SERVER_ERROR =======================", err));
+  .listen(port, () =>
+    console.log(
+      `Express server listening at ${port}! Visit https://localhost:${port}`
+    )
+  )
+  .on("error", err =>
+    console.error("LOCAL_SERVER_ERROR =======================", err)
+  );
