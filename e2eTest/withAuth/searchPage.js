129c1fb9c068af93ac37c1b0bc8deda42b6b22e1
Tylor Shin <shincode@icloud.com>
2018-06-22 12:03:58 +0900

    Change E2E wating logic until JS Loading blocker is gone.

--- e2eTest/withAuth/searchPage.js
+++ e2eTest/withAuth/searchPage.js
@@ -10,11 +10,15 @@ describe("Pluto search page features along with auth user", function() {
       .url(targetUrl)
       .resizeWindow(1920, 1080)
       .waitForElementVisible("body", 3000)
-      .pause(5000)
+      .waitForElementPresent("[class^='root__jsLoaderWrapper']")
+      .waitForElementNotPresent("[class^='root__jsLoaderWrapper']")
       .click("[class^='header__signInButton']")
       .waitForElementPresent("[placeholder='E-mail']", 5000)
       .setValue("[placeholder='E-mail']", [process.env.PLUTO_TEST_ID])
-      .setValue("[placeholder='Password']", [process.env.PLUTO_TEST_PASSWORD, browser.Keys.ENTER])
+      .setValue("[placeholder='Password']", [
+        process.env.PLUTO_TEST_PASSWORD,
+        browser.Keys.ENTER
+      ])
       .saveScreenshot("./output/e2e/withAuth/searchPage/beforeLogin.png")
       .waitForElementPresent("[class^='header__userDropdownChar']", 10000)
       .waitForElementPresent("[class^='filterContainer__filterItem']", 5000)
@@ -24,13 +28,18 @@ describe("Pluto search page features along with auth user", function() {
 
   describe("when the user auth existence doesn't matter", () => {
     it("should render search result", browser => {
-      browser.expect.element("[class^='filterContainer__filterItem']").to.be.present;
-      browser.expect.element("[class^='header__headerContainer']").to.be.present;
-      browser.expect.element("[class^='searchItem__titleWrapper']").to.be.present;
-      browser.expect.element("[class^='publishInfoList__publishInfoList']").to.be.present;
+      browser.expect.element("[class^='filterContainer__filterItem']").to.be
+        .present;
+      browser.expect.element("[class^='header__headerContainer']").to.be
+        .present;
+      browser.expect.element("[class^='searchItem__titleWrapper']").to.be
+        .present;
+      browser.expect.element("[class^='publishInfoList__publishInfoList']").to
+        .be.present;
       browser.expect.element("[class^='abstract__abstract']").to.be.present;
       browser.expect.element("[class^='keywords__keywords']").to.be.present;
-      browser.expect.element("[class^='commentInput__textAreaWrapper']").to.be.present;
+      browser.expect.element("[class^='commentInput__textAreaWrapper']").to.be
+        .present;
     });
   });
 
@@ -38,17 +47,26 @@ describe("Pluto search page features along with auth user", function() {
     describe("when click the bookmark button", () => {
       after((browser, done) => {
         browser.keys([browser.Keys.ESCAPE], () => {
-          browser.saveScreenshot("./output/e2e/withAuth/searchPage/afterTurnOffVerificationDialog.png", () => {
-            done();
-          });
+          browser.saveScreenshot(
+            "./output/e2e/withAuth/searchPage/afterTurnOffVerificationDialog.png",
+            () => {
+              done();
+            }
+          );
         });
       });
 
       it("should render e-mail verification dialog", browser => {
         browser.click("[class^='infoList__bookmarkButton']", () => {
-          browser.waitForElementPresent("[class^='verificationNeeded__verificationNeededContainer']", 5000, () => {
-            browser.expect.element("[class^='verificationNeeded__verificationNeededContainer']").to.be.present;
-          });
+          browser.waitForElementPresent(
+            "[class^='verificationNeeded__verificationNeededContainer']",
+            5000,
+            () => {
+              browser.expect.element(
+                "[class^='verificationNeeded__verificationNeededContainer']"
+              ).to.be.present;
+            }
+          );
         });
       });
     });
