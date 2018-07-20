34fc454945894c51a15e658fe304b853d5c498e6
Tylor Shin <shincode@icloud.com>
2018-07-20 13:32:05 +0900

    Adding delete already existing bubdle directory logic

--- scripts/deploy/helpers/pushToS3.ts
+++ scripts/deploy/helpers/pushToS3.ts
@@ -1,20 +1,28 @@
 import * as DeployConfig from "../config";
 const s3 = require("s3");
 
-export default function pushToS3(NEW_TAG: string) {
+export default async function pushToS3(NEW_TAG: string) {
   console.log("Start to upload bundled javascript files to S3");
 
   const s3Client = s3.createClient(DeployConfig.S3_CLIENT_OPTIONS);
 
   let uploader: DeployConfig.S3ClientUploaderOptions;
 
-  return new Promise((resolve, reject) => {
+  await new Promise(async (resolve, reject) => {
     const isProduction = process.env.NODE_ENV === "production";
+
+    console.log(process.env.NODE_ENV);
+
     const targetPrefix = isProduction
       ? `${DeployConfig.AWS_S3_PRODUCTION_FOLDER_PREFIX}/${NEW_TAG}`
       : `${DeployConfig.AWS_S3_STAGE_FOLDER_PREFIX}/${process.env.BRANCH_NAME}`;
 
     const cacheControl = isProduction ? "public, max-age=604800" : "public, max-age=0";
+
+    if (!isProduction) {
+      console.log("Trying to delete existing demo bundle javascript file.");
+      await deleteExistDemoIfExist(DeployConfig.AWS_S3_BUCKET, targetPrefix);
+    }
 
     uploader = s3Client.uploadDir({
       localDir: DeployConfig.APP_DEST,
@@ -41,5 +49,44 @@ export default function pushToS3(NEW_TAG: string) {
     });
   }).catch(err => {
     console.error(err);
+  });
+}
+
+async function deleteExistDemoIfExist(bucket: string, prefix: string) {
+  const s3Client = s3.createClient(DeployConfig.S3_CLIENT_OPTIONS);
+
+  await new Promise((resolve, reject) => {
+    s3Client.s3.headObject(
+      {
+        Bucket: bucket,
+        Key: `${prefix}/bundle.js`,
+      },
+      (err: any, _data: any) => {
+        if (err && err.statusCode !== 404) {
+          reject(new Error("Has Error to check bundle javascript file already existing."));
+        } else if (err && err.statusCode === 404) {
+          resolve();
+        } else {
+          const deleteDirManager = s3Client.deleteDir({
+            Bucket: bucket,
+            Prefix: prefix,
+          });
+
+          deleteDirManager.on("error", (error: Error) => {
+            console.error("unable to sync:", error.stack);
+            reject(error);
+          });
+
+          deleteDirManager.on("progress", () => {
+            console.log("progress", deleteDirManager.progressAmount, deleteDirManager.progressTotal);
+          });
+
+          deleteDirManager.on("end", () => {
+            console.log("END to upload dist files to S3");
+            resolve();
+          });
+        }
+      }
+    );
   });
 }
