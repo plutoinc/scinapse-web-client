3d06beee559be6a5fdb0abc634d9e6e4df06b2d6
Tylor Shin <shincode@icloud.com>
2018-07-24 11:56:43 +0900

    Archive succeeded deployment (#405)
    * Bug fixes (#402)
    
    Fix bugs
    
    * deploy (#404)
    
    * Fix reset password page
    
    * Fix all broken styles
    
    * Make reset password work at the sign in page
    
    * Make auto completion be initialized when route changes (#400)
    
    * change footer postion (#403)

--- app/components/auth/index.tsx
+++ app/components/auth/index.tsx
@@ -8,6 +8,8 @@ import SignUp from "./signUp";
 import ResetPassword from "./resetPassword";
 import EmailVerification from "./emailVerification";
 import { AppState } from "../../reducers";
+import { withStyles } from "../../helpers/withStylesHelper";
+const styles = require("./auth.scss");
 
 interface AuthComponentProps extends RouteComponentProps<any> {
   currentUser: CurrentUser;
@@ -19,31 +21,34 @@ function mapStateToProps(state: AppState) {
   };
 }
 
+@withStyles<typeof AuthComponent>(styles)
 class AuthComponent extends React.PureComponent<AuthComponentProps, {}> {
   public render() {
     const { match, currentUser } = this.props;
     const { isLoggedIn } = currentUser;
 
     return (
-      <div>
-        <Switch>
-          <AuthRedirect
-            path={`${match.url}/sign_in`}
-            component={SignIn}
-            isLoggedIn={isLoggedIn}
-            needAuthType={AuthType.ShouldLoggedOut}
-            exact={true}
-          />
-          <AuthRedirect
-            path={`${match.url}/sign_up`}
-            component={SignUp}
-            isLoggedIn={isLoggedIn}
-            needAuthType={AuthType.ShouldLoggedOut}
-            exact={true}
-          />
-          <Route path={`${match.url}/reset-password`} component={ResetPassword} exact={true} />
-          <Route path={`${match.url}/email_verification`} component={EmailVerification} exact={true} />
-        </Switch>
+      <div className={styles.pageWrapper}>
+        <div className={styles.authWrapper}>
+          <Switch>
+            <AuthRedirect
+              path={`${match.url}/sign_in`}
+              component={SignIn}
+              isLoggedIn={isLoggedIn}
+              needAuthType={AuthType.ShouldLoggedOut}
+              exact={true}
+            />
+            <AuthRedirect
+              path={`${match.url}/sign_up`}
+              component={SignUp}
+              isLoggedIn={isLoggedIn}
+              needAuthType={AuthType.ShouldLoggedOut}
+              exact={true}
+            />
+            <Route path={`${match.url}/reset-password`} component={ResetPassword} exact={true} />
+            <Route path={`${match.url}/email_verification`} component={EmailVerification} exact={true} />
+          </Switch>
+        </div>
       </div>
     );
   }
