8638ba2a29e03cfc17738352c1ba59ad8bcaf1f3
Tylor Shin <shincode@icloud.com>
2018-08-13 11:26:47 +0900

    Add making destination dirctory logic

--- scripts/deploy/buildForDeploy.ts
+++ scripts/deploy/buildForDeploy.ts
@@ -1,18 +1,18 @@
 import * as fs from "fs";
 import downloadBundleFromS3 from "./helpers/downloadBundle";
-import { setTimeout } from "timers";
+import { APP_DEST } from "./config";
 
 async function deploy() {
   const VERSION = process.env.VERSION;
+
+  if (!fs.existsSync(APP_DEST)) {
+    fs.mkdirSync(APP_DEST);
+  }
+
   fs.writeFileSync("./version", VERSION);
   console.log(`THE CURRENT VERSION IS === ${VERSION}`);
 
   await downloadBundleFromS3(VERSION);
-  await new Promise((resolve, _reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 3000);
-  });
 }
 
 deploy().then(() => {
