00751bf8efe84ed4ba115bf6a905dbeb6408d0d8
Tylor Shin <shincode@icloud.com>
2018-08-13 15:02:07 +0900

    Fix wrong downloading bundled javascript logic && Add E2ETest at production level

--- scripts/deploy/helpers/downloadBundle.ts
+++ scripts/deploy/helpers/downloadBundle.ts
@@ -1,4 +1,5 @@
 import * as AWS from "aws-sdk";
+import * as fs from "fs";
 import * as DeployConfig from "../config";
 const s3 = new AWS.S3();
 
@@ -9,19 +10,28 @@ export default async function downloadBundleFromS3(VERSION: string) {
     const key = `${DeployConfig.AWS_S3_PRODUCTION_FOLDER_PREFIX}/${VERSION}/bundle.js`;
     console.log(key, " === downloadFile Prefix");
 
-    s3.getObject(
-      {
-        Bucket: DeployConfig.AWS_S3_BUCKET,
-        Key: key,
-      },
-      (err: Error, _data: any) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve("========== Downloaded bundled javascript file for server rendering ==========");
-        }
-      }
-    );
+    if (fs.existsSync(`${DeployConfig.APP_DEST}/bundle.js`)) {
+      fs.unlinkSync(`${DeployConfig.APP_DEST}/bundle.js`);
+    }
+
+    const writeStream = fs.createWriteStream(`${DeployConfig.APP_DEST}/bundle.js`);
+
+    s3.getObject({
+      Bucket: DeployConfig.AWS_S3_BUCKET,
+      Key: key,
+    })
+      .createReadStream()
+      .pipe(writeStream);
+
+    writeStream.on("error", () => {
+      console.error("===ERROR=== ERROR! HAD FAILED TO DOWNLOAD BUNDLED JAVASCRIPT FILE! ==========");
+      reject();
+    });
+
+    writeStream.on("finish", () => {
+      console.log("========== Downloaded bundled javascript file for server rendering ==========");
+      resolve();
+    });
   }).catch(err => {
     console.error(err);
   });
