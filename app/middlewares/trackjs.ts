95f37b9087b3a09db15234e74fcae84a6f5133d6
Tylor Shin <shincode@icloud.com>
2018-08-20 15:33:35 +0900

    Hot fix) Fix wrong running machine environment finding logic && trackjs logic (#433)

--- app/middlewares/trackjs.ts
+++ app/middlewares/trackjs.ts
@@ -1,18 +1,15 @@
 import { ACTION_TYPES } from "../actions/actionTypes";
-
 declare var trackJs: any;
 
 const trackJsLogger = (store: any) => (next: any) => (action: any) => {
   try {
-    console.log(action);
-
     if (
       action.type === ACTION_TYPES.SIGN_IN_SUCCEEDED_TO_SIGN_IN ||
       action.type === ACTION_TYPES.AUTH_SUCCEEDED_TO_CHECK_LOGGED_IN
     ) {
       if (action.payload && action.payload.user && action.payload.user.id) {
-        trackJs.configure({ userId: String(action.payload.user.id) });
-        trackJs.addMetadata("user", JSON.stringify(action.payload.user));
+        trackJs && trackJs.configure({ userId: String(action.payload.user.id) });
+        trackJs && trackJs.addMetadata("user", JSON.stringify(action.payload.user));
       }
     }
 
