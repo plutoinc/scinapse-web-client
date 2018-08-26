6ee6a60f0d146f7121a618f1f595984b6c852c80
kimyukyeong <kykkjh2984@gmail.com>
2018-08-26 16:35:16 +0900

    Modify formatNumber test code

--- app/helpers/__tests__/formatNumber_spec.tsx
+++ app/helpers/__tests__/formatNumber_spec.tsx
@@ -17,25 +17,25 @@ describe("FormatNumber helper", () => {
 
   describe("when 10000 <= rawNumber < 100000", () => {
     it("should return the number itself with string type", () => {
-      expect(formatNumber(15333)).toEqual("15k+");
+      expect(formatNumber(15333)).toEqual("15k");
     });
   });
 
   describe("when 100000 <= rawNumber < 1000000", () => {
     it("should return the number itself with string type", () => {
-      expect(formatNumber(153333)).toEqual("153k+");
+      expect(formatNumber(153333)).toEqual("153k");
     });
   });
 
   describe("when 1000000 <= rawNumber < 10000000", () => {
     it("should return the number itself with string type", () => {
-      expect(formatNumber(1533333)).toEqual("1.5m+");
+      expect(formatNumber(1533333)).toEqual("1.5m");
     });
   });
 
   describe("when 10000000 <= rawNumber < 100000000", () => {
     it("should return the number itself with string type", () => {
-      expect(formatNumber(15333333)).toEqual("15.3m+");
+      expect(formatNumber(15333333)).toEqual("15.3m");
     });
   });
 });
