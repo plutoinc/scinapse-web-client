75c7165d6b3363c5cc637731308739fb4f70910f
kimyukyeong <kykkjh2984@gmail.com>
2018-08-26 17:08:03 +0900

    Change formatNumber logic

--- app/helpers/formatNumber.tsx
+++ app/helpers/formatNumber.tsx
@@ -1,10 +1,10 @@
 export default function formatNumber(rawNumber: number): string {
   if (rawNumber < 10000) {
     return rawNumber.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-  } else if (rawNumber >= 10000 && rawNumber < 1000000) {
-    return `${Math.floor(rawNumber / 1000)}k`;
+  } else if (rawNumber >= 1000 && rawNumber < 1000000) {
+    return `${Math.round(rawNumber / 100) / 10}k`;
   } else if (rawNumber >= 1000000) {
-    return `${Math.floor(rawNumber / 1000000)}m`;
+    return `${Math.round(rawNumber / 100000) / 10}m`;
   } else {
     return rawNumber.toString();
   }
