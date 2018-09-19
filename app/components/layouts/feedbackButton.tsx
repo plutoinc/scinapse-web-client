ef3f22e4c13740e1703f320d05090d94ec29f711
Tylor Shin <shincode@icloud.com>
2018-09-19 14:04:35 +0900

    Change design

--- app/components/layouts/feedbackButton.tsx
+++ app/components/layouts/feedbackButton.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as H from "history";
-import Popover from "@material-ui/core/Popover";
+import Popper from "@material-ui/core/Popper";
 import MenuItem from "@material-ui/core/MenuItem";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import FeedbackManager from "@pluto_network/scinapse-feedback";
 import * as Cookies from "js-cookie";
 import Icon from "../../icons";
@@ -59,57 +60,54 @@ class FeedbackButton extends React.PureComponent<FeedbackButtonProps, FeedbackBu
           <Icon icon="FEEDBACK_PENCIL" className={styles.feedbackButtonIcon} />
           <span>Feedback</span>
         </div>
-        <Popover
-          open={isPopoverOpen}
-          classes={{ paper: styles.popoverPaper }}
-          anchorEl={this.popoverAnchorEl!}
-          anchorOrigin={{ horizontal: "right", vertical: "top" }}
-          transformOrigin={{ horizontal: "right", vertical: "bottom" }}
-          onClose={this.handleCloseRequest}
-        >
-          <div className={styles.greetingBoxWrapper}>
-            <div className={styles.greetingBox}>Hi, There! 👋</div>
-          </div>
-          <div className={styles.dropdownMenuWrapper}>
-            <div className={styles.dropdownTitle}>
-              {// tslint:disable-next-line:max-line-length
-              `Is Scinapse helping your research?\nPlease share your experience, and make us work for you!\nWe'll try best to reflect your feedback and make it better.`}
-            </div>
-            <MenuItem onClick={this.handleCloseRequest} classes={{ root: styles.menuItem }}>
-              <a
-                className={styles.menuItemContent}
-                target="_blank"
-                // tslint:disable-next-line:max-line-length
-                href="https://docs.google.com/forms/d/e/1FAIpQLSeqrI59V-HlbaL1HaudUi1rSE1WEuMpBI-6iObJ-wHM7NhRWA/viewform?usp=sf_link"
-              >
-                1-miniute User Survey ✍️
-              </a>
-            </MenuItem>
-            <MenuItem onClick={this.handleCloseRequest} classes={{ root: styles.menuItem }}>
-              <a target="_blank" className={styles.menuItemContent} href="https://t.me/plutonetwork">
-                Telegram 🗣
-              </a>
-            </MenuItem>
+        <Popper open={isPopoverOpen} anchorEl={this.popoverAnchorEl!} placement="top-end" disablePortal={true}>
+          <ClickAwayListener onClickAway={this.handleCloseRequest}>
+            <div className={styles.popperPaper}>
+              <div className={styles.greetingBoxWrapper}>
+                <div className={styles.greetingBox}>Hi, There! 👋</div>
+              </div>
+              <div className={styles.dropdownMenuWrapper}>
+                <div className={styles.dropdownTitle}>
+                  {// tslint:disable-next-line:max-line-length
+                  `Is Scinapse helping your research?\nPlease share your experience, and make us work for you!\nWe'll try best to reflect your feedback and make it better.`}
+                </div>
+                <MenuItem onClick={this.handleCloseRequest} classes={{ root: styles.menuItem }}>
+                  <a
+                    className={styles.menuItemContent}
+                    target="_blank"
+                    // tslint:disable-next-line:max-line-length
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSeqrI59V-HlbaL1HaudUi1rSE1WEuMpBI-6iObJ-wHM7NhRWA/viewform?usp=sf_link"
+                  >
+                    1-miniute User Survey ✍️
+                  </a>
+                </MenuItem>
+                <MenuItem onClick={this.handleCloseRequest} classes={{ root: styles.menuItem }}>
+                  <a target="_blank" className={styles.menuItemContent} href="https://t.me/plutonetwork">
+                    Telegram 🗣
+                  </a>
+                </MenuItem>
 
-            <div className={styles.feedbackInput}>
-              <div className={styles.feedbackHeader}>Direct Feedback 📣</div>
+                <div className={styles.feedbackInput}>
+                  <div className={styles.feedbackHeader}>Direct Feedback 📣</div>
 
-              <form onSubmit={this.handleSubmitFeedbackForm} className={styles.feedbackForm}>
-                <div className={styles.formStyle}>
-                  <label>E-Mail (Optional)</label>
-                  <input type="email" value={emailInput} onChange={this.handleChangeEmail} />
+                  <form onSubmit={this.handleSubmitFeedbackForm} className={styles.feedbackForm}>
+                    <div className={styles.formStyle}>
+                      <label>E-Mail (Optional)</label>
+                      <input type="email" value={emailInput} onChange={this.handleChangeEmail} />
+                    </div>
+                    <div className={styles.formStyle}>
+                      <label>Feedback</label>
+                      <textarea value={feedbackContent} onChange={this.handleChangeFeedback} />
+                    </div>
+                    <div className={styles.btnWrapper}>
+                      <button>{!isLoadingFeedback ? "Send Feedback" : "is loading ..."}</button>
+                    </div>
+                  </form>
                 </div>
-                <div className={styles.formStyle}>
-                  <label>Feedback</label>
-                  <textarea value={feedbackContent} onChange={this.handleChangeFeedback} />
-                </div>
-                <div className={styles.btnWrapper}>
-                  <button>{!isLoadingFeedback ? "Send Feedback" : "is loading ..."}</button>
-                </div>
-              </form>
+              </div>
             </div>
-          </div>
-        </Popover>
+          </ClickAwayListener>
+        </Popper>
       </div>
     );
   }
