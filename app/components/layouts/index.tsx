3443da34b4a21f05ddb9492805f495dd15dbae1c
Tylor Shin <shincode@icloud.com>
2018-09-27 17:55:56 +0900

    remove unused mobile header

--- app/components/layouts/index.tsx
+++ app/components/layouts/index.tsx
@@ -1,6 +1,5 @@
 import Header from "./header";
 import Footer from "./footer";
 import FeedbackButton from "./feedbackButton";
-import MobileHeader from "./mobile/header";
 
-export { Header, Footer, FeedbackButton, MobileHeader };
+export { Header, Footer, FeedbackButton };
