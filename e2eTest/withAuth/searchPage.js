08b96c9c48dba83bbea0a4ba533e650697ada9f3
Tylor Shin <shincode@icloud.com>
2018-09-28 13:27:21 +0900

    Change wrong css policy

--- e2eTest/withAuth/searchPage.js
+++ e2eTest/withAuth/searchPage.js
@@ -17,27 +17,27 @@ describe("Pluto search page features along with auth user", function() {
       .url(targetUrl)
       .resizeWindow(1920, 1080)
       .pause(10000)
-      .waitForElementVisible("[class^='header__signInButton']")
+      .waitForElementVisible("[class^='header_signInButton']")
       .saveScreenshot("./output/e2e/withAuth/searchPage/before_click_sign_in_button.png")
-      .click("[class^='header__signInButton']")
+      .click("[class^='header_signInButton']")
       .saveScreenshot("./output/e2e/withAuth/searchPage/after_click_sign_in_button.png")
       .waitForElementVisible("[placeholder='E-mail']", 5000)
       .setValue("[placeholder='E-mail']", [process.env.PLUTO_TEST_ID])
       .setValue("[placeholder='Password']", [process.env.PLUTO_TEST_PASSWORD, browser.Keys.ENTER])
       .saveScreenshot("./output/e2e/withAuth/searchPage/after_submit_sign_in_form.png")
-      .waitForElementVisible("[class^='header__userDropdownChar']", 20000)
-      .waitForElementVisible("[class^='filterContainer__filterItem']", 20000)
-      .waitForElementVisible("[class^='title__title']", 30000);
+      .waitForElementVisible("[class^='header_userDropdownChar']", 20000)
+      .waitForElementVisible("[class^='filterContainer_filterItem']", 20000)
+      .waitForElementVisible("[class^='title_title']", 30000);
     done();
   });
 
   describe("with unverified but signed user", () => {
     it("should render search result", browser => {
-      browser.expect.element("[class^='filterContainer__filterItem']").to.be.present;
-      browser.expect.element("[class^='header__headerContainer']").to.be.present;
-      browser.expect.element("[class^='title__title']").to.be.present;
-      browser.expect.element("[class^='publishInfoList__publishInfoList']").to.be.present;
-      browser.expect.element("[class^='abstract__abstract']").to.be.present;
+      browser.expect.element("[class^='filterContainer_filterItem']").to.be.present;
+      browser.expect.element("[class^='header_headerContainer']").to.be.present;
+      browser.expect.element("[class^='title_title']").to.be.present;
+      browser.expect.element("[class^='publishInfoList_publishInfoList']").to.be.present;
+      browser.expect.element("[class^='abstract_abstract']").to.be.present;
     });
   });
 });
