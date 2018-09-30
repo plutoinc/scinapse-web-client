e1e80a8477981f38c16f99e628d38ffe7ef22fc1
Tylor Shin <shincode@icloud.com>
2018-09-30 16:53:20 +0900

    Remove TrackJS relevant logics from code

--- app/helpers/errorHandler.tsx
+++ app/helpers/errorHandler.tsx
@@ -1,23 +1,9 @@
 import * as React from "react";
 import EnvChecker from "./envChecker";
 
-declare var trackJs: any;
-
 export function logException(error: Error, errorInfo?: any) {
   if (EnvChecker.isLocal()) {
     console.error("Error!", error, errorInfo);
-  } else if (EnvChecker.isProdBrowser()) {
-    if (errorInfo && errorInfo.componentStack) {
-      // The component stack is sometimes useful in development mode
-      // In production it can be somewhat obfuscated, so feel free to omit this line.
-      console.log(errorInfo.componentStack);
-    }
-
-    // TrackJS should be configured and available before making this call
-    // Either as an imported module or a global (ESLint global ignore is used above)
-    trackJs.track(error);
-
-    this.setState({ error });
   }
 }
 
