47af998edd1674a74d025ded9e8eb0a5821c9ce2
Tylor Shin <shincode@icloud.com>
2018-10-01 11:55:26 +0900

    Remove TrackJS relevant logics from code (#467)

--- app/helpers/htmlWrapper.tsx
+++ app/helpers/htmlWrapper.tsx
@@ -1,5 +1,4 @@
 import { HelmetData } from "react-helmet";
-import EnvChecker from "./envChecker";
 const sprite = require("svg-sprite-loader/runtime/sprite.build");
 
 interface GenerateFullHTMLParams {
@@ -24,7 +23,6 @@ export function generateFullHTML({ reactDom, scriptPath, helmet, initialState, c
       {'GTM-5QR7T6H':true});</script>
       ${helmet.title.toString()}
       <script type="text/javascript">window._script_version_ = { version: '${version}' };</script>
-      ${getTrackJS()}
       ${helmet.script.toString()}
       ${helmet.meta.toString()}
       ${helmet.link.toString()}
@@ -39,15 +37,4 @@ export function generateFullHTML({ reactDom, scriptPath, helmet, initialState, c
       </body>
     </html>
   `;
-}
-
-function getTrackJS() {
-  if (!EnvChecker.isDev() && !EnvChecker.isLocal()) {
-    return `<!-- BEGIN TRACKJS -->
-    <script type="text/javascript">window._trackJs = { token: 'b96e5fcd407648ffb37c5228780fbb71' };</script>
-    <script type="text/javascript" src="https://cdn.trackjs.com/releases/current/tracker.js"></script>
-  <!-- END TRACKJS -->`;
-  } else {
-    return "";
-  }
 }
