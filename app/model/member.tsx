39f686abac95951f7b7ae6bd176b52d18b6d3a42
Tylor Shin <shincode@icloud.com>
2018-10-08 16:14:25 +0900

    Remove MemberWithCollection

--- app/model/member.tsx
+++ app/model/member.tsx
@@ -4,22 +4,9 @@ import { MemberOAuth } from "./oauth";
 export interface Member {
   id: number;
   email: string;
-  name: string;
-  profileImage: string;
-  affiliation: string;
-  major: string;
-  reputation: number;
-  articleCount: number;
-  reviewCount: number;
-  commentCount: number;
   emailVerified: boolean;
-  oauth: MemberOAuth | null;
-}
-
-export interface MemberWithCollection {
-  id: number;
-  email: string;
-  emailVerified: boolean;
+  firstName: string;
+  lastName: string;
   name: string;
   profileImage: string;
   affiliation: string;
