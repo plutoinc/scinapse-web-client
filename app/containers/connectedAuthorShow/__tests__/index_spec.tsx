c62c238749d8c2a7cdece3e422f84152f9866880
Tylor Shin <shincode@icloud.com>
2018-12-03 16:48:47 +0900

    Fix broken specs

--- app/containers/connectedAuthorShow/__tests__/index_spec.tsx
+++ app/containers/connectedAuthorShow/__tests__/index_spec.tsx
@@ -1,20 +1,20 @@
+jest.mock("../../../api/author");
+jest.mock("react-truncate");
+
 import * as React from "react";
 import * as renderer from "react-test-renderer";
-import { MemoryRouter, Route } from "react-router";
 import { Provider } from "react-redux";
 import { generateMockStore } from "../../../__tests__/mockStore";
 import { initialState } from "../../../reducers";
 import ConnectedAuthorShow from "../../../containers/connectedAuthorShow";
-import { AUTHOR_SHOW_PATH } from "../../../routes";
-import { LAYOUT_INITIAL_STATE } from "../../../components/layouts/records";
 import { RAW } from "../../../__mocks__";
-import { AUTHOR_SHOW_INITIAL_STATE } from "../../unconnectedAuthorShow/reducer";
-import { CONFIGURATION_INITIAL_STATE } from "../../../reducers/configuration";
-import { CURRENT_USER_INITIAL_STATE } from "../../../model/currentUser";
 import { mapRawAuthor } from "../../../model/author/author";
+import { AUTHOR_SHOW_PATH } from "../../../routes";
+import { MemoryRouter, Route } from "react-router-dom";
 
 describe("ConnectedAuthorShow Component", () => {
-  const mockStore = generateMockStore(initialState);
+  const mappedAuthor = mapRawAuthor(RAW.AUTHOR);
+  let mockStore = generateMockStore(initialState);
   let mockState: any;
 
   describe("when there is no all publications", () => {
@@ -22,27 +22,30 @@ describe("ConnectedAuthorShow Component", () => {
       mockStore.clearActions();
 
       mockState = {
-        layout: LAYOUT_INITIAL_STATE,
-        author: mapRawAuthor(RAW.AUTHOR),
-        coAuthors: [],
-        papers: [],
-        authorShow: AUTHOR_SHOW_INITIAL_STATE,
-        configuration: CONFIGURATION_INITIAL_STATE,
-        currentUser: CURRENT_USER_INITIAL_STATE,
-        dispatch: mockStore.dispatch,
+        ...initialState,
+        connectedAuthorShow: {
+          ...initialState.connectedAuthorShow,
+          authorId: 2258681040,
+          paperIds: [],
+          coAuthorIds: [],
+          papersTotalPage: 1,
+          papersCurrentPage: 1,
+          papersTotalCount: 1,
+        },
+        entities: {
+          ...initialState.entities,
+          authors: { "2258681040": mappedAuthor },
+        },
       };
+
+      mockStore = generateMockStore(mockState);
     });
 
     it("should render no publications at the content section", () => {
       const tree = renderer
         .create(
           <Provider store={mockStore}>
-            <MemoryRouter initialIndex={0} initialEntries={["/authors/2764552960"]}>
-              <Route path={AUTHOR_SHOW_PATH}>
-                {/*Route is needed for providing match params*/}
-                <ConnectedAuthorShow {...mockState} />
-              </Route>
-            </MemoryRouter>
+            <ConnectedAuthorShow />
           </Provider>
         )
         .toJSON();
@@ -55,25 +58,33 @@ describe("ConnectedAuthorShow Component", () => {
       mockStore.clearActions();
 
       mockState = {
-        layout: LAYOUT_INITIAL_STATE,
-        author: mapRawAuthor(RAW.AUTHOR),
-        coAuthors: [],
-        papers: [RAW.PAPER],
-        authorShow: AUTHOR_SHOW_INITIAL_STATE,
-        configuration: CONFIGURATION_INITIAL_STATE,
-        currentUser: CURRENT_USER_INITIAL_STATE,
-        dispatch: mockStore.dispatch,
+        ...initialState,
+        connectedAuthorShow: {
+          ...initialState.connectedAuthorShow,
+          authorId: 2258681040,
+          paperIds: [RAW.PAPER.id],
+          coAuthorIds: [],
+          papersTotalPage: 1,
+          papersCurrentPage: 1,
+          papersTotalCount: 1,
+        },
+        entities: {
+          ...initialState.entities,
+          authors: { "2258681040": mappedAuthor },
+          papers: { [`${RAW.PAPER.id}`]: RAW.PAPER },
+        },
       };
+
+      mockStore = generateMockStore(mockState);
     });
 
     it("should render content correctly", () => {
       const tree = renderer
         .create(
           <Provider store={mockStore}>
-            <MemoryRouter initialIndex={0} initialEntries={["/authors/2764552960"]}>
+            <MemoryRouter initialIndex={0} initialEntries={["/authors/2258681040"]}>
               <Route path={AUTHOR_SHOW_PATH}>
-                {/*Route is needed for providing match params*/}
-                <ConnectedAuthorShow {...mockState} />
+                <ConnectedAuthorShow />
               </Route>
             </MemoryRouter>
           </Provider>
@@ -88,25 +99,32 @@ describe("ConnectedAuthorShow Component", () => {
       mockStore.clearActions();
 
       mockState = {
-        layout: LAYOUT_INITIAL_STATE,
-        author: mapRawAuthor(RAW.AUTHOR),
-        coAuthors: [],
-        papers: [RAW.PAPER],
-        authorShow: { ...AUTHOR_SHOW_INITIAL_STATE, isLoadingPapers: true },
-        configuration: CONFIGURATION_INITIAL_STATE,
-        currentUser: CURRENT_USER_INITIAL_STATE,
-        dispatch: mockStore.dispatch,
+        ...initialState,
+        connectedAuthorShow: {
+          ...initialState.connectedAuthorShow,
+          authorId: 2258681040,
+          isLoadingPapers: true,
+          coAuthorIds: [],
+          papersTotalPage: 1,
+          papersCurrentPage: 1,
+          papersTotalCount: 1,
+        },
+        entities: {
+          ...initialState.entities,
+          authors: { "2258681040": mappedAuthor },
+        },
       };
+
+      mockStore = generateMockStore(mockState);
     });
 
     it("should render loading spinner correctly", () => {
       const tree = renderer
         .create(
           <Provider store={mockStore}>
-            <MemoryRouter initialIndex={0} initialEntries={["/authors/2764552960"]}>
+            <MemoryRouter initialIndex={0} initialEntries={["/authors/2258681040"]}>
               <Route path={AUTHOR_SHOW_PATH}>
-                {/*Route is needed for providing match params*/}
-                <ConnectedAuthorShow {...mockState} />
+                <ConnectedAuthorShow />
               </Route>
             </MemoryRouter>
           </Provider>
