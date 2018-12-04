fe8de8c27b60464968e06bf542dd3f172af41104
Tylor Shin <shincode@icloud.com>
2018-12-04 21:02:42 +0900

    Make default dev zone load the latest succees master build version (#528)
    * Add logger and try catch statement to debug dev master logic
    
    * Add more logs in the deploy process
    
    * Change wrong async await order
    
    * Change way render function
    
    * Add devHandlerTsconfig.json for dev handler file
    
    * Remove cache from dev & prod  zone build

--- app/server/devRenderer.tsx
+++ app/server/devRenderer.tsx
@@ -4,49 +4,81 @@ const fs = require("fs");
 const s3 = new AWS.S3();
 
 class DevRenderer {
-  private branchName: string | undefined;
-
-  constructor(branchName?: string) {
-    this.branchName = branchName;
-  }
-
   public async render(event: Lambda.Event) {
-    await this.downloadJSFromS3();
+    const branch = this.getTargetBranch(event);
+    const version = await this.getVersion(event, branch);
+    await this.downloadJSFromS3(version, branch);
     const bundle = require("/tmp/bundle.js");
-    console.log(bundle.ssr, "=== ssr");
     const render = bundle.ssr;
-    const result = await render(event);
+
+    let result: string;
+    if (branch === "master") {
+      result = await render({ ...event, queryStringParameters: { branch: "master", version } });
+    } else {
+      result = await render(event);
+    }
+
     return result;
   }
 
-  private async downloadJSFromS3() {
-    await new Promise(async (resolve, _reject) => {
-      let targetKey: string = `${DeployConfig.AWS_S3_DEV_FOLDER_PREFIX}/${this.branchName}/bundle.js`;
+  private getTargetBranch(event: Lambda.Event) {
+    let targetBranch: string = "master";
 
+    if (event.queryStringParameters && event.queryStringParameters.branch) {
+      try {
+        console.log("GET BRANCH_NAME FROM BRANCH QUERYPARAMS");
+        const demoBranch = decodeURIComponent(event.queryStringParameters.branch);
+        targetBranch = demoBranch;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    console.log(`targetBranch is ${targetBranch}`);
+
+    return targetBranch;
+  }
+
+  private async getVersion(event: Lambda.Event, targetBranch: string): Promise<string> {
+    console.log(event.queryStringParameters);
+    if (targetBranch !== "master") {
+      return targetBranch;
+    }
+
+    try {
+      const versionResponse = await s3
+        .getObject({
+          Bucket: DeployConfig.AWS_S3_BUCKET,
+          Key: "version",
+        })
+        .promise();
+
+      console.log("SUCCEEDED GET THE LATEST VERSION FILE TO GET MASTER BRANCH VERSION");
+      if (versionResponse.Body) {
+        const version = (versionResponse.Body as Buffer).toString("utf8");
+        console.log("SUCCEEDED SET THE LATEST VERSION FILE TO GET MASTER BRANCH VERSION");
+        return version;
+      }
+      return "master";
+    } catch (err) {
+      console.log("ERROR OCCURRED WHEN DURING DOWNLOAD THE LATEST VERSION FILE");
+      console.error(err);
+      throw err;
+    }
+  }
+
+  private async downloadJSFromS3(version: string, targetBranch: string) {
+    let targetKey: string = `${DeployConfig.AWS_S3_DEV_FOLDER_PREFIX}/${version}/bundle.js`;
+
+    if (targetBranch === "master") {
+      targetKey = `${DeployConfig.AWS_S3_PRODUCTION_FOLDER_PREFIX}/${version}/bundle.js`;
+    }
+
+    await new Promise((resolve, _reject) => {
       console.log(`BUNDLE ALREADY EXISTS? ${fs.existsSync("/tmp/bundle.js")}`);
 
       if (fs.existsSync("/tmp/bundle.js")) {
         fs.unlinkSync("/tmp/bundle.js");
-      }
-
-      if (!this.branchName) {
-        try {
-          const versionResponse = await s3
-            .getObject({
-              Bucket: DeployConfig.AWS_S3_BUCKET,
-              Key: "version",
-            })
-            .promise();
-
-          if (versionResponse.Body) {
-            const version = (versionResponse.Body as Buffer).toString("utf-8");
-            targetKey = `${DeployConfig.AWS_S3_PRODUCTION_FOLDER_PREFIX}/${version}/bundle.js`;
-          }
-        } catch (err) {
-          console.log("ERROR OCCURRED WHEN DURING DOWNLOAD THE LATEST VERSION FILE");
-          console.error(err);
-          throw err;
-        }
       }
 
       const writeStream = fs.createWriteStream("/tmp/bundle.js");
@@ -71,14 +103,14 @@ class DevRenderer {
 async function handler(event: Lambda.Event, _context: Lambda.Context) {
   const path = event.path;
   const queryParamsObj = event.queryStringParameters || {};
+
+  console.log(JSON.stringify(event.queryStringParameters), "=== event.queryStringParameters");
   console.log(JSON.stringify(queryParamsObj), "=== queryParamsObj");
 
   console.log(event, "=== event at parent function");
   console.log(path, "=== path at parent function");
 
-  const targetBranch = decodeURIComponent(queryParamsObj.branch);
-  console.log(`targetBranch is ${targetBranch}`);
-  const devRenderer = new DevRenderer(targetBranch);
+  const devRenderer = new DevRenderer();
   const result = await devRenderer.render(event);
   return result;
 }
