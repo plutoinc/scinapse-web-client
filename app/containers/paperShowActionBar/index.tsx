ff0faceae8ab4aced578f9897782fb936af365cf
NohYujeong <shdbwjd0326@naver.com>
2018-12-05 17:35:24 +0900

    Change PaperShowCollectionButton Design & Move components to container

--- app/containers/paperShowActionBar/index.tsx
+++ app/containers/paperShowActionBar/index.tsx
@@ -2,25 +2,18 @@ import * as React from "react";
 import { denormalize } from "normalizr";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import Popover from "@material-ui/core/Popover";
 import GlobalDialogManager from "../../helpers/globalDialogManager";
 import { withStyles } from "../../helpers/withStylesHelper";
 import Icon from "../../icons";
 import { AppState } from "../../reducers";
-import { collectionSchema, Collection } from "../../model/collection";
 import { PaperShowActionBarState } from "./reducer";
-import CollectionDropdown from "../../components/paperShow/components/collectionDropdown";
 import { CurrentUser } from "../../model/currentUser";
 import { trackEvent } from "../../helpers/handleGA";
-import { addPaperToCollection, removePaperFromCollection } from "../../components/dialog/actions";
 import { paperSchema, Paper } from "../../model/paper";
-import { PostCollectionParams } from "../../api/collection";
-import { postNewCollection } from "../../actions/paperShow";
-import { openCollectionDropdown, closeCollectionDropdown } from "./actions";
+import PaperShowCollectionControlButton from "../paperShowCollectionControlButton";
 const styles = require("./actionBar.scss");
 
 interface PaperShowActionBarProps {
-  myCollections: Collection[] | null;
   paper: Paper | null;
   paperShowActionBar: PaperShowActionBarState;
   currentUser: CurrentUser;
@@ -29,8 +22,6 @@ interface PaperShowActionBarProps {
 
 @withStyles<typeof PaperShowActionBar>(styles)
 class PaperShowActionBar extends React.PureComponent<PaperShowActionBarProps> {
-  private collectionDivElement: HTMLDivElement | null;
-
   public render() {
     const { paper } = this.props;
 
@@ -41,17 +32,7 @@ class PaperShowActionBar extends React.PureComponent<PaperShowActionBarProps> {
             <div className={styles.leftSide} />
             <div className={styles.rightSide}>
               <li className={styles.actionItem}>{this.getCitationBox()}</li>
-              <li className={styles.actionItem}>
-                <div
-                  onClick={this.handleRequestToOpenCollectionDropdown}
-                  className={styles.actionSave}
-                  ref={el => (this.collectionDivElement = el)}
-                >
-                  <Icon icon={"BOOKMARK_GRAY"} />
-                  <span>Save to Collection</span>
-                </div>
-                {this.getCollectionPopover()}
-              </li>
+              <PaperShowCollectionControlButton />
             </div>
           </ul>
         </div>
@@ -59,87 +40,6 @@ class PaperShowActionBar extends React.PureComponent<PaperShowActionBarProps> {
     }
     return null;
   }
-
-  private getCollectionPopover = () => {
-    const { paperShowActionBar, myCollections } = this.props;
-
-    if (!myCollections) {
-      return null;
-    }
-
-    return (
-      <Popover
-        open={paperShowActionBar.isCollectionDropdownOpen}
-        anchorEl={this.collectionDivElement!}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        onClose={this.handleRequestToCloseCollectionDropdown}
-        classes={{
-          paper: styles.collectionDropdownPaper,
-        }}
-      >
-        <CollectionDropdown
-          isLoadingMyCollections={paperShowActionBar.isLoadingMyCollections}
-          isPositingNewCollection={paperShowActionBar.isPositingNewCollection}
-          myCollections={myCollections}
-          handleAddingPaperToCollection={this.handleAddingPaperToCollection}
-          handleRemovingPaperFromCollection={this.handleRemovingPaperFromCollection}
-          handleSubmitNewCollection={this.handleSubmitNewCollection}
-        />
-      </Popover>
-    );
-  };
-
-  private handleRequestToOpenCollectionDropdown = () => {
-    const { currentUser, dispatch } = this.props;
-
-    trackEvent({ category: "Additional Action", action: "Click [Add Collection] Button" });
-
-    if (!currentUser.isLoggedIn) {
-      return GlobalDialogManager.openSignUpDialog();
-    } else if (currentUser.isLoggedIn) {
-      dispatch(openCollectionDropdown());
-    }
-  };
-
-  private handleRequestToCloseCollectionDropdown = () => {
-    const { dispatch } = this.props;
-
-    dispatch(closeCollectionDropdown());
-  };
-
-  private handleAddingPaperToCollection = async (collection: Collection) => {
-    const { dispatch, paper } = this.props;
-
-    if (paper) {
-      await dispatch(
-        addPaperToCollection({
-          collection,
-          paperId: paper.id,
-        })
-      );
-      await this.getMyCollections();
-    }
-  };
-
-  private handleRemovingPaperFromCollection = async (collection: Collection) => {
-    const { dispatch, paper } = this.props;
-
-    if (paper) {
-      await dispatch(
-        removePaperFromCollection({
-          collection,
-          paperIds: [paper.id],
-        })
-      );
-      await this.getMyCollections();
-    }
-  };
-
-  private handleSubmitNewCollection = async (params: PostCollectionParams) => {
-    const { dispatch } = this.props;
-    await dispatch(postNewCollection(params));
-  };
 
   private getCitationBox = () => {
     const { paper } = this.props;
@@ -165,35 +65,10 @@ class PaperShowActionBar extends React.PureComponent<PaperShowActionBarProps> {
       return null;
     }
   };
-
-  private getMyCollections = async () => {
-    // const { dispatch, currentUser, paper } = this.props;
-    // if (currentUser.isLoggedIn && paper) {
-    //   try {
-    //     const collectionResponse = await dispatch(getMyCollections(paper.id));
-    //     if (collectionResponse && collectionResponse.result.length > 0) {
-    //       collectionResponse.content.filter(obj => obj.contains_selected).map(async collection => {
-    //         const response = await dispatch(getPapers(collection.id));
-    //         if (response && response.result.length > 0) {
-    //           this.setState({
-    //             papersInCollection: [
-    //               ...this.state.papersInCollection,
-    //               response.entities.papersInCollection[paper.id],
-    //             ],
-    //           });
-    //         }
-    //       });
-    //     }
-    //   } catch (err) {
-    //     console.error(`Error for fetching paper show page data`, err);
-    //   }
-    // }
-  };
 }
 
 const mapStateToProps = (state: AppState) => {
   return {
-    myCollections: denormalize(state.paperShowActionBar.myCollectionIds, [collectionSchema], state.entities),
     paperShowActionBar: state.paperShowActionBar,
     currentUser: state.currentUser,
     paper: denormalize(state.paperShow.paperId, paperSchema, state.entities),
--- app/containers/paperShowActionBar/reducer.tsx
+++ app/containers/paperShowActionBar/reducer.tsx
@@ -2,21 +2,11 @@ import { ACTION_TYPES, Actions } from "../../actions/actionTypes";
 
 export interface PaperShowActionBarState
   extends Readonly<{
-      isLoadingMyCollections: boolean;
-      isPositingNewCollection: boolean;
-      hasFailedToLoadMyCollections: boolean;
-      hasFailedToPositingNewCollection: boolean;
       isCollectionDropdownOpen: boolean;
-      myCollectionIds: number[];
     }> {}
 
 export const PAPER_SHOW_ACTION_BAR_INITIAL_STATE: PaperShowActionBarState = {
-  isLoadingMyCollections: false,
-  isPositingNewCollection: false,
-  hasFailedToLoadMyCollections: false,
-  hasFailedToPositingNewCollection: false,
   isCollectionDropdownOpen: false,
-  myCollectionIds: [],
 };
 
 export function reducer(
@@ -24,55 +14,6 @@ export function reducer(
   action: Actions
 ): PaperShowActionBarState {
   switch (action.type) {
-    case ACTION_TYPES.PAPER_SHOW_START_TO_GET_COLLECTIONS: {
-      return {
-        ...state,
-        isLoadingMyCollections: true,
-        hasFailedToLoadMyCollections: false,
-      };
-    }
-
-    case ACTION_TYPES.PAPER_SHOW_FAILED_TO_GET_COLLECTIONS: {
-      return {
-        ...state,
-        isLoadingMyCollections: false,
-        hasFailedToLoadMyCollections: true,
-      };
-    }
-
-    case ACTION_TYPES.PAPER_SHOW_SUCCEEDED_GET_COLLECTIONS: {
-      return {
-        ...state,
-        myCollectionIds: action.payload.collectionIds,
-        isLoadingMyCollections: false,
-        hasFailedToLoadMyCollections: false,
-      };
-    }
-
-    case ACTION_TYPES.GLOBAL_DIALOG_START_TO_POST_COLLECTION: {
-      return {
-        ...state,
-        isPositingNewCollection: true,
-        hasFailedToPositingNewCollection: false,
-      };
-    }
-
-    case ACTION_TYPES.GLOBAL_DIALOG_SUCCEEDED_POST_COLLECTION: {
-      return {
-        ...state,
-        isPositingNewCollection: false,
-        hasFailedToPositingNewCollection: false,
-        myCollectionIds: [...[action.payload.collectionId], ...state.myCollectionIds],
-      };
-    }
-    case ACTION_TYPES.GLOBAL_DIALOG_FAILED_TO_POST_COLLECTION: {
-      return {
-        ...state,
-        isPositingNewCollection: false,
-        hasFailedToPositingNewCollection: true,
-      };
-    }
-
     case ACTION_TYPES.PAPER_SHOW_ACTION_BAR_OPEN_COLLECTION_DROPDOWN: {
       return { ...state, isCollectionDropdownOpen: true };
     }
