501110aea89332576a9eabf20e92d6f1c5afc667
Tylor Shin <shincode@icloud.com>
2019-01-07 10:47:27 +0900

    Add completion API Mock & Fix broken specs

---
+++ app/api/__mocks__/completion.ts
@@ -0,0 +1,31 @@
+import PlutoAxios from "../pluto";
+import { SuggestionKeyword } from "../../model/suggestion";
+import { CompletionKeyword } from "../../components/home/records";
+
+class CompletionAPI extends PlutoAxios {
+  public async getSuggestionKeyword(_query: string): Promise<SuggestionKeyword> {
+    console.log("==========================================");
+    return { keyword: "papre", suggestion: "paper", highlighted: "<b>paper</b>" };
+  }
+
+  public async getKeywordCompletion(_query: string) {
+    const completionKeywords: CompletionKeyword[] = [
+      { keyword: "paprec", type: "KEYWORD" },
+      { keyword: "papreg", type: "KEYWORD" },
+      { keyword: "paprent", type: "KEYWORD" },
+      { keyword: "parents", type: "KEYWORD" },
+      { keyword: "papreen", type: "KEYWORD" },
+      { keyword: "papreddy kashireddy", type: "KEYWORD" },
+      { keyword: "papreen nahar", type: "KEYWORD" },
+      { keyword: "papreck", type: "KEYWORD" },
+      { keyword: "papreck justin", type: "KEYWORD" },
+      { keyword: "papreckiene", type: "KEYWORD" },
+    ];
+
+    return completionKeywords;
+  }
+}
+
+const completionAPI = new CompletionAPI();
+
+export default completionAPI;
