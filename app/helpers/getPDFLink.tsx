de49d91bfe9c0b59060bd656987e6e93b3d37916
Tylor Shin <shincode@icloud.com>
2019-01-15 18:54:05 +0900

    Add [PDF] flag to pdf link

--- app/helpers/getPDFLink.tsx
+++ app/helpers/getPDFLink.tsx
@@ -1,13 +1,14 @@
 import { PaperSource } from "../model/paperSource";
 
 export function getPDFLink(urls: PaperSource[]): PaperSource | undefined {
-  const pdfSourceRecord =
+  return (
     urls &&
     urls.find((paperSource: PaperSource) => {
-      return (
-        paperSource.url.startsWith("https://arxiv.org/pdf/") ||
-        (paperSource.url.startsWith("http") && paperSource.url.endsWith(".pdf"))
-      );
-    });
-  return pdfSourceRecord;
+      return isPDFLink(paperSource.url);
+    })
+  );
+}
+
+export function isPDFLink(url: string) {
+  return url.startsWith("https://arxiv.org/pdf/") || (url.startsWith("http") && url.endsWith(".pdf"));
 }
