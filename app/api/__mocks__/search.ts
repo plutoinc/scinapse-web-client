17a872a073ae5e0a23ba9080799ddc854faa1f2a
Tylor Shin <shincode@icloud.com>
2019-01-28 11:55:13 +0900

    Fix wrong search API mock

--- app/api/__mocks__/search.ts
+++ app/api/__mocks__/search.ts
@@ -1,45 +1,7 @@
-import { CancelToken } from "axios";
 import PlutoAxios from "../pluto";
 import { RAW } from "../../__mocks__";
-import { PaginationResponseV2, PageObjectV2 } from "../types/common";
-import { Paper } from "../../model/paper";
-import { AggregationData } from "../../model/aggregation";
-import { Suggestion } from "../../model/suggestion";
-import { BasePaperAuthor } from "../../model/author";
-import { Affiliation } from "../../model/affiliation";
+import { SearchResult, SearchParams } from "../search";
 const camelcaseKeys = require("camelcase-keys");
-
-interface SearchParams {
-  query: string;
-  sort: string;
-  filter: string;
-  page?: number;
-  cancelToken?: CancelToken;
-}
-
-interface MatchEntityAuthor extends BasePaperAuthor {
-  lastKnownAffiliation: Affiliation | null;
-  paperCount: number;
-  citationCount: number;
-  profileImageUrl: string | null;
-  representativePapers: Paper[];
-}
-
-export interface MatchEntity {
-  entity: MatchEntityAuthor;
-  type: "AUTHOR";
-}
-
-export interface SearchResult extends PaginationResponseV2<Paper[]> {
-  data: {
-    content: Paper[];
-    page: PageObjectV2 | null;
-    aggregation: AggregationData;
-    matchedEntities: MatchEntity[];
-    resultModified: boolean;
-    suggestion: Suggestion | null;
-  };
-}
 
 class SearchAPI extends PlutoAxios {
   public async search(params: SearchParams) {
@@ -48,12 +10,14 @@ class SearchAPI extends PlutoAxios {
     }
 
     const searchResult = {
-      content: RAW.PAPER,
-      page: null,
-      aggregation: RAW.AGGREGATION_RESPONSE,
-      matchedEntities: null,
-      resultModified: true,
-      suggestion: null,
+      data: {
+        content: RAW.PAPER,
+        page: null,
+        aggregation: RAW.AGGREGATION_RESPONSE,
+        matchedEntities: null,
+        resultModified: true,
+        suggestion: null,
+      },
     };
 
     const camelizedRes = camelcaseKeys(searchResult, { deep: true });
