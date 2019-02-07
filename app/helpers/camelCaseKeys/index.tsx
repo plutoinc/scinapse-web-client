fe27e62910e60d46c3adf0de899783b97cc8745e
Tylor Shin <shincode@icloud.com>
2019-02-07 14:06:33 +0900

    Make own camelCaseKeys helper function

---
+++ app/helpers/camelCaseKeys/index.tsx
@@ -0,0 +1,35 @@
+export function camelCaseKeys(rawObject: any): any {
+  const newObject: any = {};
+  const calculatedObject = { ...rawObject };
+
+  if (Array.isArray(rawObject)) {
+    return rawObject.map(elem => {
+      return camelCaseKeys(elem);
+    });
+  }
+
+  const keys = Object.keys(rawObject);
+
+  for (let key of keys) {
+    let newKey: string;
+
+    if (typeof calculatedObject[key] === "object") {
+      const cameledValue = camelCaseKeys(calculatedObject[key]);
+      calculatedObject[key] = cameledValue;
+    }
+
+    const trimmedKey = key.trim();
+    if (trimmedKey.length === 0) {
+      throw new Error("Wrong key value");
+    }
+
+    newKey = trimmedKey.replace(/[_.\- ]+(\w|$)/g, (_m, p1) => {
+      return p1.toUpperCase();
+    });
+
+    newKey = newKey.slice(0, 1).toLowerCase() + newKey.slice(1);
+    newObject[newKey] = calculatedObject[key];
+  }
+
+  return newObject;
+}
