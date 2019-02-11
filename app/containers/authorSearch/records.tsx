4e524b022909985aa5e19e7779b80cf6620b88f3
NohYujeong <shdbwjd0326@naver.com>
2019-02-11 19:01:32 +0900

    Add authorSearch container

---
+++ app/containers/authorSearch/records.tsx
@@ -0,0 +1,30 @@
+import { Author } from "../../model/author/author";
+
+export interface AuthorSearchState
+  extends Readonly<{
+      sort: Scinapse.ArticleSearch.SEARCH_SORT_OPTIONS;
+      isLoading: boolean;
+      pageErrorCode: number | null;
+      searchInput: string;
+      page: number;
+      numberOfElements: number;
+      totalElements: number;
+      totalPages: number;
+      isFirst: boolean;
+      isEnd: boolean;
+      searchItemsToShow: Author[];
+    }> {}
+
+export const AUTHOR_SEARCH_INITIAL_STATE: AuthorSearchState = {
+  sort: "RELEVANCE",
+  isLoading: false,
+  pageErrorCode: null,
+  searchInput: "",
+  page: 1,
+  numberOfElements: 0,
+  totalElements: 0,
+  totalPages: 0,
+  isFirst: true,
+  isEnd: false,
+  searchItemsToShow: [],
+};
