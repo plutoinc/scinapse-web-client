b6c6c1e36e752765d775453f5aa7133609562c3d
NohYujeong <shdbwjd0326@naver.com>
2019-02-11 19:02:26 +0900

    Create author type for authorSearch

---
+++ app/api/types/author.ts
@@ -0,0 +1,23 @@
+import { CancelToken } from "axios";
+import { Author } from "../../model/author/author";
+
+export interface GetAuthorsParam {
+  sort: string;
+  page: number;
+  query: string;
+  filter?: string;
+  size?: number;
+  cancelToken?: CancelToken;
+}
+
+export interface GetAuthorsResult {
+  authors: Author[];
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  sort: string | null;
+  totalElements: number;
+  totalPages: number;
+}
