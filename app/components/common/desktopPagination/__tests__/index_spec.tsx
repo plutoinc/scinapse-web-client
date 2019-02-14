69177b1bbfc4afeec07b887dc64b91cd7e52cf72
NohYujeong <shdbwjd0326@naver.com>
2019-02-14 13:59:41 +0900

    Change test code

--- app/components/common/desktopPagination/__tests__/index_spec.tsx
+++ app/components/common/desktopPagination/__tests__/index_spec.tsx
@@ -80,7 +80,7 @@ describe("DesktopPagination", () => {
       });
 
       it("should return 6~15 range result", () => {
-        expect(makePageNumberArray(mockPagination)).toEqual([6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        expect(makePageNumberArray(mockPagination)).toEqual([9, 10, 11, 12, 13, 14, 15]);
       });
     });
   });
