cad52c854f2733a1c5664ca63de6ba74511eb7f1
am2rican5 <am2rican5@gmail.com>
2019-02-15 13:27:01 +0900

    fixed sitemap regex

--- app/server/handleSitemap.tsx
+++ app/server/handleSitemap.tsx
@@ -16,34 +16,27 @@ export default async function handleSiteMapRequest(requestPath: string) {
     s3ObjKey = `${reqPathToken[reqPathToken.length - 1]}.gz`;
   }
 
-  try {
-    const body = await new Promise((resolve, reject) => {
-      s3.getObject(
-        {
-          Bucket: "scinapse-sitemap",
-          Key: s3ObjKey,
-        },
-        (err: AWS.AWSError, data: any) => {
-          if (err) {
-            console.error("Error occured while retriving sitemap object from S3", err);
-            reject(err);
-          } else {
-            resolve(data.Body.toString("base64"));
-          }
+  const body = await new Promise((resolve, reject) => {
+    s3.getObject(
+      {
+        Bucket: "scinapse-sitemap",
+        Key: s3ObjKey,
+      },
+      (err: AWS.AWSError, data: any) => {
+        if (err) {
+          console.error("Error occured while retriving sitemap object from S3", err);
+          reject(err);
+        } else {
+          resolve(data.Body.toString("base64"));
         }
-      );
-    });
+      }
+    );
+  });
 
-    return {
-      statusCode: 200,
-      headers: responseHeader,
-      isBase64Encoded: true,
-      body,
-    };
-  } catch (error) {
-    if (error.code && (error.code === "NoSuchKey" || error.code === "NoSuchBucket")) {
-      return { statusCode: 404 };
-    }
-    throw error;
-  }
+  return {
+    statusCode: 200,
+    headers: responseHeader,
+    isBase64Encoded: true,
+    body,
+  };
 }
