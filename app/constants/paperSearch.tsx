b2c7bbf5c83efc3acf65655c2722b1c231a6e7c3
NohYujeong <shdbwjd0326@naver.com>
2019-02-19 15:53:19 +0900

    Remove unused parameter

--- app/constants/paperSearch.tsx
+++ app/constants/paperSearch.tsx
@@ -8,12 +8,7 @@ export enum FILTER_TYPE_HAS_RANGE {
   JOURNAL_IF,
 }
 
-export enum FILTER_TYPE_HAS_EXPANDING_OPTION {
-  FOS,
-  JOURNAL,
-}
-
-export type FILTER_BOX_TYPE = "PUBLISHED_YEAR" | "JOURNAL_IF" | "FOS" | "JOURNAL";
+export type FILTER_BOX_TYPE = "PUBLISHED_YEAR" | "FOS" | "JOURNAL";
 
 export interface ChangeRangeInputParams {
   type: FILTER_TYPE_HAS_RANGE;
