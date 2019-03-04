e92fbb6e0e78ed761de410a987a7d7c6b008aa70
NohYujeong <shdbwjd0326@naver.com>
2019-03-04 16:18:24 +0900

    Add to authTabs logic when sign form in page

--- app/components/auth/authTabs/index.tsx
+++ app/components/auth/authTabs/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as classNames from "classnames";
 import { withStyles } from "../../../helpers/withStylesHelper";
 import { GLOBAL_DIALOG_TYPE } from "../../dialog/reducer";
+import { Link } from "react-router-dom";
 const s = require("./authTabs.scss");
 
 type AuthTabTypes = "sign in" | "sign up";
@@ -11,7 +12,34 @@ interface AuthTabsProps {
   activeTab: AuthTabTypes;
 }
 
-const AuthTabs: React.FunctionComponent<AuthTabsProps> = ({ onClickTab, activeTab }) => {
+function pageAuthTabs(props: AuthTabsProps) {
+  const { activeTab } = props;
+  return (
+    <div className={s.authTab}>
+      <Link
+        className={classNames({
+          [s.authTabItem]: true,
+          [s.active]: activeTab === "sign in",
+        })}
+        to="/users/sign_in"
+      >
+        SIGN IN
+      </Link>
+      <Link
+        className={classNames({
+          [s.authTabItem]: true,
+          [s.active]: activeTab === "sign up",
+        })}
+        to="/users/sign_up"
+      >
+        SIGN UP
+      </Link>
+    </div>
+  );
+}
+
+function dialogAuthTabs(props: AuthTabsProps) {
+  const { onClickTab, activeTab } = props;
   return (
     <div className={s.authTab}>
       <div
@@ -38,6 +66,12 @@ const AuthTabs: React.FunctionComponent<AuthTabsProps> = ({ onClickTab, activeTa
       </div>
     </div>
   );
+}
+
+const AuthTabs: React.FunctionComponent<AuthTabsProps> = props => {
+  const isDialog = !!props.onClickTab;
+
+  return isDialog ? dialogAuthTabs(props) : pageAuthTabs(props);
 };
 
 export default withStyles<typeof AuthTabs>(s)(AuthTabs);
