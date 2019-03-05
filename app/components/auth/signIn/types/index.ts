9bc2d46b775e4be86229dd786a9c0bda0dc6babc
Tylor Shin <shincode@icloud.com>
2019-03-05 13:08:12 +0900

    Remove Redux logic relevant with Sign In feature

--- app/components/auth/signIn/types/index.ts
+++ app/components/auth/signIn/types/index.ts
@@ -1,11 +1,9 @@
 import { Dispatch } from "react-redux";
 import { RouteComponentProps } from "react-router-dom";
-import { SignInState } from "../reducer";
 import { GLOBAL_DIALOG_TYPE } from "../../../dialog/reducer";
 import { OAUTH_VENDOR } from "../../../../api/types/auth";
 
 export interface SignInContainerProps extends RouteComponentProps<any> {
-  signInState: SignInState;
   dispatch: Dispatch<any>;
   handleChangeDialogType?: (type: GLOBAL_DIALOG_TYPE) => void;
 }
