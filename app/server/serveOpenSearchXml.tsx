f88fd231b48ce8d5c1aa07272783700e2571b5c3
Tylor Shin <shincode@icloud.com>
2019-03-07 16:13:20 +0900

    Change XML string(& to &amp;)

--- app/server/serveOpenSearchXml.tsx
+++ app/server/serveOpenSearchXml.tsx
@@ -1,15 +1,13 @@
 const xmlString = `
+<?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-<ShortName>Scinapse</ShortName>
-<Description>Search </Description>
-<Language>*</Language>
-<InputEncoding>UTF-8</InputEncoding>
-<Image width="16" height="16" type="image/x-icon">https://assets.pluto.network/scinapse/favicon.ico</Image>
-<Tags>scinapse academic scholar science paper medicine</Tags>
-<Contact>dev@pluto.network</Contact>
-<Url type="text/html" template="http://scinapse.io/search?query={searchTerms}&sort=RELEVANCE&filter=year%3D%3A%2Cfos%3D%2Cjournal%3D&page=1"/>
-<moz:SearchForm>https://scinapse.io</moz:SearchForm>
-<Url type="application/opensearchdescription+xml" rel="self" template="https://scinapse.io/" />
+    <ShortName>Scinapse</ShortName>
+    <Description>Search for academic information on scinapse.io</Description>
+    <InputEncoding>UTF-8</InputEncoding>
+    <Image width="16" height="16" type="image/x-icon">https://assets.pluto.network/scinapse/favicon.ico</Image>
+    <Url type="text/html" method="get" template="https://scinapse.io/search?query={searchTerms}&amp;sort=RELEVANCE&amp;filter=year%3D%3A%2Cfos%3D%2Cjournal%3D&amp;page=1"/>
+    <Url type="application/opensearchdescription+xml" rel="self" template="https://scinapse.io/opensearch.xml" />
+    <moz:SearchForm>https://scinapse.io</moz:SearchForm>
 </OpenSearchDescription>
 `;
 
