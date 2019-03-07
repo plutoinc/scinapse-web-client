128832b479307d3875e18ea8257d705a456f9ffc
Tylor Shin <shincode@icloud.com>
2019-03-07 15:26:22 +0900

    Make open search XML be served as binary

--- app/server/serveOpenSearchXml.tsx
+++ app/server/serveOpenSearchXml.tsx
@@ -1,12 +1,15 @@
 const xmlString = `
-<?xml version="1.0" encoding="UTF-8"?>
-
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
- <ShortName>Scinapse</ShortName>
- <Description>Use Scinapse Search</Description>
- <Tags>Scinapse</Tags>
- <Url type="text/html"
-      template="http://scinapse.io/search?query={searchTerms}&sort=RELEVANCE&filter=year%3D%3A%2Cfos%3D%2Cjournal%3D&page=1"/>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
+<ShortName>Scinapse</ShortName>
+<Description>Search </Description>
+<Language>*</Language>
+<InputEncoding>UTF-8</InputEncoding>
+<Image width="16" height="16" type="image/x-icon">https://assets.pluto.network/scinapse/favicon.ico</Image>
+<Tags>scinapse academic scholar science paper medicine</Tags>
+<Contact>dev@pluto.network</Contact>
+<Url type="text/html" template="http://scinapse.io/search?query={searchTerms}&sort=RELEVANCE&filter=year%3D%3A%2Cfos%3D%2Cjournal%3D&page=1"/>
+<moz:SearchForm>https://scinapse.io</moz:SearchForm>
+<Url type="application/opensearchdescription+xml" rel="self" template="https://scinapse.io/openSearchSetting.xml" />
 </OpenSearchDescription>
 `;
 
@@ -16,7 +19,7 @@ export function serveOpenSearchXML() {
     headers: {
       "Content-Type": "application/opensearchdescription+xml",
     },
-    isBase64Encoded: false,
+    isBase64Encoded: true,
     body: xmlString,
   };
 }
