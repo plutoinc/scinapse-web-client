88f5a06feb352d151a5c8b7581a4b3b4e5e1fa2e
NohYujeong <shdbwjd0326@naver.com>
2019-03-13 13:18:52 +0900

    Add emoji-mart library

--- app/helpers/htmlWrapper.tsx
+++ app/helpers/htmlWrapper.tsx
@@ -38,6 +38,7 @@ gtag('config', 'AW-817738370');
       ${gTagScript}
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet" />
         <style id="jss-server-side" type="text/css">${css}</style>
+        <link rel="stylesheet" href=" https://missive.github.io/emoji-mart/emoji-mart.css" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.css" 
         integrity="sha384-xNwWFq3SIvM4dq/1RUyWumk8nj/0KFg4TOnNcfzUU4X2gNn3WoRML69gO7waf3xh" crossorigin="anonymous">
       </head>
