4ab378ec4f8da3052e22793252fe1e0bb3c24ae1
Tylor Shin <shincode@icloud.com>
2019-03-19 13:02:23 +0900

    Change the logic that find a pdf url (#735)

--- app/helpers/getPDFLink.tsx
+++ app/helpers/getPDFLink.tsx
@@ -1,15 +1,23 @@
 import { PaperSource } from "../model/paperSource";
 
 export function getPDFLink(urls: PaperSource[]): PaperSource | undefined {
-  return (
-    urls &&
-    urls.find((paperSource: PaperSource) => {
-      return isPDFLink(paperSource.url);
-    })
-  );
+  const pdfLink = urls.find(url => url.isPdf);
+
+  if (pdfLink) {
+    return pdfLink;
+  }
+
+  // TODO: Remove below logic after done checking pdf sources
+  return urls.find((paperSource: PaperSource) => {
+    return isPDFLink(paperSource);
+  });
 }
 
-export function isPDFLink(url: string) {
+export function isPDFLink(source: PaperSource) {
+  if (source.isPdf) return true;
+
+  // TODO: Remove below logic after done checking pdf sources
+  const url = source.url;
   return (
     url.startsWith("https://arxiv.org/pdf/") ||
     (url.startsWith("http") && url.endsWith(".pdf") && !url.includes("springer"))
