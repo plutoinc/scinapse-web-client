130dc21604b9b03128f2161124d2384460c798ba
Tylor Shin <shincode@icloud.com>
2019-03-19 09:36:09 +0900

    Enable XHR cacnelation logic to ArticleSearch component

--- app/components/articleSearch/__tests__/actions_spec.tsx
+++ app/components/articleSearch/__tests__/actions_spec.tsx
@@ -18,7 +18,7 @@ import axios from "axios";
 import * as Actions from "../actions";
 import { generateMockStore } from "../../../__tests__/mockStore";
 import { ACTION_TYPES } from "../../../actions/actionTypes";
-import { GetPapersParams } from "../../../api/types/paper";
+import { SearchPapersParams } from "../../../api/types/paper";
 import { FILTER_RANGE_TYPE, FILTER_TYPE_HAS_RANGE } from "../../../constants/paperSearch";
 
 describe("articleSearch actions", () => {
@@ -72,7 +72,7 @@ describe("articleSearch actions", () => {
     const mockPage = 3;
     const mockFilter = "mockFilter";
     const mockSort = "RELEVANCE";
-    let mockParams: GetPapersParams;
+    let mockParams: SearchPapersParams;
 
     it("should return getPapers action when mode is QUERY", async () => {
       mockParams = {
@@ -82,7 +82,7 @@ describe("articleSearch actions", () => {
         cancelToken: axios.CancelToken.source().token,
         sort: mockSort,
       };
-      await store.dispatch(Actions.fetchSearchPapers(mockParams));
+      await store.dispatch(Actions.searchPapers(mockParams));
       const actions = store.getActions();
       expect(actions[0]).toEqual({
         type: ACTION_TYPES.ARTICLE_SEARCH_START_TO_GET_PAPERS,
