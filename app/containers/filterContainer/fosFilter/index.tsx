9779d4d7f8668b76f7f5d60d0ad44254f826fa8f
Tylor Shin <shincode@icloud.com>
2019-03-29 11:02:48 +0900

    make basic fos component

---
+++ app/containers/filterContainer/fosFilter/index.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import axios, { CancelTokenSource } from "axios";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { withStyles } from "../../../helpers/withStylesHelper";
+const s = require("./fosFilter.scss");
+
+interface FOSFilterBoxProps extends RouteComponentProps<any> {}
+
+interface ReducerState<T> {
+  isOpen: boolean;
+  inputValue: string;
+}
+
+interface ReducerAction<T> {
+  type: string;
+  payload?: {
+    suggestions?: T;
+    inputValue?: string;
+    errorMsg?: string;
+  };
+}
+
+function reducer<T>(state: ReducerState<T>, action: ReducerAction<T>) {
+  switch (action.type) {
+    case "OPEN_BOX":
+      return {
+        ...state,
+        isOpen: true,
+      };
+
+    case "CLOSE_BOX":
+      return {
+        ...state,
+        isOpen: false,
+      };
+
+    case "CHANGE_INPUT": {
+      if (action.payload) {
+        return {
+          ...state,
+          inputValue: action.payload.inputValue,
+        };
+      }
+      return state;
+    }
+
+    default:
+      throw new Error();
+  }
+}
+
+const FOSFilterBox: React.FunctionComponent<FOSFilterBoxProps> = props => {
+  const [state, dispatch] = React.useReducer(reducer as React.Reducer<ReducerState<any>, ReducerAction<any>>, {
+    isOpen: false,
+    inputValue: "",
+  });
+  const cancelTokenSource = React.useRef<CancelTokenSource>(axios.CancelToken.source());
+
+  React.useEffect(
+    () => {
+      return () => {
+        cancelTokenSource.current.cancel();
+        cancelTokenSource.current = axios.CancelToken.source();
+      };
+    },
+    [props.location]
+  );
+
+  return (
+    <div>
+      <input
+        value={state.inputValue}
+        onChange={e => {
+          const { value } = e.currentTarget;
+          dispatch({
+            type: "CHANGE_INPUT",
+            payload: {
+              inputValue: value,
+            },
+          });
+        }}
+        placeholder="Search Field Of Study..."
+        className={s.input}
+      />
+    </div>
+  );
+};
+
+export default withRouter(withStyles<typeof FOSFilterBox>(s)(FOSFilterBox));
