4c734b09018f6a25e53f401f505a81d5e7a59bd6
Tylor Shin <shincode@icloud.com>
2019-04-03 23:29:01 +0900

    change the way to track pageview ticket

--- app/components/articleSearch/sideEffect.tsx
+++ app/components/articleSearch/sideEffect.tsx
@@ -3,48 +3,23 @@ import { LoadDataParams } from "../../routes";
 import { searchPapers } from "./actions";
 import { SearchPapersParams } from "../../api/types/paper";
 import { ACTION_TYPES } from "../../actions/actionTypes";
-import ActionTicketManager from "../../helpers/actionTicketManager";
-import EnvChecker from "../../helpers/envChecker";
 
-export async function getSearchData(params: LoadDataParams<null>, isLocationChanged?: boolean) {
+export async function getSearchData(params: LoadDataParams<null>) {
   const { queryParams, dispatch } = params;
   const searchQueryObject: SearchPapersParams = PaperSearchQueryFormatter.makeSearchQueryFromParamsObject(queryParams);
 
   if (!searchQueryObject.query) {
-    return dispatch({
+    dispatch({
       type: ACTION_TYPES.ARTICLE_SEARCH_FAILED_TO_GET_PAPERS,
       payload: {
         statusCode: 400,
       },
     });
+    return null;
   }
 
   try {
-    const promiseArray: Array<Promise<any>> = [];
-    const searchResults = dispatch(searchPapers({ ...searchQueryObject, cancelToken: params.cancelToken }));
-    if (!EnvChecker.isOnServer() && isLocationChanged) {
-      searchResults.then(result => {
-        if (!result) {
-          ActionTicketManager.trackTicket({
-            pageType: "searchResult",
-            actionType: "fire",
-            actionArea: "paperList",
-            actionTag: "pageView",
-            actionLabel: String(0),
-          });
-        } else {
-          ActionTicketManager.trackTicket({
-            pageType: "searchResult",
-            actionType: "fire",
-            actionArea: "paperList",
-            actionTag: "pageView",
-            actionLabel: String(result.length),
-          });
-        }
-      });
-    }
-    promiseArray.push(searchResults);
-    await Promise.all(promiseArray);
+    const searchResults = await dispatch(searchPapers({ ...searchQueryObject, cancelToken: params.cancelToken }));
     return searchResults;
   } catch (err) {
     console.error(`Error for fetching search result page data`, err);
--- app/containers/authorSearch/sideEffect.tsx
+++ app/containers/authorSearch/sideEffect.tsx
@@ -3,50 +3,25 @@ import { LoadDataParams } from "../../routes";
 import { ACTION_TYPES } from "../../actions/actionTypes";
 import { fetchSearchAuthors } from "../../components/articleSearch/actions";
 import { GetAuthorsParam } from "../../api/types/author";
-import ActionTicketManager from "../../helpers/actionTicketManager";
-import EnvChecker from "../../helpers/envChecker";
 
-export async function getAuthorSearchData(params: LoadDataParams<null>, isLocationChanged?: boolean) {
+export async function getAuthorSearchData(params: LoadDataParams<null>) {
   const { queryParams, dispatch } = params;
 
   const searchQueryObject: GetAuthorsParam = PaperSearchQueryFormatter.makeSearchQueryFromParamsObject(queryParams);
 
   if (!searchQueryObject.query) {
-    return dispatch({
+    dispatch({
       type: ACTION_TYPES.ARTICLE_SEARCH_FAILED_TO_GET_AUTHORS,
       payload: {
         statusCode: 400,
       },
     });
+
+    return null;
   }
 
   try {
-    const promiseArray: Array<Promise<any>> = [];
-    const authorSearchResults = dispatch(fetchSearchAuthors(searchQueryObject));
-    if (!EnvChecker.isOnServer() && isLocationChanged) {
-      authorSearchResults.then(result => {
-        if (!result) {
-          ActionTicketManager.trackTicket({
-            pageType: "authorSearchResult",
-            actionType: "fire",
-            actionArea: "authorList",
-            actionTag: "pageView",
-            actionLabel: String(0),
-          });
-        } else {
-          ActionTicketManager.trackTicket({
-            pageType: "authorSearchResult",
-            actionType: "fire",
-            actionArea: "authorList",
-            actionTag: "pageView",
-            actionLabel: String(result.length),
-          });
-        }
-      });
-    }
-    promiseArray.push(authorSearchResults);
-
-    await Promise.all(promiseArray);
+    const authorSearchResults = await dispatch(fetchSearchAuthors(searchQueryObject));
     return authorSearchResults;
   } catch (err) {
     console.error(`Error for fetching search result page data`, err);
