c253d44d35453c62ead6391eea2d8a93d11e7fc3
Tyler Shin <shincode@icloud.com>
2019-04-11 11:22:43 +0900

    remove old paper show action bar and select the new one as default

--- app/helpers/actionTicketManager/actionTicket.tsx
+++ app/helpers/actionTicketManager/actionTicket.tsx
@@ -2,8 +2,6 @@ import * as store from "store";
 import * as format from "date-fns/format";
 import EnvChecker from "../envChecker";
 import { DEVICE_ID_KEY, SESSION_ID_KEY, USER_ID_KEY } from "../../constants/actionTicket";
-import getABType from "../getABType";
-import { FULL_PAPER_TEST } from "../../constants/abTest";
 
 export interface ActionTicketParams {
   pageType: Scinapse.ActionTicket.PageType;
@@ -51,8 +49,6 @@ export default class ActionTicket {
       this.actionArea = params.actionArea;
       this.pageType = params.pageType;
       this.actionLabel = params.actionLabel;
-      this.expName = "requestFullText";
-      this.expUser = getABType(FULL_PAPER_TEST);
     }
   }
 
