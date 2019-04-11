8d1faf457f3aeabcdcb275fda9ffd52aaf8a3cb4
Tyler Shin <shincode@icloud.com>
2019-04-11 13:52:51 +0900

    set user test environment

--- app/constants/abTest.tsx
+++ app/constants/abTest.tsx
@@ -1,6 +1,25 @@
-interface ABTest {
-  name: string;
+interface UserGroup {
+  groupName: string;
   weight: number;
 }
 
-export const LIVE_TESTS: ABTest[] = [];
+interface Test {
+  name: string;
+  userGroup: UserGroup[];
+}
+
+export const benefitTestUserGroup = [
+  { groupName: "queryCountSession", weight: 0.1224 },
+  { groupName: "refPaperCountSession", weight: 0.3673 },
+  { groupName: "getFromFirstResultPage", weight: 0.1837 },
+  { groupName: "paperviewCountDevice", weight: 0.0735 },
+  { groupName: "downloadCount", weight: 0.0864 },
+  { groupName: "control", weight: 0.1667 },
+];
+
+export const LIVE_TESTS: Test[] = [
+  {
+    name: "benefit",
+    userGroup: benefitTestUserGroup,
+  },
+];
