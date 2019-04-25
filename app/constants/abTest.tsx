c3dca7a80c271ebe0b8fbdbdc61118d93b41d711
roamcne <roamcne@gmail.com>
2019-04-25 19:04:24 +0900

    Improve weighted random selection logic

--- app/constants/abTest.tsx
+++ app/constants/abTest.tsx
@@ -34,3 +34,24 @@ export interface BenefitExpTicketContext {
 }
 
 export const LIVE_TESTS: Test[] = [];
+
+function getRandomPool(): { [key: string]: string[] } {
+  const randomPool: { [key: string]: string[] } = {};
+  LIVE_TESTS.forEach(test => {
+    test.userGroup.forEach(group => {
+      const groupRandomPool: string[] = [];
+      for (let i = 0; i < group.weight; i++) {
+        groupRandomPool.push(group.groupName);
+      }
+      randomPool[test.name] = groupRandomPool;
+    });
+  });
+  return randomPool;
+}
+
+const RANDOM_POOL = getRandomPool();
+
+export function getRandomUserGroup(testName: string): string {
+  const groupRandomPool = RANDOM_POOL[testName];
+  return groupRandomPool[Math.floor(Math.random() * groupRandomPool.length)];
+}
