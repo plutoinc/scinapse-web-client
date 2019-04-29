933f2d92f88a5f30397af3d14aeff6e43790edef
roamcne <roamcne@gmail.com>
2019-04-29 15:01:04 +0900

    Modify modal context texts

--- app/components/auth/authContextText/constants.tsx
+++ app/components/auth/authContextText/constants.tsx
@@ -3,6 +3,7 @@ export const controlSignUpContext: { [key: string]: string } = {
   citePaper: "⚠️ Oops, only scinapse members can copy citation.",
   viewMorePDF: "⚠️ Oops, only scinapse members can view full text.",
   addToCollection: "⚠️ Oops, only scinapse members can add papers to collection.",
+  clickRequestFullTextBtn: "Oops, only scinapse members can request the full text of this paper.",
   query: "⚠️ Oops, only scinapse members can search more.",
   paperShow: "⚠️ Oops, only scinapse members can view paper information more.",
   paperFromSearch: "⚠️ Oops, only scinapse members can view more papers.",
@@ -12,14 +13,15 @@ export const controlSignUpContext: { [key: string]: string } = {
 };
 
 export const positiveSignUpContext: { [key: string]: string } = {
-  downloadPdf: "🔍 To download PDF more, you need to be a Scinapse member.",
+  downloadPdf: "📜 To download PDF more, you need to be a Scinapse member.",
   citePaper: "🔍 To copy citation, you need to be a Scinapse member.",
-  viewMorePDF: "🔍 To view full text, you need to be a Scinapse member.",
+  viewMorePDF: "📜 To view full text, you need to be a Scinapse member.",
   addToCollection: "🔍 To add papers to collection, you need to be a Scinapse member.",
+  clickRequestFullTextBtn: "📜 To request the full text of this paper, you need to be a Scinapse member.",
   query: "🔍 To search more, you need to be a Scinapse member.",
-  paperShow: "🔍 To view paper information more, you need to be a Scinapse member.",
-  paperFromSearch: "🔍 To view more papers, you need to be a Scinapse member.",
+  paperShow: "📈 To view paper information more, you need to be a Scinapse member.",
+  paperFromSearch: "📜 To view more papers, you need to be a Scinapse member.",
   queryLover: "💌 Hey Scinapse lover! Please help us to fully support your research by signing up.",
-  authorFromSearch: "🔍 To view the author profile, you need to be a Scinapse member.",
-  nextPageFromSearch: "🔍 To go to the next page, you need to be a Scinapse member.",
+  authorFromSearch: "👨 To view the author profile, you need to be a Scinapse member.",
+  nextPageFromSearch: "➡ To go to the next page, you need to be a Scinapse member.",
 };
