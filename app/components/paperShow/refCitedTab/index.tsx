4e59ebed33bdd90d2d73040874b155bc5be328c7
Tylor Shin <shincode@icloud.com>
2019-05-10 15:25:24 +0900

    fix ref cited tab (#818)
    * Make TabItem component
    
    * make PDFButton and clean up code
    
    * remove duplicated RefCitedBars
    
    * fix wrong spelling

--- app/components/paperShow/refCitedTab/index.tsx
+++ app/components/paperShow/refCitedTab/index.tsx
@@ -1,48 +1,62 @@
 import * as React from "react";
 import * as classNames from "classnames";
 import { withStyles } from "../../../helpers/withStylesHelper";
-import { Paper } from "../../../model/paper";
 import CiteBox from "../../../containers/paperShowActionBar/components/citeBox";
 import PdfDownloadButton from "../components/pdfDownloadButton";
-import FullTextBtn from "../../../containers/paperShowActionBar/components/fullTextRequestBtn";
-import FullTextDialog from "../../../containers/paperShowActionBar/components/fullTextDialog";
+import RequestFullTextBtn from "../../../containers/paperShowActionBar/components/fullTextRequestBtn";
+import RequestFullTextDialog from "../../../containers/paperShowActionBar/components/fullTextDialog";
+import { PDFButtonProps, TabItemProps, PaperShowRefCitedTabProps } from "./types";
 const styles = require("./refCitedTab.scss");
 
-interface PaperShowRefCitedTabProps {
-  paper: Paper;
-  isLoadingOaCheck: boolean;
-  isFetchingPdf: boolean;
-  failedToLoadPDF: boolean;
-  isFixed: boolean;
-  isOnRef: boolean;
-  isOnCited: boolean;
-  hasBestPdf: boolean;
-  showFullText?: boolean;
-  isOnFullText?: boolean;
+const TabItem: React.FunctionComponent<TabItemProps> = props => {
+  return (
+    <li
+      className={classNames({
+        [styles.headerTabItem]: true,
+        [styles.active]: props.active,
+      })}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </li>
+  );
+};
 
-  handleClickRef: () => void;
-  handleClickCited: () => void;
-  handleClickFullText?: () => void;
-}
-
-const PaperShowRefCitedTab: React.SFC<PaperShowRefCitedTabProps> = props => {
+const PDFButton: React.FunctionComponent<PDFButtonProps> = props => {
   const [isOpen, setIsOpen] = React.useState(false);
-  const referenceCount = props.paper.referenceCount;
-  const citedCount = props.paper.citedCount;
-  let fullTextNode;
 
-  if (props.showFullText) {
-    fullTextNode = (
-      <li
-        className={classNames({
-          [styles.headerTabItem]: true,
-          [styles.active]: props.isOnFullText,
-        })}
-        onClick={props.handleClickFullText}
-      >
-        Full text
-      </li>
+  if (props.hasPDF) {
+    return (
+      <div className={styles.actionItem}>
+        <PdfDownloadButton paper={props.paper} isLoadingOaCheck={props.isLoadingOaCheck} />
+      </div>
     );
+  }
+  return (
+    <>
+      <div className={styles.actionItem}>
+        <RequestFullTextBtn
+          isLoadingOaCheck={props.isLoadingOaCheck}
+          paperId={props.paper!.id}
+          handleSetIsOpen={setIsOpen}
+          btnStyle={{ flex: "1 0 auto", height: "36px", padding: "0 12px 0 8px" }}
+        />
+      </div>
+      <RequestFullTextDialog
+        paperId={props.paper.id}
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+
+const PaperShowRefCitedTab: React.FunctionComponent<PaperShowRefCitedTabProps> = props => {
+  let fullTextNode;
+  if (props.hasFullText && props.handleClickFullTextTab) {
+    fullTextNode = <TabItem active={!!props.isOnFullText} onClick={props.handleClickFullTextTab} text="Full Text" />;
   }
 
   return (
@@ -55,51 +69,22 @@ const PaperShowRefCitedTab: React.SFC<PaperShowRefCitedTabProps> = props => {
       <div className={styles.paperContentBlockHeaderTabContentWrapper}>
         <ul className={styles.headerTabList}>
           {fullTextNode}
-          <li
-            className={classNames({
-              [styles.headerTabItem]: true,
-              [styles.active]: props.isOnRef,
-            })}
-            onClick={props.handleClickRef}
-          >
-            {`References (${referenceCount})`}
-          </li>
-          <li
-            className={classNames({
-              [styles.headerTabItem]: true,
-              [styles.active]: props.isOnCited,
-            })}
-            onClick={props.handleClickCited}
-          >
-            {`Cited By (${citedCount})`}
-          </li>
+          <TabItem
+            active={props.isOnRef}
+            onClick={props.handleClickRefTab}
+            text={`References (${props.paper.referenceCount})`}
+          />
+          <TabItem
+            active={props.isOnCited}
+            onClick={props.handleClickCitedTab}
+            text={`Citations (${props.paper.citedCount})`}
+          />
         </ul>
-
         <div className={styles.rightBtnBox}>
           <div className={styles.actionItem}>
             <CiteBox paper={props.paper} btnStyle={{ maxWidth: "74px", width: "100%", height: "36px" }} />
           </div>
-          {props.hasBestPdf && (props.isFetchingPdf || !props.failedToLoadPDF) ? (
-            <div className={styles.actionItem}>
-              <PdfDownloadButton paper={props.paper} isLoadingOaCheck={props.isLoadingOaCheck} />
-            </div>
-          ) : (
-            <div className={styles.actionItem}>
-              <FullTextBtn
-                isLoadingOaCheck={props.isLoadingOaCheck}
-                paperId={props.paper!.id}
-                handleSetIsOpen={setIsOpen}
-                btnStyle={{ flex: "1 0 auto", height: "36px", padding: "0 12px 0 8px" }}
-              />
-            </div>
-          )}
-          <FullTextDialog
-            paperId={props.paper.id}
-            isOpen={isOpen}
-            onClose={() => {
-              setIsOpen(false);
-            }}
-          />
+          <PDFButton paper={props.paper} isLoadingOaCheck={props.isLoadingOaCheck} hasPDF={props.hasFullText} />
         </div>
       </div>
     </div>
