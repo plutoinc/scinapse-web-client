0fb71d89a1c206bd7af19278d84f56333177301f
NohYujeong <shdbwjd0326@naver.com>
2019-05-17 13:30:15 +0900

    Create relatedPaperList component in papershow

---
+++ app/components/relatedPapers/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { withStyles } from "../../helpers/withStylesHelper";
+const styles = require("./relatedPapers.scss");
+
+const RelatedPapersInPaperShow: React.FC<{}> = () => {
+  return <div />;
+};
+
+export default withStyles<typeof styles>(styles)(RelatedPapersInPaperShow);
