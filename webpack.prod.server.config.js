a5e692c3f6771d9ff8e6eaa9a2da88bcd977ffd2
Tylor Shin <tylor.shin@gmail.com>
2019-05-23 15:15:57 +0900

    hot fix) change NODE_ENV option in production server build

--- webpack.prod.server.config.js
+++ webpack.prod.server.config.js
@@ -84,5 +84,6 @@ module.exports = {
       "process.env.TARGET": JSON.stringify("server"),
     }),
     new LodashModuleReplacementPlugin(),
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
   ],
 };
