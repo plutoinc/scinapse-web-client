0a0d850344a065c73dbdf932d6bdcd7122030e71
Tylor Shin <tylor.shin@gmail.com>
2019-05-29 17:57:43 +0900

    make possible new tab open to title & fix currentPage is moved when new tab is opend

--- app/components/common/paperItem/title.tsx
+++ app/components/common/paperItem/title.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { escapeRegExp } from "lodash";
 import { connect, Dispatch } from "react-redux";
-import { withRouter, RouteComponentProps } from "react-router-dom";
-import HighLightedContent from "../highLightedContent";
+import { withRouter, RouteComponentProps, Link } from "react-router-dom";
 import { withStyles } from "../../../helpers/withStylesHelper";
 import { formulaeToHTMLStr } from "../../../helpers/displayFormula";
 import actionTicketManager from "../../../helpers/actionTicketManager";
@@ -17,14 +15,20 @@ export interface TitleProps extends RouteComponentProps<any> {
   pageType: Scinapse.ActionTicket.PageType;
   shouldBlockUnverifiedUser: boolean;
   actionArea?: Scinapse.ActionTicket.ActionArea;
-  searchQueryText?: string;
   currentPage?: number;
 }
 
-class Title extends React.PureComponent<TitleProps, {}> {
+class Title extends React.Component<TitleProps> {
+  public shouldComponentUpdate(nextProps: TitleProps) {
+    if (this.props.paper.id !== nextProps.paper.id) {
+      return true;
+    }
+    return false;
+  }
+
   public render() {
-    const { paper, searchQueryText, source } = this.props;
-    const title = paper.titleHighlighted || paper.title;
+    const { paper } = this.props;
+    const title = paper.title;
 
     if (!title) {
       return null;
@@ -35,37 +39,15 @@ class Title extends React.PureComponent<TitleProps, {}> {
       .replace(/\s{2,}/g, " ")
       .replace(/#[A-Z0-9]+#/g, "");
 
-    const noSearchQueryText = !searchQueryText;
-    const searchQuery = escapeRegExp(searchQueryText);
-    if (noSearchQueryText) {
-      return (
-        <div>
-          <a href={`/papers/${paper.id}`} onClick={this.handleClickTitle} className={styles.title}>
-            <span dangerouslySetInnerHTML={{ __html: formulaeToHTMLStr(title) }} />
-          </a>
-        </div>
-      );
-    }
     return (
-      <div>
-        <HighLightedContent
-          content={trimmedTitle}
-          highLightContent={searchQuery}
-          className={styles.title}
-          onClickFunc={this.handleClickTitle}
-          href={source}
-          to={{
-            pathname: `/papers/${paper.id}`,
-          }}
-        />
-      </div>
+      <Link to={`/papers/${paper.id}`} onClick={this.handleClickTitle} className={styles.title}>
+        <span dangerouslySetInnerHTML={{ __html: formulaeToHTMLStr(trimmedTitle) }} />
+      </Link>
     );
   }
 
-  private handleClickTitle = async (e: React.MouseEvent<HTMLAnchorElement>) => {
-    const { dispatch, pageType, actionArea, paper, history } = this.props;
-    e.preventDefault();
-
+  private handleClickTitle = async () => {
+    const { dispatch, pageType, actionArea, paper } = this.props;
     actionTicketManager.trackTicket({
       pageType,
       actionType: "fire",
@@ -82,8 +64,6 @@ class Title extends React.PureComponent<TitleProps, {}> {
         })
       );
     }
-
-    history.push(`/papers/${paper.id}`);
   };
 }
 
