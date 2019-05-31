b731339180c62f66d3e1baaf9625782b63b26aa9
Tylor Shin <tylor.shin@gmail.com>
2019-05-31 14:37:55 +0900

    make sitemap route

--- server/index.tsx
+++ server/index.tsx
@@ -1,13 +1,30 @@
+import getSitemap from './routes/sitemap';
+const SITEMAP_REGEX = /^\/sitemap(\/sitemap_[0-9]+\.xml)?\/?$/;
+
 const handler = async (event: LambdaProxy.Event): Promise<LambdaProxy.Response> => {
   console.log(JSON.stringify(event, null, 2));
-  console.log(event.path);
+  const path = event.path;
+
+  if (SITEMAP_REGEX.test(path)) {
+    const res = await getSitemap(path);
+    return {
+      statusCode: 200,
+      isBase64Encoded: true,
+      headers: {
+        'Content-Encoding': 'gzip',
+        'Content-Type': 'text/xml',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: res.body,
+    };
+  }
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ error: 'Not Found' }),
+    body: JSON.stringify({ error: 'SUCCESS' }),
   };
 };
 
