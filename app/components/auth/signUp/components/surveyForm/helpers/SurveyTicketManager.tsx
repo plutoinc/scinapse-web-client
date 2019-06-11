c48fe6edc5f5ee7182cb205128579bf1659fbc9b
NohYujeong <shdbwjd0326@naver.com>
2019-06-11 19:13:09 +0900

    replace lodash omit to plain js code

--- app/components/auth/signUp/components/surveyForm/helpers/SurveyTicketManager.tsx
+++ app/components/auth/signUp/components/surveyForm/helpers/SurveyTicketManager.tsx
@@ -1,5 +1,4 @@
 import { AnswerToQuestion } from '../constants';
-import { omit, findIndex } from 'lodash';
 
 interface SurveyTicketParams {
   surveyName: string;
@@ -7,25 +6,17 @@ interface SurveyTicketParams {
 }
 
 export function SurveyTicketFormatter(rawSurveyContext: Partial<AnswerToQuestion>[]) {
-  let finalTicketContext: SurveyTicketParams[] = [];
+  let finalTicketContext: SurveyTicketParams;
 
   rawSurveyContext.forEach(survey => {
-    const targetIndex = findIndex(finalTicketContext, ['surveyName', survey.surveyName]);
-    if (targetIndex >= 0) {
-      const targetSurvey = finalTicketContext[targetIndex];
-      finalTicketContext = [
-        ...finalTicketContext.slice(0, targetIndex),
-        {
-          surveyName: targetSurvey.surveyName,
-          questions: [...targetSurvey.questions, omit(survey, 'surveyName')],
-        },
-        ...finalTicketContext.slice(targetIndex + 1, finalTicketContext.length),
-      ];
-    } else {
-      finalTicketContext = [
+    const { surveyName, ...withoutSurveyNameContext } = survey;
+    if (!!finalTicketContext) {
+      finalTicketContext = {
         ...finalTicketContext,
-        { surveyName: survey.surveyName!, questions: [omit(survey, 'surveyName')] },
-      ];
+        questions: [...finalTicketContext.questions, withoutSurveyNameContext],
+      };
+    } else {
+      finalTicketContext = { surveyName: surveyName!, questions: [withoutSurveyNameContext] };
     }
   });
 
