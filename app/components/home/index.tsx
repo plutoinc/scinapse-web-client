6f55add83dad3914a4283651332f284c01db538f
NohYujeong <shdbwjd0326@naver.com>
2019-06-18 11:20:21 +0900

    Refactoring home for apply react fc

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -16,14 +16,93 @@ const styles = require('./home.scss');
 
 const MAX_KEYWORD_SUGGESTION_LIST_COUNT = 5;
 
-export interface HomeProps extends RouteComponentProps<null> {
-  layout: LayoutState;
-  dispatch: Dispatch<any>;
+type Props = ReturnType<typeof mapStateToProps> &
+  RouteComponentProps<any> & {
+    layout: LayoutState;
+    dispatch: Dispatch<any>;
+  };
+
+function getHelmetNode() {
+  const structuredDataJSON = {
+    '@context': 'http://schema.org',
+    '@type': 'Organization',
+    url: 'https://scinapse.io',
+    logo: 'https://s3.amazonaws.com/pluto-asset/scinapse/scinapse-logo.png',
+    contactPoint: [
+      {
+        '@type': 'ContactPoint',
+        email: 'team@pluto.network',
+        url: 'https://pluto.network',
+        contactType: 'customer service',
+      },
+    ],
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: 'https://scinapse.io/search?query={search_term_string}&utm_source=google_search_result',
+      'query-input': 'required name=search_term_string',
+    },
+    sameAs: [
+      'https://www.facebook.com/PlutoNetwork',
+      'https://twitter.com/pluto_network',
+      'https://medium.com/pluto-network',
+      'https://pluto.network',
+    ],
+  };
+
+  return (
+    <Helmet script={[{ type: 'application/ld+json', innerHTML: JSON.stringify(structuredDataJSON) }]}>
+      <link rel="canonical" href="https://scinapse.io" />
+    </Helmet>
+  );
 }
 
-interface HomeState {
-  isSearchEngineMood: boolean;
+function getContainerStyle(layout: LayoutState): React.CSSProperties {
+  if (layout.userDevice !== UserDevice.DESKTOP) {
+    return { position: 'absolute', margin: '0 0 9px 0', width: '100%' };
+  } else {
+    return {};
+  }
 }
+
+const Home: React.FC<Props> = props => {
+  const [isSearchEngineMood, setIsSearchEngineMood] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsSearchEngineMood(getUserGroupName(SEARCH_ENGINE_MOOD_TEST) === 'searchEngine');
+  }, []);
+
+  const containerStyle = getContainerStyle(props.layout);
+  return (
+    <div className={styles.articleSearchFormContainer}>
+      {getHelmetNode()}
+      <h1 style={{ display: 'none' }}>Scinapse | Academic search engine for paper</h1>
+      <div className={styles.searchFormInnerContainer}>
+        <div className={styles.searchFormContainer}>
+          <div className={styles.formWrapper}>
+            <div className={styles.searchTitle}>
+              <Icon icon="SCINAPSE_HOME_LOGO" className={styles.scinapseHomeLogo} />
+            </div>
+            <div className={styles.searchSubTitle}>Academic Search Engine</div>
+            <div tabIndex={0} className={styles.searchInputForm}>
+              <SearchQueryInput
+                maxCount={MAX_KEYWORD_SUGGESTION_LIST_COUNT}
+                actionArea="home"
+                autoFocus
+                inputClassName={isSearchEngineMood ? styles.searchEngineMoodInput : styles.searchInput}
+              />
+            </div>
+            <div className={styles.searchTryKeyword} />
+            <div className={styles.searchSubTitle}>We’re better than Google Scholar. We mean it.</div>
+          </div>
+        </div>
+        <div className={styles.contentBlockDivider} />
+
+        <TrendingPaper />
+        <Footer containerStyle={containerStyle} />
+      </div>
+    </div>
+  );
+};
 
 function mapStateToProps(state: AppState) {
   return {
@@ -31,99 +110,4 @@ function mapStateToProps(state: AppState) {
   };
 }
 
-@withStyles<typeof Home>(styles)
-class Home extends React.PureComponent<HomeProps, HomeState> {
-  public constructor(props: HomeProps) {
-    super(props);
-
-    this.state = {
-      isSearchEngineMood: false,
-    };
-  }
-
-  public componentDidMount() {
-    this.setState({
-      isSearchEngineMood: getUserGroupName(SEARCH_ENGINE_MOOD_TEST) === 'searchEngine',
-    });
-  }
-
-  public render() {
-    const containerStyle = this.getContainerStyle();
-    return (
-      <div className={styles.articleSearchFormContainer}>
-        {this.getHelmetNode()}
-        <h1 style={{ display: 'none' }}>Scinapse | Academic search engine for paper</h1>
-        <div className={styles.searchFormInnerContainer}>
-          <div className={styles.searchFormContainer}>
-            <div className={styles.formWrapper}>
-              <div className={styles.searchTitle}>
-                <Icon icon="SCINAPSE_HOME_LOGO" className={styles.scinapseHomeLogo} />
-              </div>
-              <div className={styles.searchSubTitle}>Academic Search Engine</div>
-              <div tabIndex={0} className={styles.searchInputForm}>
-                <SearchQueryInput
-                  maxCount={MAX_KEYWORD_SUGGESTION_LIST_COUNT}
-                  actionArea="home"
-                  autoFocus
-                  inputClassName={this.state.isSearchEngineMood ? styles.searchEngineMoodInput : styles.searchInput}
-                />
-              </div>
-              <div className={styles.searchTryKeyword} />
-              <div className={styles.searchSubTitle}>We’re better than Google Scholar. We mean it.</div>
-            </div>
-          </div>
-          <div className={styles.contentBlockDivider} />
-
-          <TrendingPaper />
-          <Footer containerStyle={containerStyle} />
-        </div>
-      </div>
-    );
-  }
-
-  private getHelmetNode = () => {
-    const structuredDataJSON = {
-      '@context': 'http://schema.org',
-      '@type': 'Organization',
-      url: 'https://scinapse.io',
-      logo: 'https://s3.amazonaws.com/pluto-asset/scinapse/scinapse-logo.png',
-      contactPoint: [
-        {
-          '@type': 'ContactPoint',
-          email: 'team@pluto.network',
-          url: 'https://pluto.network',
-          contactType: 'customer service',
-        },
-      ],
-      potentialAction: {
-        '@type': 'SearchAction',
-        target: 'https://scinapse.io/search?query={search_term_string}&utm_source=google_search_result',
-        'query-input': 'required name=search_term_string',
-      },
-      sameAs: [
-        'https://www.facebook.com/PlutoNetwork',
-        'https://twitter.com/pluto_network',
-        'https://medium.com/pluto-network',
-        'https://pluto.network',
-      ],
-    };
-
-    return (
-      <Helmet script={[{ type: 'application/ld+json', innerHTML: JSON.stringify(structuredDataJSON) }]}>
-        <link rel="canonical" href="https://scinapse.io" />
-      </Helmet>
-    );
-  };
-
-  private getContainerStyle = (): React.CSSProperties => {
-    const { layout } = this.props;
-
-    if (layout.userDevice !== UserDevice.DESKTOP) {
-      return { position: 'absolute', margin: '0 0 9px 0', width: '100%' };
-    } else {
-      return {};
-    }
-  };
-}
-
-export default withRouter(connect(mapStateToProps)(Home));
+export default withRouter(connect(mapStateToProps)(withStyles<typeof Home>(styles)(Home)));
