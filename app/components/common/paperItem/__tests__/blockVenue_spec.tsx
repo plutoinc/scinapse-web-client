1a26cdd25e0f64b9342c752bee6b26b288c11ebb
Tylor Shin <shincode@icloud.com>
2019-06-24 11:29:23 +0900

    fix broken venue styles when venue is conference (#884)

---
+++ app/components/common/paperItem/__tests__/blockVenue_spec.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import BlockVenue from '../blockVenue';
+import { RAW } from '../../../../__mocks__';
+
+describe('BlockVenue Component', () => {
+  describe('when paper is from conference', () => {
+    it('should render correctly', () => {
+      const paper = RAW.PAPER_FROM_CONFERENCE;
+
+      const tree = renderer
+        .create(
+          <BlockVenue
+            journal={paper.journal}
+            conferenceInstance={paper.conferenceInstance}
+            publishedDate={paper.publishedDate}
+            pageType="searchResult"
+            actionArea="fakeArea"
+          />
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
