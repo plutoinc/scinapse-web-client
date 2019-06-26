2f371be58e0d359e5d154124ede6220d76ae271f
Tyler Shin <shincode@icloud.com>
2019-06-26 12:46:36 +0900

    remove user tracking information from unsigned user

--- app/middlewares/trackUser.ts
+++ app/middlewares/trackUser.ts
@@ -6,37 +6,56 @@ import EnvChecker from '../helpers/envChecker';
 import { USER_ID_KEY } from '../constants/actionTicket';
 declare var Sentry: any;
 
+function setUserToTrackers(user: CurrentUser) {
+  // set sentry
+  if (EnvChecker.isProdBrowser() && typeof Sentry !== 'undefined') {
+    Sentry.configureScope((scope: any) => {
+      scope.setUser({
+        id: user.id.toString(),
+        email: user.email,
+        username: `${user.firstName} ${user.lastName || ''}`,
+      });
+    });
+  }
+
+  // set other trackers
+  store.set(USER_ID_KEY, user.id);
+  ReactGA.set({ userId: user.id });
+}
+
+function removeUserFromTrackers() {
+  if (EnvChecker.isProdBrowser() && typeof Sentry !== 'undefined') {
+    Sentry.configureScope((scope: any) => {
+      scope.setUser({});
+    });
+  }
+
+  store.remove(USER_ID_KEY);
+  ReactGA.set({ userId: null });
+}
+
 const setUserToTracker = () => (next: any) => (action: any) => {
   try {
-    if (
-      action.type === ACTION_TYPES.SIGN_IN_SUCCEEDED_TO_SIGN_IN ||
-      action.type === ACTION_TYPES.AUTH_SUCCEEDED_TO_CHECK_LOGGED_IN
-    ) {
-      if (action.payload && action.payload.user && action.payload.user.id) {
-        const user = action.payload.user as CurrentUser;
-
-        if (EnvChecker.isProdBrowser()) {
-          Sentry.configureScope((scope: any) => {
-            scope.setUser({
-              id: user.id.toString(),
-              email: user.email,
-              username: `${user.firstName} ${user.lastName || ''}`,
-            });
-          });
+    switch (action.type) {
+      case ACTION_TYPES.SIGN_IN_SUCCEEDED_TO_SIGN_IN:
+      case ACTION_TYPES.AUTH_SUCCEEDED_TO_CHECK_LOGGED_IN: {
+        if (action.payload && action.payload.user) {
+          const user = action.payload.user as CurrentUser;
+          setUserToTrackers(user);
+        } else {
+          removeUserFromTrackers();
         }
-
-        store.set(USER_ID_KEY, user.id);
-        ReactGA.set({ userId: action.payload.user.id });
-      }
-    } else if (action.type === ACTION_TYPES.AUTH_SUCCEEDED_TO_SIGN_OUT) {
-      if (EnvChecker.isProdBrowser()) {
-        Sentry.configureScope((scope: any) => {
-          scope.setUser({});
-        });
+        break;
       }
 
-      store.remove(USER_ID_KEY);
-      ReactGA.set({ userId: null });
+      case ACTION_TYPES.AUTH_SUCCEEDED_TO_SIGN_OUT:
+      case ACTION_TYPES.AUTH_FAILED_TO_CHECK_LOGGED_IN: {
+        removeUserFromTrackers();
+        break;
+      }
+
+      default:
+        break;
     }
 
     return next(action);
