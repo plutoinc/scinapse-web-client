fc90481bb7b0565a3a4c393b3753756b4929c3a5
NohYujeong <shdbwjd0326@naver.com>
2019-07-03 01:22:14 +0900

    Fixed wrong typing text

--- app/constants/abTestGlobalValue.tsx
+++ app/constants/abTestGlobalValue.tsx
@@ -1,6 +1,6 @@
 import { ABTest } from './abTest';
 
 export const SEARCH_ITEM_IMPROVEMENT_TEST: ABTest = 'searchItemImprovement';
-export const SOURCE_DOMAIN_TEST: ABTest = 'sourceDomain;';
+export const SOURCE_DOMAIN_TEST: ABTest = 'sourceDomain';
 export const GURU_AT_SEARCH_TEST: ABTest = 'guruAtSearch';
 export const KNOWLEDGE_BASED_RECOMMEND_TEST: ABTest = 'knowledgeBasedRecommend';
