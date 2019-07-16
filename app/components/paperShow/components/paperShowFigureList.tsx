41ac163b63a4dbfa2bbfc90dcfbb1ac0227c96d7
NohYujeong <shdbwjd0326@naver.com>
2019-07-16 15:42:59 +0900

    Remove unneeded annotation

--- app/components/paperShow/components/paperShowFigureList.tsx
+++ app/components/paperShow/components/paperShowFigureList.tsx
@@ -40,13 +40,6 @@ const PaperShowFigureList: React.FC<{ paper: Paper; isMobile: boolean }> = ({ pa
 
   if (!paper || rawPaperFigures.length === 0) return null;
 
-  // const showAllBtn =
-  //   {rawPaperFigures.length > 6 && (
-  //     <button className={styles.showAllBtn} onClick={() => setShouldShowAll(!shouldShowAll)}>
-  //       {showAllBtnText}
-  //     </button>
-  //   )} ;
-
   const figureList = finalFigures.map((figure, i) => {
     return (
       <LargePaperFigure figure={figure} key={i} handleOpenFigureDetailDialog={() => openPaperFigureDetailDialog(i)} />
