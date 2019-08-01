3cf871e76784ddc60373e94bfa42c1d1a8a50b4a
Tyler Shin <shincode@icloud.com>
2019-08-01 18:56:03 +0900

    apply source domain A/B test

--- app/constants/abTestGlobalValue.tsx
+++ app/constants/abTestGlobalValue.tsx
@@ -1,6 +1,5 @@
 import { ABTest } from './abTest';
 
-export const SOURCE_DOMAIN_TEST: ABTest = 'sourceDomain';
 export const DUMMY_TEST: ABTest = 'dummy';
 export const FIGURE_TEST: ABTest = 'figure';
 export const QUERY_LOVER_EXPERIMENT = 'queryLover';
--- app/constants/abTestObject.tsx
+++ app/constants/abTestObject.tsx
@@ -1,5 +1,5 @@
 import { Test } from './abTest';
-import { SOURCE_DOMAIN_TEST, DUMMY_TEST, FIGURE_TEST, QUERY_LOVER_EXPERIMENT } from './abTestGlobalValue';
+import { DUMMY_TEST, FIGURE_TEST, QUERY_LOVER_EXPERIMENT } from './abTestGlobalValue';
 
 export const queryLover: Test = {
   name: QUERY_LOVER_EXPERIMENT,
@@ -9,11 +9,6 @@ export const queryLover: Test = {
 export const dummy: Test = {
   name: DUMMY_TEST,
   userGroup: [{ groupName: 'a', weight: 1 }, { groupName: 'b', weight: 1 }],
-};
-
-export const sourceDomain: Test = {
-  name: SOURCE_DOMAIN_TEST,
-  userGroup: [{ groupName: 'control', weight: 1 }, { groupName: 'sourceDomain', weight: 1 }],
 };
 
 export const figure: Test = {
