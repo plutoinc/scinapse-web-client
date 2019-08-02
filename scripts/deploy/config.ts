2bb05b5857f235751757addfe88789573248248f
Tylor Shin <shincode@icloud.com>
2019-08-02 14:44:09 +0900

    enhance dev zone deployment system (#947)
    * change deploy logic in dev zone
    
    * escape branch name
    
    * add server public path
    
    * change version strategy
    
    * trying to use AWS-SSM parameter store
    
    * use AWS-SSM parameter store to SSR
    
    * add IAM role
    
    * check branch name
    
    * remove server public path option
    
    * remove AWS-SDK from webpack's external options
    
    * remove unused console log
    
    * set circle ci timezone

--- scripts/deploy/config.ts
+++ scripts/deploy/config.ts
@@ -1,50 +1,6 @@
-// S3 Upload Options
-interface S3Options {
-  region: string;
-}
-
-export interface S3ClientOptions {
-  maxAsyncS3: number; // this is the default
-  s3RetryCount: number; // this is the default
-  s3RetryDelay: number; // this is the default
-  multipartUploadThreshold: number; // this is the default (20 MB)
-  multipartUploadSize: number; // this is the default (15 MB)
-  s3Options: S3Options;
-}
-
-interface S3Params {
-  Bucket: string;
-  Prefix: string;
-  CacheControl?: string;
-}
-
-export interface S3ClientUploaderDownloaderOptions {
-  localDir?: string;
-  localFile?: string;
-  s3Params: S3Params;
-  on: Function;
-  progressAmount: "string";
-  progressTotal: "string";
-}
-
-export const S3_CLIENT_OPTIONS: S3ClientOptions = {
-  maxAsyncS3: 20, // this is the default
-  s3RetryCount: 3, // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    region: "us-east-1",
-  },
-};
-
-// NOTE: Change below options following your environment
-// Local defined Constants
-export const DEPLOY_VERSION = process.env.DEPLOY_VERSION;
-export const PRODUCTION_GIT_TAG: string = "production";
-export const AWS_S3_BUCKET: string = "pluto-web-client";
-export const APP_DEST: string = "./dist/";
-export const AWS_S3_PRODUCTION_FOLDER_PREFIX: string = "production";
-export const AWS_S3_DEV_FOLDER_PREFIX: string = "dev";
-export const VERSION_FILE_NAME: string = "version";
-export const CDN_BASE_HOST: string = "https://search-bundle.pluto.network";
+export const AWS_S3_BUCKET = 'pluto-web-client';
+export const APP_DEST = './dist/';
+export const AWS_S3_PRODUCTION_FOLDER_PREFIX = 'production';
+export const AWS_S3_DEV_FOLDER_PREFIX = 'dev';
+export const CDN_BASE_HOST = 'https://search-bundle.pluto.network';
+export const AWS_SSM_PARAM_STORE_NAME = '/scinapse-web-client/dev/branch-mapper';
