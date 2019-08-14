6ce898aab17b15b1ba5de40907b93868279a3c52
Tyler Shin <shincode@icloud.com>
2019-08-14 18:21:52 +0900

    fix mobile sign in test

--- e2eTest/signIn_spec.ts
+++ e2eTest/signIn_spec.ts
@@ -19,7 +19,7 @@ describe(MOBILE_TEST_NAME, () => {
       await expect(page.$("[class^='improvedHeader_headerContainer']")).resolves.not.toBeNull();
     });
 
-    it('should render proper sign up button', async () => {
+    it('should render sign up button', async () => {
       await expect(page.$("[class^='improvedHeader_signUpButton']")).resolves.not.toBeNull();
     });
   });
@@ -41,46 +41,32 @@ describe(MOBILE_TEST_NAME, () => {
     });
   });
 
-  describe('when user sign in', () => {
-    beforeEach(async () => {
-      await page.click("[class^='improvedHeader_signUpButton']");
-
+  describe('when user try to sign in', () => {
+    it('should sign in', async () => {
       await Promise.all([
-        page.waitForSelector("[class^='firstForm_formWrapper']", { timeout: 30000 }),
+        page.waitForSelector("[class^='signIn_formWrapper']", { timeout: 30000 }),
         clickWithCapture({
           page,
           testName: MOBILE_TEST_NAME,
           caseName: 'user use sign in feature',
           actionName: 'click sign in button',
-          selector: "[class^='improvedHeader_signInButton']",
+          selector: "[class^='authTabs_authTabItem']:first-of-type",
         }),
       ]);
 
-      await expect(page.$("[class^='firstForm_formWrapper']")).resolves.not.toBeNull();
-      await page.click("[class^='authTabs_authTabItem']:first-of-type");
-      await page.click("input[name^='email']", { clickCount: 3 });
-      await page.type("input[name^='email']", '');
-      await page.click("input[name^='password']", { clickCount: 3 });
-      await page.type("input[name^='password']", '');
-    });
-
-    afterEach(async () => {
-      await page.goBack();
-    });
-
-    describe('when user click sign in button', () => {
-      it('should sign in', async () => {
-        await Promise.all([
-          clickWithCapture({
-            page,
-            testName: MOBILE_TEST_NAME,
-            caseName: 'user use sign in feature',
-            actionName: 'click sign in button',
-            selector: 'button[class^="authButton_authBtn"]:first-of-type',
-          }),
-          page.waitForNavigation(),
-        ]);
-      });
+      await page.type("input[name^='email']", 'test@test.com');
+      await page.type("input[name^='password']", 'testtest');
+      await Promise.all([
+        page.waitForSelector("[class^='improvedHeader_userDropdownChar']", { timeout: 30000 }),
+        clickWithCapture({
+          page,
+          testName: MOBILE_TEST_NAME,
+          caseName: 'user use sign in feature',
+          actionName: 'submit sign in form',
+          selector: 'button[class^="authButton_authBtn"]:first-of-type',
+        }),
+      ]);
+      await expect(page.$("[class^='improvedHeader_userDropdownChar']")).resolves.not.toBeNull();
     });
   });
 });
@@ -122,46 +108,21 @@ describe(DESKTOP_TEST_NAME, () => {
     });
   });
 
-  describe('when user sign in', () => {
-    beforeEach(async () => {
-      await page.click("[class^='improvedHeader_signInButton']");
-
+  describe('when user try to sign in', () => {
+    it('should sign in', async () => {
+      await page.type("input[name^='email']", 'test@test.com');
+      await page.type("input[name^='password']", 'testtest');
       await Promise.all([
-        page.waitForSelector("[class^='signIn_formWrapper']", { timeout: 30000 }),
         clickWithCapture({
           page,
           testName: DESKTOP_TEST_NAME,
-          caseName: 'user use sign in feature',
-          actionName: 'click sign in button',
-          selector: "[class^='improvedHeader_signInButton']",
+          caseName: 'user use search feature',
+          actionName: 'submit search form',
+          selector: 'button[class^="authButton_authBtn"]',
         }),
+        page.waitForSelector("[class^='improvedHeader_userDropdownChar']", { timeout: 30000 }),
       ]);
-
-      await expect(page.$("[class^='signIn_formWrapper']")).resolves.not.toBeNull();
-
-      await page.click("input[name^='email']", { clickCount: 3 });
-      await page.type("input[name^='email']", '');
-      await page.click("input[name^='password']", { clickCount: 3 });
-      await page.type("input[name^='password']", '');
-    });
-
-    afterEach(async () => {
-      await page.goBack();
-    });
-
-    describe('when user click sign in button', () => {
-      it('should sign in', async () => {
-        await Promise.all([
-          clickWithCapture({
-            page,
-            testName: DESKTOP_TEST_NAME,
-            caseName: 'user use search feature',
-            actionName: 'click search icon',
-            selector: 'button[class^="authButton_authBtn"]:first-of-type',
-          }),
-          page.waitForNavigation(),
-        ]);
-      });
+      await expect(page.$("[class^='improvedHeader_userDropdownChar']")).resolves.not.toBeNull();
     });
   });
 });
