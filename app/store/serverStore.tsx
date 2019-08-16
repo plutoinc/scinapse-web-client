4830879e076a72160430f08c72692fc11449e244
Tyler Shin <shincode@icloud.com>
2019-08-16 15:34:43 +0900

    change server's createStore method & remove unneeded redux-thunk dependency from package.json

--- app/store/serverStore.tsx
+++ app/store/serverStore.tsx
@@ -1,13 +1,12 @@
-import { createStore, compose, applyMiddleware, AnyAction } from 'redux';
-import thunk, { ThunkMiddleware, ThunkDispatch } from 'redux-thunk';
-import { rootReducer, initialState, AppState } from '../reducers';
+import { configureStore } from 'redux-starter-kit';
+import { rootReducer, initialState } from '../reducers';
 
 export default class ServerStoreManager {
   public static getStore() {
-    return createStore<AppState, AnyAction, { dispatch: ThunkDispatch<AppState, undefined, AnyAction> }, {}>(
-      rootReducer,
-      initialState,
-      compose(applyMiddleware(thunk as ThunkMiddleware<AppState>))
-    );
+    return configureStore({
+      reducer: rootReducer,
+      devTools: false,
+      preloadedState: initialState,
+    });
   }
 }
