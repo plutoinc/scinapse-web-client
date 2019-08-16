79e73f322d3d4d45278776713b6720e2a4cd4bd7
Tylor Shin <shincode@icloud.com>
2019-08-16 14:53:29 +0900

    add basic E2E test (#958)
    * set E2E test environment & make basic home page test
    
    * test e2e test applied deployment
    
    * add puppeteer orbs
    
    * revert deployment script
    
    * add click with capture helper method
    
    * Add home mobile layout e2e test
    
    * Fixed title contains wrong text
    
    * Add paper show e2e test script
    
    * WIP - e2e script
    
    * fix mobile sign in test
    
    * change waitFor option to natural option
    
    * change waitFor logic to real world waiting

---
+++ e2eTest/paperShow_spec.ts
@@ -0,0 +1,68 @@
+import getHost from './helpers/getHost';
+import clickWithCapture from './helpers/clickWithCapture';
+
+const DESKTOP_TEST_NAME = 'desktop paper show page test';
+const MOBILE_TEST_NAME = 'mobile paper show page test';
+
+function paperShowE2E(TEST_NAME: string, width: number, height: number) {
+  describe(TEST_NAME, () => {
+    beforeAll(async () => {
+      await page.setViewport({ width, height });
+      await page.goto(`https://${getHost()}/papers/2559394418`, { waitUntil: 'networkidle0' });
+    });
+
+    describe('when enter the page', () => {
+      it('should render proper title', async () => {
+        await expect(page.title()).resolves.toMatch(
+          '[PDF] Quantum Machine Learning | Scinapse | Academic search engine for paper'
+        );
+      });
+
+      it('should render the search input element', async () => {
+        await expect(page.$("input[class^='improvedHeader_searchInput']")).resolves.not.toBeNull();
+      });
+    });
+
+    describe('when user use search feature', () => {
+      beforeEach(async () => {
+        await page.click("input[class^='improvedHeader_searchInput']", { clickCount: 3 });
+        await page.type("input[class^='improvedHeader_searchInput']", 'cern');
+      });
+
+      afterEach(async () => {
+        await page.goBack();
+      });
+
+      describe('when user click the search icon', () => {
+        it('should show the search result page', async () => {
+          await Promise.all([
+            page.waitForSelector("[class^='searchList_searchItems']", { timeout: 30000 }),
+            clickWithCapture({
+              page,
+              testName: TEST_NAME,
+              caseName: 'user use search feature',
+              actionName: 'click search icon',
+              selector: "[class^='searchQueryInput_searchIconButton']",
+            }),
+          ]);
+
+          await expect(page.$("[class^='searchList_searchItems']")).resolves.not.toBeNull();
+        });
+      });
+
+      describe('when user press the enter key', () => {
+        it('should show the search result page', async () => {
+          await Promise.all([
+            page.waitForSelector("[class^='searchList_searchItems']", { timeout: 30000 }),
+            page.keyboard.press('Enter'),
+          ]);
+
+          await expect(page.$("[class^='searchList_searchItems']")).resolves.not.toBeNull();
+        });
+      });
+    });
+  });
+}
+
+paperShowE2E(DESKTOP_TEST_NAME, 1920, 1080);
+paperShowE2E(MOBILE_TEST_NAME, 320, 568);
---
+++ e2eTest/searchResult_spec.ts
@@ -0,0 +1,67 @@
+import getHost from './helpers/getHost';
+import clickWithCapture from './helpers/clickWithCapture';
+
+const DESKTOP_TEST_NAME = 'desktop search result page test';
+const MOBILE_TEST_NAME = 'mobile search result page test';
+
+function searchResultE2E(TEST_NAME: string, width: number, height: number) {
+  describe(TEST_NAME, () => {
+    beforeAll(async () => {
+      await page.setViewport({ width, height });
+      await page.goto(`https://${getHost()}/search?query=machine%20learning`, { waitUntil: 'networkidle0' });
+    });
+
+    describe('when enter the page', () => {
+      it('should render proper title', async () => {
+        await expect(page.title()).resolves.toMatch('machine learning | Scinapse | Academic search engine for paper');
+      });
+
+      it('should render proper search result', async () => {
+        await expect(page.$("[class^='searchList_searchItems']")).resolves.not.toBeNull();
+      });
+
+      it('should render proper header', async () => {
+        await expect(page.$("[class^='improvedHeader_headerContainer']")).resolves.not.toBeNull();
+      });
+    });
+
+    describe('when user use search feature', () => {
+      beforeEach(async () => {
+        await page.click("input[class^='improvedHeader_searchInput']", { clickCount: 3 });
+        await page.type("input[class^='improvedHeader_searchInput']", 'cern');
+      });
+
+      afterEach(async () => {
+        await page.goBack();
+      });
+
+      describe('when user click the search icon', () => {
+        it('should show the search result page', async () => {
+          await Promise.all([
+            page.waitFor(3000),
+            clickWithCapture({
+              page,
+              testName: TEST_NAME,
+              caseName: 'user use search feature',
+              actionName: 'click search icon',
+              selector: "[class^='searchQueryInput_searchIconButton']",
+            }),
+          ]);
+
+          await expect(page.$("[class^='searchList_searchItems']")).resolves.not.toBeNull();
+        });
+      });
+
+      describe('when user press the enter key', () => {
+        it('should show the search result page', async () => {
+          await Promise.all([page.waitFor(10000), page.keyboard.press('Enter')]);
+
+          await expect(page.$("[class^='searchList_searchItems']")).resolves.not.toBeNull();
+        });
+      });
+    });
+  });
+}
+
+searchResultE2E(DESKTOP_TEST_NAME, 1920, 1080);
+searchResultE2E(MOBILE_TEST_NAME, 320, 568);
