688874494f313eed968d29dae50f96c6900fa8ed
kimyukyeong <kykkjh2984@gmail.com>
2019-08-23 14:14:44 +0900

    Add medium style of global button component

--- app/components/common/button/button.tsx
+++ app/components/common/button/button.tsx
@@ -7,16 +7,29 @@ interface ButtonProps {
   size?: 'small' | 'medium' | 'large';
   variant?: 'text' | 'outlined' | 'contained';
   color?: 'blue' | 'gray' | 'black';
-  width?: 'full-width' | 'default';
-  state?: 'disabled' | 'active';
+  width?: 'full-width';
+  state?: 'disabled';
   onClick?: ((e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => void);
+  href?: '';
 }
 
 class Button extends React.PureComponent<ButtonProps, {}> {
   public render() {
     let cx = classNames.bind(styles);
     let className = cx(this.props.size, this.props.variant, this.props.color, this.props.state);
-    return <button className={className}>{this.props.children}</button>;
+
+    if (this.props.href) {
+      return (
+        <link className={className} href={this.props.href}>
+          {this.props.children}
+        </link>
+      );
+    }
+    return (
+      <button className={className} onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
