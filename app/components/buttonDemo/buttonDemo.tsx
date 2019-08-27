5e7912ad7d4aa0b87f60d6665e4fff36a9156b57
kimyukyeong <kykkjh2984@gmail.com>
2019-08-27 12:08:40 +0900

    Edit button show case & Change icon size of large button

--- app/components/buttonDemo/buttonDemo.tsx
+++ app/components/buttonDemo/buttonDemo.tsx
@@ -4,20 +4,95 @@ import Icon from '../../icons';
 
 const ButtonDemo: React.FC = () => {
   const availableSizes: ButtonSize[] = ['small', 'medium', 'large'];
-  const availableVariant: ButtonVariant[] = ['text', 'outlined', 'contained'];
+  const availableVariant: ButtonVariant[] = ['contained', 'outlined', 'text'];
   const availableColor: ButtonColor[] = ['blue', 'gray', 'black'];
 
-  const buttons = availableSizes.map(size => {
-    return availableVariant.map(variant => {
-      return availableColor.map(color => {
+  const basicButtons = availableVariant.map(variant => {
+    return availableColor.map(color => {
+      return availableSizes.map(size => {
         return (
           <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
             <Button size={size} variant={variant} color={color}>
               <Icon icon="BOOKMARK" />
               <span>Bookmark</span>
             </Button>
-            <small style={{ display: 'block', textAlign: 'center', marginTop: '2px' }}>
-              {`${size}/${variant}/${color}`}
+            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+              {`basic button : ${size}/${variant}/${color}`}
+            </small>
+          </div>
+        );
+      });
+    });
+  });
+
+  const onlyTextButtons = availableVariant.map(variant => {
+    return availableColor.map(color => {
+      return availableSizes.map(size => {
+        return (
+          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
+            <Button size={size} variant={variant} color={color}>
+              <span>Bookmark</span>
+            </Button>
+            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+              {`only text button : ${size}/${variant}/${color}`}
+            </small>
+          </div>
+        );
+      });
+    });
+  });
+
+  const onlyIconButtons = availableVariant.map(variant => {
+    return availableColor.map(color => {
+      return availableSizes.map(size => {
+        return (
+          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
+            <Button size={size} variant={variant} color={color}>
+              <Icon icon="BOOKMARK" />
+            </Button>
+            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+              {`only icon button : ${size}/${variant}/${color}`}
+            </small>
+          </div>
+        );
+      });
+    });
+  });
+
+  const basicFullWidthButtons = availableVariant.map(variant => {
+    return availableColor.map(color => {
+      return availableSizes.map(size => {
+        return (
+          <div
+            style={{ margin: '10px 0', textAlign: 'center', width: '200px', display: 'inline' }}
+            key={size + variant + color}
+          >
+            <Button size={size} variant={variant} color={color} fullWidth={true}>
+              <Icon icon="BOOKMARK" />
+              <span>Bookmark</span>
+            </Button>
+            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+              {` basic full-width button : ${size}/${variant}/${color}`}
+            </small>
+          </div>
+        );
+      });
+    });
+  });
+
+  const onlyTextFullWidthButtons = availableVariant.map(variant => {
+    return availableColor.map(color => {
+      return availableSizes.map(size => {
+        return (
+          <div
+            style={{ margin: '10px 0', textAlign: 'center', width: '200px', display: 'inline' }}
+            key={size + variant + color}
+          >
+            <Button size={size} variant={variant} color={color} fullWidth={true}>
+              <span>Bookmark</span>
+            </Button>
+            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+              {`only text full-width button : ${size}/${variant}/${color}`}
             </small>
           </div>
         );
@@ -35,7 +110,11 @@ const ButtonDemo: React.FC = () => {
         flexDirection: 'column',
       }}
     >
-      {buttons}
+      {basicButtons}
+      {onlyTextButtons}
+      {onlyIconButtons}
+      {basicFullWidthButtons}
+      {onlyTextFullWidthButtons}
     </div>
   );
 };
