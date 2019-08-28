8b74da6968f69c0fdbe76717057031516f6d6d0e
SangHyuk Henry Jung <onlymytho@gmail.com>
2019-08-28 23:05:09 +0900

    Optimize button rendering loop

--- app/components/buttonDemo/buttonDemo.tsx
+++ app/components/buttonDemo/buttonDemo.tsx
@@ -1,137 +1,105 @@
 import React from 'react';
 import Button, { ButtonSize, ButtonVariant, ButtonColor } from '../common/button/button';
+import copySelectedTextToClipboard from '../../helpers/copySelectedTextToClipboard';
 import Icon from '../../icons';
 
 const ButtonDemo: React.FC = () => {
   const availableSizes: ButtonSize[] = ['small', 'medium', 'large'];
   const availableVariant: ButtonVariant[] = ['contained', 'outlined', 'text'];
   const availableColor: ButtonColor[] = ['blue', 'gray', 'black'];
+  const availableFullWidth = [false, true];
+  const availableDisabled = [false, true];
+  const availableIconDisplay = ['left', 'right', 'only', 'no'];
 
-  const basicButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
-            <Button type="button" size={size} variant={variant} color={color}>
-              <Icon icon="BOOKMARK" />
-              <span>Bookmark</span>
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {`basic button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
+  const copyButtonCode = () => {
+    // 이 부분 어떻게 짜는지 모르겠음.
+    // 버튼 클릭하면 해당 버튼의 Rendering 코드를 clipboard에 copy하는 함수임.
+    alert('Copied: ' + this.target);
+    copySelectedTextToClipboard(this.target);
+  };
+
+  const allButtons = availableDisabled.map(disabled => {
+    return availableFullWidth.map(fullWidth => {
+      return availableIconDisplay.map(iconDisplay => {
+        return availableSizes.map(size => {
+          return availableColor.map(color => {
+            return availableVariant.map(variant => {
+              return (
+                <div style={{ margin: '16px 0' }} key={size + variant + color}>
+                  <div style={{ display: 'inline-block', width: '200px' }}>
+                    {/* 이 부분 깔끔하게 바꾸면 좋겠음.. */}
+                    {(() => {
+                      switch (iconDisplay) {
+                        case 'left':
+                          return (
+                            <Button
+                              onClick={copyButtonCode}
+                              type="button"
+                              size={size}
+                              variant={variant}
+                              color={color}
+                              fullWidth={fullWidth}
+                              disabled={disabled}
+                            >
+                              <Icon icon="BOOKMARK" />
+                              <span>Bookmark</span>
+                            </Button>
+                          );
+                        case 'right':
+                          return (
+                            <Button
+                              onClick={copyButtonCode}
+                              type="button"
+                              size={size}
+                              variant={variant}
+                              color={color}
+                              fullWidth={fullWidth}
+                              disabled={disabled}
+                            >
+                              <span>Bookmark</span>
+                              <Icon icon="BOOKMARK" />
+                            </Button>
+                          );
+                        case 'only':
+                          return (
+                            <Button
+                              onClick={copyButtonCode}
+                              type="button"
+                              size={size}
+                              variant={variant}
+                              color={color}
+                              fullWidth={false}
+                              disabled={disabled}
+                            >
+                              <Icon icon="BOOKMARK" />
+                            </Button>
+                          );
+                        case 'no':
+                          return (
+                            <Button
+                              onClick={copyButtonCode}
+                              type="button"
+                              size={size}
+                              variant={variant}
+                              color={color}
+                              fullWidth={fullWidth}
+                              disabled={disabled}
+                            >
+                              <span>Bookmark</span>
+                            </Button>
+                          );
+                      }
+                    })()}
+                  </div>
+                  <small style={{ display: 'inline-block', textAlign: 'center', margin: '8px', color: '#666666' }}>
+                    {`${size}/${variant}/${color}/fullWidth=${fullWidth}/disalbed=${disabled}/icon=${iconDisplay}`}
+                  </small>
+                </div>
+              );
+            });
+          });
+        });
       });
-    });
-  });
-
-  const rightIconBasicButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
-            <Button type="button" size={size} variant={variant} color={color}>
-              <span>Bookmark</span>
-              <Icon icon="BOOKMARK" />
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {`basic button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
-      });
-    });
-  });
-
-  const onlyTextButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
-            <Button type="button" size={size} variant={variant} color={color}>
-              <span>Bookmark</span>
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {`only text button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
-      });
-    });
-  });
-
-  const onlyIconButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div style={{ margin: '10px 0', textAlign: 'center' }} key={size + variant + color}>
-            <Button type="button" size={size} variant={variant} color={color}>
-              <Icon icon="BOOKMARK" />
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {`only icon button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
-      });
-    });
-  });
-
-  const basicFullWidthButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div
-            style={{ margin: '10px 0', textAlign: 'center', width: '200px', display: 'inline' }}
-            key={size + variant + color}
-          >
-            <Button type="button" size={size} variant={variant} color={color} fullWidth={true}>
-              <Icon icon="BOOKMARK" />
-              <span>Bookmark</span>
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {` basic full-width button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
-      });
-    });
-  });
-
-  const onlyTextFullWidthButtons = availableVariant.map(variant => {
-    return availableColor.map(color => {
-      return availableSizes.map(size => {
-        return (
-          <div
-            style={{ margin: '10px 0', textAlign: 'center', width: '200px', display: 'inline' }}
-            key={size + variant + color}
-          >
-            <Button type="button" size={size} variant={variant} color={color} fullWidth={true}>
-              <Icon icon="BOOKMARK" />
-              <span>Sign up</span>
-            </Button>
-            <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-              {`only text full-width button : ${size}/${variant}/${color}`}
-            </small>
-          </div>
-        );
-      });
-    });
-  });
-
-  const disbaledButtons = availableVariant.map(variant => {
-    return availableSizes.map(size => {
-      return (
-        <div style={{ margin: '10px 0', textAlign: 'center', width: '200px', display: 'inline' }} key={size + variant}>
-          <Button type="button" size={size} variant={variant} disabled={true}>
-            <Icon icon="BOOKMARK" />
-            <span>Confirm</span>
-          </Button>
-          <small style={{ display: 'block', textAlign: 'center', margin: '8px', color: '#666666' }}>
-            {`disabled button : ${size}/${variant}`}
-          </small>
-        </div>
-      );
     });
   });
 
@@ -139,19 +107,12 @@ const ButtonDemo: React.FC = () => {
     <div
       style={{
         margin: '100px auto',
-        width: '1200px',
+        width: '600px',
         display: 'flex',
-        alignItems: 'center',
         flexDirection: 'column',
       }}
     >
-      {basicButtons}
-      {rightIconBasicButtons}
-      {onlyTextButtons}
-      {onlyIconButtons}
-      {basicFullWidthButtons}
-      {onlyTextFullWidthButtons}
-      {disbaledButtons}
+      {allButtons}
     </div>
   );
 };
