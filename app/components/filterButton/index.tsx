fd5e58ca5b9dc95d828528d5874643a3da8efa83
NohYujeong <shdbwjd0326@naver.com>
2019-09-09 11:03:03 +0900

    Apply filter button color change ab test in filter button component

--- app/components/filterButton/index.tsx
+++ app/components/filterButton/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import classNames from 'classnames';
 import { withStyles } from '../../helpers/withStylesHelper';
 import Icon from '../../icons';
+import { getUserGroupName } from '../../helpers/abTestHelper';
+import { FILTER_BUTTON_COLOR_EXPERIMENT } from '../../constants/abTestGlobalValue';
 const s = require('./filterButton.scss');
 
 export enum FILTER_BUTTON_TYPE {
@@ -18,10 +20,18 @@ export interface FilterButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const FilterButton: React.FC<FilterButtonProps> = ({ content, onClick, isActive, selected }) => {
+  const [applyButtonColor, setApplyButtonColor] = React.useState(false);
+
+  React.useEffect(() => {
+    const doChangeButtonColor = getUserGroupName(FILTER_BUTTON_COLOR_EXPERIMENT) === 'gray30';
+    setApplyButtonColor(doChangeButtonColor);
+  }, []);
+
   return (
     <button
       className={classNames({
-        [s.filterBtn]: !isActive,
+        [s.filterBtn]: !isActive && !applyButtonColor,
+        [s.grayFilterBtn]: !isActive && applyButtonColor,
         [s.active]: isActive,
         [s.selected]: selected,
       })}
