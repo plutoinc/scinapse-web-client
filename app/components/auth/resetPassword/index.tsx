ca6d416d8bfd9743366a748bc8f9840abf344df9
NohYujeong <shdbwjd0326@naver.com>
2019-09-17 10:53:32 +0900

    Change button component url

--- app/components/auth/resetPassword/index.tsx
+++ app/components/auth/resetPassword/index.tsx
@@ -8,7 +8,7 @@ import { withStyles } from '../../../helpers/withStylesHelper';
 import alertToast from '../../../helpers/makePlutoToastAction';
 import PlutoAxios from '../../../api/pluto';
 import { MINIMUM_PASSWORD_LENGTH } from '../../../constants/auth';
-import Button from '../../common/button/button';
+import Button from '../../common/button';
 const styles = require('./resetPassword.scss');
 
 interface FormValues {
