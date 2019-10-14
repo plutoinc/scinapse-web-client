d6f246913e5be7d8120969c955612c3a65884e48
SangHyuk Henry Jung <onlymytho@gmail.com>
2019-10-14 15:40:00 +0900

    fix icon styles

--- app/containers/paperShowActionBar/components/citeBox.tsx
+++ app/containers/paperShowActionBar/components/citeBox.tsx
@@ -37,7 +37,7 @@ const CiteBox: React.FunctionComponent<CiteBoxProps> = props => {
       }}
     >
       <div>
-        <Icon icon={'CITATION_QUOTE'} className={s.citeIcon} />
+        <Icon icon={'CITATION'} className={s.citeIcon} />
         <span>Cite</span>
       </div>
     </div>
