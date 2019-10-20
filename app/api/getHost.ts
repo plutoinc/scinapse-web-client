7d93a84dbdb49687d9727a21ad87e10bc16fa718
NohYujeong <shdbwjd0326@naver.com>
2019-10-20 19:38:01 +0900

    Apply prod api

--- app/api/getHost.ts
+++ app/api/getHost.ts
@@ -1,12 +1,12 @@
 import EnvChecker from '../helpers/envChecker';
 
 const PROD_API_HOST = 'https://api.scinapse.io'; // This API HOST is used for a REAL service.
-const DEV_API_HOST = 'https://dev-api.scinapse.io'; // This API Host is used for DEV, Stage service.
+// const DEV_API_HOST = 'https://dev-api.scinapse.io'; // This API Host is used for DEV, Stage service.
 
 export default function getAPIHost() {
   if (EnvChecker.isLocal() || EnvChecker.isLocalServer()) {
-    return DEV_API_HOST;
-    // return PROD_API_HOST;
+    // return DEV_API_HOST;
+    return PROD_API_HOST;
   } else if (EnvChecker.isDev()) {
     return PROD_API_HOST;
   } else {
