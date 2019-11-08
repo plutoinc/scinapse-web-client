ab73347887686871de077ab105a185b8000deff4
Tyler Shin <shincode@icloud.com>
2019-11-08 11:56:26 +0900

    change useObserver dependencies

--- app/hooks/useIntersectionHook.tsx
+++ app/hooks/useIntersectionHook.tsx
@@ -5,27 +5,24 @@ import { ActionTicketParams } from '../helpers/actionTicketManager/actionTicket'
 export function useObserver(threshold: number | number[] | undefined, ticketParams: ActionTicketParams) {
   const elRef = React.useRef<HTMLDivElement | null>(null);
 
-  React.useEffect(
-    () => {
-      const intersectionObserver = new IntersectionObserver(
-        (entries, observer) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              ActionTicketManager.trackTicket(ticketParams);
-              observer.unobserve(entry.target);
-            }
-          });
-        },
-        { threshold }
-      );
+  React.useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            ActionTicketManager.trackTicket(ticketParams);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold }
+    );
 
-      if (elRef.current) {
-        intersectionObserver.observe(elRef.current);
-      }
+    if (elRef.current) {
+      intersectionObserver.observe(elRef.current);
+    }
 
-      return () => intersectionObserver.disconnect();
-    },
-    [ticketParams, threshold]
-  );
+    return () => intersectionObserver.disconnect();
+  }, []);
   return { elRef };
 }
