4549de2cca79540bb4933085fc91ee977baf3e31
NohYujeong <shdbwjd0326@naver.com>
2019-12-12 11:35:46 +0900

    Change logo context

--- app/components/layouts/improvedHeader.tsx
+++ app/components/layouts/improvedHeader.tsx
@@ -228,7 +228,7 @@ class ImprovedHeader extends React.PureComponent<HeaderProps, HeaderStates> {
           <Icon icon="SCINAPSE_LOGO_SMALL" />
           <div className={styles.instituteLogo}>
             <div className={styles.serviceName}>scinapse</div>
-            <div className={styles.instituteName}>for {currentUserState.ipInstitute.nameAbbrev}</div>
+            <div className={styles.instituteName}>{currentUserState.ipInstitute.nameAbbrev}</div>
           </div>
         </Link>
       );
