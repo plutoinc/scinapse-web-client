ba7a9d76f4b4ad546096fbd2715b33b4f54f6bea
NohYujeong <shdbwjd0326@naver.com>
2019-12-12 10:32:34 +0900

    Change reducer logic in currentUser

--- app/reducers/currentUser.tsx
+++ app/reducers/currentUser.tsx
@@ -3,7 +3,6 @@ import { CURRENT_USER_INITIAL_STATE, CurrentUser } from '../model/currentUser';
 
 export function reducer(state: CurrentUser = CURRENT_USER_INITIAL_STATE, action: ReduxAction<any>): CurrentUser {
   switch (action.type) {
-    case ACTION_TYPES.AUTH_SUCCEEDED_TO_CHECK_LOGGED_IN:
     case ACTION_TYPES.SIGN_IN_SUCCEEDED_TO_SIGN_IN: {
       return {
         ...action.payload.user,
@@ -36,6 +35,24 @@ export function reducer(state: CurrentUser = CURRENT_USER_INITIAL_STATE, action:
 
     case ACTION_TYPES.EMAIL_VERIFICATION_SUCCEEDED_TO_VERIFY_TOKEN: {
       return { ...state, emailVerified: true };
+    }
+
+    case ACTION_TYPES.AUTH_SUCCEEDED_TO_CHECK_LOGGED_IN: {
+      if (action.payload.loggedIn) {
+        return {
+          ...action.payload.user,
+          isLoggedIn: action.payload.loggedIn,
+          oauthLoggedIn: action.payload.oauthLoggedIn,
+          ipInstitute: action.payload.ipInstitute,
+          isLoggingIn: false,
+        };
+      }
+
+      return {
+        ...state,
+        ipInstitute: action.payload.ipInstitute,
+        isLoggingIn: false,
+      };
     }
 
     case ACTION_TYPES.CONNECTED_AUTHOR_SHOW_SUCCEEDED_TO_UPDATE_PROFILE_IMAGE_DATA: {
