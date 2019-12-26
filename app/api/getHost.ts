a13f5ae9bc8a3f5db350af839827fe68d4c741cb
Tyler Shin <shincode@icloud.com>
2019-12-26 14:12:05 +0900

    change API prefix

--- app/api/getHost.ts
+++ app/api/getHost.ts
@@ -1,6 +1,6 @@
 import EnvChecker from '../helpers/envChecker';
 
-const PROD_API_HOST = 'https://api.scinapse.io'; // This API HOST is used for a REAL service.
+const PROD_API_HOST = 'https://scinapse.io/api'; // This API HOST is used for a REAL service.
 // const STAGE_API_HOST = 'https://stage-api.scinapse.io:8443';
 // const DEV_API_HOST = 'https://dev-api.scinapse.io'; // This API Host is used for DEV, Stage service.
 const LOCAL_API_HOST = 'http://localhost:3000/api';
