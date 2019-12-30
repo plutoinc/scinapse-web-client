4239f915d16ab732607c43d5704b8e4df877f97d
NohYujeong <shdbwjd0326@naver.com>
2019-12-30 11:09:57 +0900

    Wrapped popupContentBanner for NoSsr

--- app/components/popupConsentBanner/index.tsx
+++ app/components/popupConsentBanner/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
+import NoSsr from '@material-ui/core/NoSsr';
 import { Button } from '@pluto_network/pluto-design-elements';
 const useStyles = require('isomorphic-style-loader/useStyles');
 const s = require('./popupConsentBanner.scss');
@@ -21,30 +22,32 @@ const PopupConsentBanner: React.FC = () => {
   if (acceptCookie === 'true') return null;
 
   return (
-    <div className={s.bannerWrapper}>
-      <div className={s.bannerText}>
-        <div className={s.title}>This website uses cookies.</div>
-        <div className={s.context}>
-          We use cookies to improve your online experience. By continuing to use our website we assume you agree to the
-          placement of these cookies.<br />
-          To learn more, you can find in our{' '}
-          <a className={s.link} href="https://scinapse.io/privacy-policy">
-            Privacy Policy.
-          </a>
+    <NoSsr>
+      <div className={s.bannerWrapper}>
+        <div className={s.bannerText}>
+          <div className={s.title}>This website uses cookies.</div>
+          <div className={s.context}>
+            We use cookies to improve your online experience. By continuing to use our website we assume you agree to
+            the placement of these cookies.<br />
+            To learn more, you can find in our{' '}
+            <a className={s.link} href="https://scinapse.io/privacy-policy">
+              Privacy Policy.
+            </a>
+          </div>
+        </div>
+        <div className={s.bannerButton}>
+          <Button
+            elementType="button"
+            onClick={() => {
+              setAcceptCookie('true');
+              Cookies.set(ACCEPT_COOKIE_KEY, 'true', { expires: 365 });
+            }}
+          >
+            Accept
+          </Button>
         </div>
       </div>
-      <div className={s.bannerButton}>
-        <Button
-          elementType="button"
-          onClick={() => {
-            setAcceptCookie('true');
-            Cookies.set(ACCEPT_COOKIE_KEY, 'true', { expires: 365 });
-          }}
-        >
-          Accept
-        </Button>
-      </div>
-    </div>
+    </NoSsr>
   );
 };
 
