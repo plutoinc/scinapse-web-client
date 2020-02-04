696d4f00a9581afd120737a9fef22e6bbc59aa3f
NohYujeong <shdbwjd0326@naver.com>
2020-02-04 10:12:23 +0900

    Remove unused parameter

--- app/components/layouts/types/header.tsx
+++ app/components/layouts/types/header.tsx
@@ -4,10 +4,8 @@ import { LayoutState } from '../reducer';
 import { CurrentUser } from '../../../model/currentUser';
 import { MyCollectionsState } from '../../../containers/paperShowCollectionControlButton/reducer';
 import { Paper } from '../../../model/paper';
-import { Profile } from '../../../model/profile';
 
 export interface HeaderProps extends RouteComponentProps<any> {
-  profile: Profile;
   layoutState: LayoutState;
   currentUserState: CurrentUser;
   myCollectionsState: MyCollectionsState;
