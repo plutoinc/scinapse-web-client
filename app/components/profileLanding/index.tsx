9824ff5dbc2d7c39795cfb6d9d43f36b35e79140
Tyler Shin <shincode@icloud.com>
2020-02-04 17:01:10 +0900

    apply minimum mobile design to the landing page

--- app/components/profileLanding/index.tsx
+++ app/components/profileLanding/index.tsx
@@ -1,59 +1,58 @@
 import React, { FC, useEffect, useState } from 'react';
-import { withStyles } from '../../helpers/withStylesHelper';
+import { isEqual } from 'lodash';
 import { Button } from '@pluto_network/pluto-design-elements';
 import { useLocation, useHistory } from 'react-router-dom';
-import QueryString from 'qs';
-import AffiliationAPI from '../../api/affiliation';
 import { useSelector } from 'react-redux';
+import AffiliationAPI from '../../api/affiliation';
 import { AppState } from '../../reducers';
 import globalDialogManager from '../../helpers/globalDialogManager';
 import { getCurrentPageType } from '../locationListener';
+import getQueryParamsObject from '../../helpers/getQueryParamsObject';
+const useStyles = require('isomorphic-style-loader/useStyles');
 const s = require('./profileLanding.scss');
 
 type ProfileLandingQuery = {
   aid: string;
-}
+};
 
 const ProfileLanding: FC = () => {
+  useStyles(s);
   const location = useLocation();
   const history = useHistory();
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [isValidAffiliationId, setIsValidAffiliationId] = useState<boolean>(false);
-  const currentUser = useSelector((state: AppState) => state.currentUser);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const currentUser = useSelector((state: AppState) => state.currentUser, isEqual);
 
-  useEffect(() => {
-    const queryString = location.search.split('?')[1];
-    const { aid } = QueryString.parse(queryString) as ProfileLandingQuery;
-    if (aid) {
-      AffiliationAPI.getAffiliation(aid).then(profileAff => {
-        setIsValidAffiliationId(profileAff.domains.length > 0);
-        setIsLoaded(true);
-      }).catch(() => {
-        setIsValidAffiliationId(false);
-        setIsLoaded(true);
-      });
-    } else {
-      setIsValidAffiliationId(false);
-      setIsLoaded(true);
-    }
-  }, [location.search]);
+  useEffect(
+    () => {
+      if (currentUser.profileId) history.push(`/profiles/${currentUser.profileId}`);
+    },
+    [currentUser.profileId, history]
+  );
 
-  useEffect(() => {
-    if (currentUser.profileId) {
-      history.push(`/profiles/${currentUser.profileId}`);
-    }
-  }, [currentUser.profileId]);
+  useEffect(
+    () => {
+      const queryString = getQueryParamsObject(location.search);
+      const { aid } = queryString as ProfileLandingQuery;
 
-  useEffect(() => {
-    if (isValidAffiliationId || !isLoaded) return;
-    history.push('/profiles/landing/enquiry');
-  }, [isValidAffiliationId, isLoaded]);
+      if (!aid) history.push('/profiles/landing/enquiry');
 
-  const handleContinueButtonClick = () => {
-    if (isLoaded && isValidAffiliationId) {
+      AffiliationAPI.getAffiliation(aid)
+        .then(profileAff => {
+          if (profileAff.domains.length === 0) history.push('/profiles/landing/enquiry');
+          setIsLoaded(true);
+        })
+        .catch(() => {
+          history.push('/profiles/landing/enquiry');
+        });
+    },
+    [location.search, history]
+  );
+
+  const handleClickContinueBtn = () => {
+    if (isLoaded) {
       history.push(`/profiles/verify-email${location.search}`);
     }
-  }
+  };
 
   const handleSigninButtonClick = () => {
     globalDialogManager.openSignInDialog({
@@ -64,61 +63,34 @@ const ProfileLanding: FC = () => {
       },
       isBlocked: false,
     });
-  }
+  };
 
   return (
-    <>
-      <div className={s.wrapper}>
-        <h1>Create a profile</h1>
-        <p>Some appealing description to hookup the user to make them want to create a profile.</p>
+    <div className={s.wrapper}>
+      <div>
+        <h1>Welcome to your professional research community</h1>
+        <p>Make your own profile, and join the research network</p>
         <p>If you are already a Scinapse user, please sign in.</p>
-        <div className={s.keyButtonContainer}>
-          {
-            currentUser.isLoggedIn || (
-              <Button
-                elementType="button"
-                color="gray"
-                isLoading={!isLoaded}
-                onClick={handleSigninButtonClick}
-              >
-                <span>
-                  Sign in
-                </span>
-              </Button>
-            )
-          }
-          {
-            isValidAffiliationId
-            ? (
-              <Button
-                elementType="button"
-                color="blue"
-                isLoading={!isLoaded}
-                onClick={handleContinueButtonClick}
-              >
-                <span>
-                  {
-                    currentUser.isLoggedIn ? 'Connect profile' : 'Create an account & make profile'
-                  }
-                </span>
-              </Button>
-            ) : (
-              <Button
-                elementType="button"
-                color="blue"
-                isLoading={!isLoaded}
-                onClick={handleContinueButtonClick}
-              >
-                <span>
-                  Sorry
-                </span>
-              </Button>
-            )
-          }
+        <div className={s.btnContainer}>
+          {!currentUser.isLoggedIn && (
+            <Button
+              style={{ marginBottom: '16px' }}
+              elementType="button"
+              color="gray"
+              isLoading={!isLoaded}
+              onClick={handleSigninButtonClick}
+              fullWidth
+            >
+              <span>Sign in</span>
+            </Button>
+          )}
+          <Button elementType="button" color="blue" isLoading={!isLoaded} onClick={handleClickContinueBtn} fullWidth>
+            <span>{currentUser.isLoggedIn ? 'Connect profile' : 'Create an account & make profile'}</span>
+          </Button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default withStyles<typeof ProfileLanding>(s)(ProfileLanding);
+export default ProfileLanding;
