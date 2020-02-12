c8d3d0b1d945a09f0ed228324b4a2b284d2b543f
NohYujeong <shdbwjd0326@naver.com>
2020-02-12 16:34:31 +0900

    Remove unused import syntax

--- app/containers/profile/index.tsx
+++ app/containers/profile/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useRef, useEffect } from 'react';
-import { useParams, useLocation, RouteComponentProps } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { isEqual } from 'lodash';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
@@ -27,7 +27,6 @@ import getQueryParamsObject from '../../helpers/getQueryParamsObject';
 import { PendingPaper } from '../../reducers/profilePendingPaperList';
 import PendingPaperList from './components/pendingPaperList';
 import PaperImportDialog from './components/paperImportDialog';
-import { fetchAuthorShowPageData } from './sideEffects';
 import { CoAuthor } from '../../components/common/coAuthor';
 const useStyles = require('isomorphic-style-loader/useStyles');
 const s = require('./connectedAuthor.scss');
@@ -37,9 +36,7 @@ enum AvailableTab {
   INFORMATION,
 }
 
-type ProfilePageProps = RouteComponentProps<{ profileId: string }>;
-
-const ProfilePage: FC<ProfilePageProps> = ({ match }) => {
+const ProfilePage: FC = () => {
   useStyles(s);
   const { profileId } = useParams();
   const dispatch = useThunkDispatch();
