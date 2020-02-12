3ab8b89ada365c4851174ae16ba55e0afc95c344
NohYujeong <shdbwjd0326@naver.com>
2020-02-12 18:01:14 +0900

    Remove failed trigger

--- app/containers/profile/sideEffects.tsx
+++ app/containers/profile/sideEffects.tsx
@@ -1,15 +1,9 @@
 import { LoadDataParams } from '../../routes';
-import { ActionCreators } from '../../actions/actionTypes';
 import { fetchProfileData, fetchProfilePapers, fetchProfilePendingPapers } from '../../actions/profile';
 
 export async function fetchAuthorShowPageData(params: LoadDataParams<{ profileId: string }>) {
   const { dispatch, match, queryParams } = params;
   const profileId = match.params.profileId;
-
-  if (isNaN(parseInt(profileId, 10))) {
-    // TODO: Change below
-    return dispatch(ActionCreators.failedToLoadAuthorShowPageData({ statusCode: 400 }));
-  }
 
   await Promise.all([
     await dispatch(fetchProfileData(profileId)),
