a487c61fc33eebeee3bab0d5157a5339d864679a
Tylor Shin <shincode@icloud.com>
2020-04-09 14:44:56 +0900

    change wrong API prefix

--- app/api/getHost.ts
+++ app/api/getHost.ts
@@ -7,9 +7,9 @@ const DEV_SERVER_API_HOST = 'https://dev.scinapse.io';
 
 export default function getAPIPrefix() {
   if (EnvChecker.isOnServer()) {
-    if (process.env.NODE_ENV === 'production') return `${PROD_SERVER_API_HOST}/api`;
-    if (process.env.NODE_ENV === 'development') return `${DEV_SERVER_API_HOST}/api`;
-    return `${STAGE_SERVER_API_HOST}/api`;
+    if (process.env.NODE_ENV === 'production') return PROD_SERVER_API_HOST;
+    if (process.env.NODE_ENV === 'development') return DEV_SERVER_API_HOST;
+    return STAGE_SERVER_API_HOST;
   }
 
   return CLIENT_API_PREFIX;
